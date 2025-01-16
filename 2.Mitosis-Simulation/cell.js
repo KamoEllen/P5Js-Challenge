7edf0aaaae9eea9d8b21175b4a1e22704f11b143
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-16 17:23:59 +0200

    Update 2.Mitosis-Simulation - daughter cells burst apart in opposite directions

--- 2.Mitosis-Simulation/cell.js
+++ 2.Mitosis-Simulation/cell.js
@@ -1,48 +1,34 @@
-function Cell(pos, r , c) {
+function Cell(pos, r, c) {
+  if (pos) {
+    this.pos = pos.copy();
+  } else {
+    this.pos = createVector(random(width), random(height));
+  }
 
-    if (pos)
-    {
-        this.pos = pos.copy();
-    }
-    else
-    {
-      this.pos = createVector(random(width), random(height));  
-    }
+  this.r = r || 60;
+  this.c = c || color(random(100, 255), 0, random(100, 255));
 
-    //this.pos = pos || createVector(random(width), random(height));
-    //this.pos = createVector(random(width) , random(height));
-    this.r = r || 60; 
-    //this.c = color(random(100,255),0,random(100,255));
-    this.c = c || color(random(100,255),0,random(100,255));
+  this.clicked = function (x, y) {
+    var d = dist(this.pos.x, this.pos.y, x, y);
+    if (d < this.r) {
+      //distance less than radius
+      return true;
+    } else {
+      return false;
+    }
+  };
+  this.mitosis = function () {
+    this.pos.x += random(-this.r, this.r);
+    var cellA = new Cell(this.pos, this.r * 0.8, this.c);
+    return cellA;
+  };
 
-    this.clicked = function(x,y)
-    {
-        var d = dist(this.pos.x,this.pos.y , x, y);
-          if (d < this.r) //distance less than radius
-        {
-            return true;
-        }
-        else 
-        {
-            return false;
-        }
-    }
-    this.mitosis = function() //makes 2 new cells
-    {
-        var cellA = new Cell(this.pos, this.r/2, this.c);
-        //var cellB = new Cell(this.pos, this.r/2, this.c);
-        return cellA;
-    }
-
-    this.move  = function()
-    {
-        var vel = p5.Vector.random2D();
-        this.pos.add(vel);
-    }
-    this.show  = function()
-    {
-        fill(this.c , 100);
-        //stroke();
-            ellipse(this.pos.x,this.pos.y, this.r,this.r);
-    }
+  this.move = function () {
+    var vel = p5.Vector.random2D();
+    this.pos.add(vel);
+  };
+  this.show = function () {
+    fill(this.c, 100);
+    ellipse(this.pos.x, this.pos.y, this.r, this.r);
+  };
 }
--- 2.Mitosis-Simulation/sketch.js
+++ 2.Mitosis-Simulation/sketch.js
@@ -1,34 +1,27 @@
-var cells=[];
+var cells = [];
 function setup() {
-    createCanvas(600, 400);
-    //Cell -name of constructor function tht makes cell obj
-    //cell - individual var holdin 1 obj 
-    cells.push(new Cell());
-    cells.push(new Cell());
+  createCanvas(600, 400);
+  //Cell -name of constructor function tht makes cell obj
+  //cell - individual var holdin 1 obj
+  cells.push(new Cell());
+  cells.push(new Cell());
 }
 
 function draw() {
-    background(51); 
-    for (var i = 0; i < cells.length; i++)
-    {
-      cells[i].move();
-      cells[i].show();   
-    }
-   
+  background(51);
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].move();
+    cells[i].show();
+  }
 }
-//did user click mouse  & did they click on cell obj
-function mousePressed() //global function determine if user clicked
-{
-    for (var i =cells.length -1; i >= 0;i--)
-    {
-        if (cells[i].clicked(mouseX,mouseY))
-        {
-            cells.push(cells[i].mitosis());
-            cells.push(cells[i].mitosis());
-            cells.splice(i,1);
-           // console.log("CLicked");
-        }
+function mousePressed() {
+  //global function  if user clicked
+  for (var i = cells.length - 1; i >= 0; i--) {
+    if (cells[i].clicked(mouseX, mouseY)) {
+      cells.push(cells[i].mitosis());
+      cells.push(cells[i].mitosis());
+      cells.splice(i, 1);
+      // console.log("CLicked");
     }
+  }
 }
-
-
