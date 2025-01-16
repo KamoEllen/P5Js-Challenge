41a83a5e55385290c192e7d20ef0ea59a595ea10
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-16 22:53:47 +0200

    Update 3.Solar-System , added moons , used floor(random(0,4)) to generate random number of moons

--- 3.Solar-System/sketch.js
+++ 3.Solar-System/sketch.js
@@ -3,13 +3,18 @@ let sun;
 function setup() {
     createCanvas(600, 600);  
     sun = new Planet(50, 0);  
-    sun.spawnMoons(5);
+    sun.spawnMoons(5);  // Create 5 planets
+
+    // Fix: Convert random number to integer for number of moons
+    for (let planet of sun.planets) {
+        let numMoons = floor(random(0, 4));  // Will give 0, 1, 2, or 3 moons
+        planet.spawnMoons(numMoons);
+    }
 }
- 
+
 function draw() {
     background(0);
-    translate(width/2 , height/2);
+    translate(width/2, height/2);
     sun.show();
     sun.orbit();
-   
 }
