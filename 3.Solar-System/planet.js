52fc32ffe3bb9fac54f682d7b8cab941abf8615e
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-17 11:03:33 +0200

    Update 3.Solar-System ,easy cam library was used to replace peasy-cam library . The installation was easier it seems more dependable.Easy cam is going to be used to rotate & zoom around the planets.

--- 3.Solar-System/planet.js
+++ 3.Solar-System/planet.js
@@ -1,68 +1,54 @@
-// Planet.js
 class Planet {
-    planets = [];
-    radius;
-    angle;
-    distance;
-    orbitSpeed;
-    v;
-  
     constructor(radius, distance) {
-      this.radius = radius;
-      this.distance = distance;
-      this.v = createVector(distance, 0, 0);  // Start on x-axis
-      this.angle = random(TWO_PI);
-      this.orbitSpeed = radius > 20 ? 
-                       random(0.005, 0.01) : 
-                       random(0.02, 0.03);
+        this.planets = [];
+        this.radius = radius;
+        this.distance = distance;
+        this.v = createVector(distance, 0, 0);
+        this.angle = random(TWO_PI);
+        this.orbitSpeed = radius > 20 ? 
+                         random(0.005, 0.01) : 
+                         random(0.02, 0.03);
+        // Add random color
+        this.color = color(random(100, 255), random(100, 255), random(100, 255));
     }
 
     spawnMoons(total) {
         this.planets = new Array(total);
         for (let i = 0; i < this.planets.length; i++) {
-          const moonRadius = this.radius * 0.3;
-          const moonDistance = random(this.radius * 2, this.radius * 4);
-          this.planets[i] = new Planet(moonRadius, moonDistance);
+            const moonRadius = this.radius * 0.3;
+            const moonDistance = random(this.radius * 2, this.radius * 4);
+            this.planets[i] = new Planet(moonRadius, moonDistance);
         }
-      }
-  
-    // spawnMoons(total) {
-    //   this.planets = new Array(total);
-    //   for (let i = 0; i < this.planets.length; i++) {
-    //     const moonRadius = this.radius * 0.3;
-    //     const moonDistance = random(this.radius * 2, this.radius * 4);
-    //     this.planets[i] = new Planet(moonRadius, moonDistance);
-    //   }
-    // }
-  
-    orbit() {
-      this.v = createVector(
-        cos(this.angle) * this.distance,
-        sin(this.angle) * this.distance,
-        0
-      );
-      this.angle += this.orbitSpeed;
-      
-      if (this.planets != null) {
-        for (let planet of this.planets) {
-          planet.orbit();
-        }
-      }
     }
-  
-    show() {
-      push();
-      noStroke();
-      fill(255);
-      
-      translate(this.v.x, this.v.y, this.v.z);
-      sphere(this.radius);
-  
-      if (this.planets != null) {
-        for (let planet of this.planets) {
-          planet.show();
+
+    orbit() {
+        this.v = createVector(
+            cos(this.angle) * this.distance,
+            sin(this.angle) * this.distance,
+            0
+        );
+        this.angle += this.orbitSpeed;
+        
+        if (this.planets != null) {
+            for (let planet of this.planets) {
+                planet.orbit();
+            }
         }
-      }
-      pop();
+    }
+
+    show() {
+        push();
+        noStroke();
+        fill(this.color);
+        
+        translate(this.v.x, this.v.y, this.v.z);
+        sphere(this.radius);
+
+        if (this.planets != null) {
+            for (let planet of this.planets) {
+                planet.show();
+            }
+        }
+        pop();
     }
 }
--- 3.Solar-System/sketch.js
+++ 3.Solar-System/sketch.js
@@ -1,10 +1,14 @@
+let easycam;
 let sun;
 
 function setup() {
-    createCanvas(1000,1000, WEBGL);
-    sun = new Planet(50, 0,0);
-    sun.spawnMoons(1,1);
-
+    createCanvas(1000, 1000, WEBGL);
+    sun = new Planet(50, 0);
+    easycam = createEasyCam();
+    
+    // Create some planets
+    sun.spawnMoons(5); // This will create 5 planets around the sun
+    
     for (let planet of sun.planets) {
         let numMoons = floor(random(0, 4));
         planet.spawnMoons(numMoons);
@@ -14,11 +18,6 @@ function setup() {
 function draw() {
     background(0);
     lights();
-    //Ad camera control
-    // camera(200, -200, 200,  // camera position
-    //        0, 0, 0,         // look at center
-    //        0, 1, 0);        // up vector
-  translate(width/2, height/2);
     sun.show();
     sun.orbit();
 }
