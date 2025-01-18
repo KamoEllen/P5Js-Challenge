a46e4b013375454ec169ba3f779beccaf4351c23
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-18 21:18:39 +0200

    Update 5.3D-Terrain demo vid

--- 5.3D-Terrain-Generation/sketch.js
+++ 5.3D-Terrain-Generation/sketch.js
@@ -1,43 +1,50 @@
-let cols;
-let rows;
-let scl = 20;
-let w = 600;
-let h = 600;
-let flying = 0; // Add this for movement
+var cols, rows;
+var scl = 20;
+var w = 1400;
+var h = 1000;
+
+var flying = 0;
+
+var terrain = [];
 
 function setup() {
-    createCanvas(w, h, WEBGL);
-    cols = w / scl;
-    rows = h / scl;
+  createCanvas(600, 600, WEBGL);
+  cols = w / scl;
+  rows = h / scl;
+
+  for (var x = 0; x < cols; x++) {
+    terrain[x] = [];
+    for (var y = 0; y < rows; y++) {
+      terrain[x][y] = 0; 
+    }
+  }
 }
 
 function draw() {
-    background(0);
-    stroke(255);
-    noFill();
-    
-    // Basic 3D setup
-    translate(width/2, height/2);
-    rotateX(PI/3);
-    translate(-w/2, -h/2);
-    
-    // Move through noise space
-    flying -= 0.1;
-    
-    let yoff = flying;
-    for (let y = 0; y < rows; y++) {
-        let xoff = 0;
-        beginShape(TRIANGLE_STRIP);
-        for (let x = 0; x < cols; x++) {
-            // Use noise for height
-            let height = map(noise(xoff, yoff), 0, 1, -100, 100);
-            
-            vertex(x*scl, y*scl, height);
-            vertex(x*scl, (y+1)*scl, height);
-            
-            xoff += 0.2;
-        }
-        endShape();
-        yoff += 0.2;
+ stroke(5);
+  flying -= 0.1;
+  var yoff = flying;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      terrain[x][y] = map(noise(xoff, yoff), 2, 4, -100, 100);
+      xoff += 0.2;
     }
+    yoff += 0.2;
+  }
+
+
+  background(55);
+  translate(0, 50);
+  rotateX(PI / 3);
+  fill(255, 200, 255, 150);
+  translate(-w / 2, -h / 2);
+  for (var y = 0; y < rows - 1; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (var x = 0; x < cols; x++) {
+      vertex(x * scl, y * scl, terrain[x][y]);
+      vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
+    }
+    endShape();
+  }
 }
