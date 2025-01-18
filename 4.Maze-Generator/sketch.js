82af4af18dd2c3e488995a3c3a8f7a9e7d4f68f8
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-18 10:46:01 +0200

    Update 4.Maze-Generate , finding the non-visited neighbouring cells

--- 4.Maze-Generator/sketch.js
+++ 4.Maze-Generator/sketch.js
@@ -8,7 +8,7 @@ let current;
 function setup() {
     createCanvas(600, 400);
     cols = floor(width / w);
-    rows = floor(height / w);
+    rows = floor(height / w); 
 
     for (let j = 0; j < rows; j++) {
         for (let i = 0; i < cols; i++) {
@@ -25,14 +25,50 @@ function draw() {
         grid[i].show();
     }
     current.visited= true;
+    current.checkNeighbors();
 }
-
+function index(i,j)
+{
+    return i+ j * cols;
+}
 class Cell {
     constructor(i, j) {
-        this.i = i;
-        this.j = j;
+        this.i = i;//rows
+        this.j = j; //cols
+         //which walls are active or not
+        //recursive backytracker - visit cell , dont backtrack  write 
+        //program to check if cells are visited
         this.walls = [true, true, true, true];
         this.visited = false;
+    }
+
+    checkNeighbors()
+    {
+         var neighbors = [];
+        //  var index = i + j-1 * cols;
+         //top - right - bottom - left
+         var top = grid[index(i , j-1)]
+         var right = grid[index(i+1 , j)];
+         var bottom  = grid[index(i , j+1)];
+         var left  = grid[index(i-1 , j)];
+
+         if (!top.visited)
+         {
+            neighbors.push(top);
+         }
+          if (!right.visited)
+         {
+            neighbors.push(right);
+         }
+          if (!bottom.visited)
+         {
+            neighbors.push(bottom);
+         }
+          if (!left.visited)
+         {
+            neighbors.push(left);
+         }
+
     }
 
     show() {
