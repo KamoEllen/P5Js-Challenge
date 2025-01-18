aa6711c9966288302506bcef558519f4e7107bb1
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-18 20:51:59 +0200

    Added 5.3D-Terrain-Generation using vetex ,  beginShape , endShape from processing.org>ref>shape

--- 5.3D-Terrain-Generation/sketch.js
+++ 5.3D-Terrain-Generation/sketch.js
@@ -14,16 +14,20 @@ function setup() {
 
 function draw() {
     background(0);
+    stroke(255);
+    noFill();
     // offsets and grid transformations
     //-w / 2 and -h / 2: These move the starting point from the center to the top-left
     //(w % scl) / 2: This handles centering when the canvas size isn't perfectly divisible by the scale
     let xOffset = -w / 2 + (w % scl) / 2;
     let yOffset = -h / 2 + (h % scl) / 2;
+
     for (let y = 0; y < rows; y++) {
+        beginShape(TRIANGLE_STRIP);
         for (let x = 0; x < cols; x++) {
-            stroke(255);
-            noFill();
-            rect(x * scl + xOffset, y * scl + yOffset, scl, scl);
+            vertex(x*scl + xOffset, y * scl + yOffset);
+           // rect(x * scl + xOffset, y * scl + yOffset, scl, scl);
         }
+        endShape();
     }
 }
