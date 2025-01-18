ee09535c1c6f64e33f759ea774c1af66f6346e04
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-18 11:08:17 +0200

    Update 4.Maze-Generate , select and visit random neighbor that hasnt been visited

--- 4.Maze-Generator/sketch.js
+++ 4.Maze-Generator/sketch.js
@@ -4,7 +4,6 @@ let w = 40; // width of each cell
 let grid = [];
 let current;
 
-
 function setup() {
     createCanvas(600, 400);
     cols = floor(width / w);
@@ -24,56 +23,58 @@ function draw() {
     for (let i = 0; i < grid.length; i++) {
         grid[i].show();
     }
-    current.visited= true;
-    current.checkNeighbors();
+    current.visited = true;
+    let next = current.checkNeighbors();
+    if (next) {
+        next.visited = true;
+        current = next;
+    }
 }
-function index(i,j)
-{
-   
-    if (i < 0 || j < 0 || i > cols-1 || j > rows -1)
-    {
+
+function index(i, j) {
+    if (i < 0 || j < 0 || i > cols-1 || j > rows-1) {
         return -1;
     }
-    return i+ j * cols;
+    return i + j * cols;
 }
+
 class Cell {
     constructor(i, j) {
-        this.i = i;//rows
-        this.j = j; //cols
-         //which walls are active or not
-        //recursive backytracker - visit cell , dont backtrack  write 
-        //program to check if cells are visited
+        this.i = i;
+        this.j = j;
         this.walls = [true, true, true, true];
         this.visited = false;
     }
 
-    checkNeighbors()
-    {
-         var neighbors = [];
-        //  var index = i + j-1 * cols;
-         //top - right - bottom - left
-         var top = grid[index(i , j-1)]
-         var right = grid[index(i+1 , j)];
-         var bottom  = grid[index(i , j+1)];
-         var left  = grid[index(i-1 , j)];
+    checkNeighbors() {
+        let neighbors = [];
+        
+        // Fixed: Using this.i and this.j instead of i, j
+        let top = grid[index(this.i, this.j-1)];
+        let right = grid[index(this.i+1, this.j)];
+        let bottom = grid[index(this.i, this.j+1)];
+        let left = grid[index(this.i-1, this.j)];
 
-         if (top && !top.visited)
-         {
+        if (top && !top.visited) {
             neighbors.push(top);
-         }
-          if (right && !right.visited)
-         {
+        }
+        if (right && !right.visited) {
             neighbors.push(right);
-         }
-          if (bottom && !bottom.visited)
-         {
+        }
+        if (bottom && !bottom.visited) {
             neighbors.push(bottom);
-         }
-          if (left && !left.visited)
-         {
+        }
+        if (left && !left.visited) {
             neighbors.push(left);
-         }
+        }
 
+        // Fixed: Check neighbors.length, not neighbors > 0
+        if (neighbors.length > 0) {
+            let r = floor(random(0, neighbors.length));
+            return neighbors[r];
+        } else {
+            return undefined;
+        }
     }
 
     show() {
