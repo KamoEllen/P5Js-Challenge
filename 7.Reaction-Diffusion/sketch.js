b47e445d922c9d9bbc58274e1881e17350a35848
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-19 17:55:19 +0200

    Added 7.Reaction-Diffusion ,added function to swap the current and next grid

--- 7.Reaction-Diffusion/sketch.js
+++ 7.Reaction-Diffusion/sketch.js
@@ -26,17 +26,39 @@ function setup() {
 function draw() {
     background(5);
 
+    for (var x = 0; x < width; x++)
+        {
+            for (var y = 0; y < height; y++)
+            {
+               next[x][y].a = grid[x][y].a*0.9;
+               next[x][y].b = grid[x][y].b*0.8;
+              // next[x][y] = (a/b)/2;
+            }
+        }
+
     loadPixels();
     for (var x = 0; x < width; x++)
     {
         for (var y = 0; y < height; y++)
         {
             var pix = (x + y * width)* 4;
-            pixels[pix + 0] =floor(grid[x][y].a*255);
+            // pixels[pix + 0] =floor(grid[x][y].a*255);
+            pixels[pix + 0] =floor(next[x][y].a*255);
             pixels[pix + 1] = 0;
-            pixels[pix + 2] = floor(grid[x][y].b*255);
+            // pixels[pix + 2] = floor(grid[x][y].b*255);
+            pixels[pix + 2] = floor(next[x][y].b*255);
             pixels[pix + 3] = 255;
         }
     }
     updatePixels();
+
+    swap();
+
+}
+
+swap()
+{
+    var temp = grid; //really like this
+    grid = next;
+    next = grid;
 }
