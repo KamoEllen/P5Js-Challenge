b664dac5e694f9a8184170e06de91280f46f0f4f
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-19 16:26:29 +0200

    Added 7.Reaction-Diffusion , grid stores amont of a and b chemicals across every pixel

---
+++ 7.Reaction-Diffusion/readme.md
@@ -0,0 +1,4 @@
+Charles Babbage - almost invented it, he fumbled 
+Alan Turing - built complete computer
+
+Alan Turing invented the Reaction Diffusion Algorithm. Proud to be learning about it.
--- 7.Reaction-Diffusion/sketch.js
+++ 7.Reaction-Diffusion/sketch.js
@@ -1,5 +1,20 @@
+//1 array is first row
+// [ [ 1 , 1 , 1]
+//   [ 1 , 1 , 1]
+//   [ 1 , 1 , 1]
+//   [ 1 , 1 , 1] ]
+
 function setup() {
-    createCanvas(600, 400, WEBGL);
+    createCanvas(600, 400);
+    pixelDensity(1);
+    grid = [];
+    for (var x = 0; x < width; x++)
+    {
+        for (var y = 0; y < width; y++)
+            {
+        
+            }
+    }
 }
 
 function draw() {
