933016005d1706b03010b45de3b347e4bcb1c624
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-20 00:43:51 +0200

    Update 7.Reaction-Diffusion , feel like it should work . need to find error

--- 7.Reaction-Diffusion/sketch.js
+++ 7.Reaction-Diffusion/sketch.js
@@ -1,81 +1,104 @@
-//1 array is first row
-// [ [ 1 , 1 , 1]
-//   [ 1 , 1 , 1]
-//   [ 1 , 1 , 1]
-//   [ 1 , 1 , 1] ]
+//Reaction-Diffusion System
 var grid;
 var next;
-var dA =1;
+var dA = 1;
 var dB = 0.5;
-var feed= 0.055;
-var k = 0.062; //kill rate - how fast values r being removed
+var feed = 0.055;
+var k = 0.062; //kill rate
 
 function setup() {
     createCanvas(600, 400);
     pixelDensity(1);
     grid = [];
     next = [];
-    for (var x = 0; x < width; x++)
-    {
+    
+    // Initialize grids
+    for (var x = 0; x < width; x++) {
         grid[x] = [];
         next[x] = [];
-        for (var y = 0; y < height; y++)
-            {
-              grid[x][y] = {a: random(1), b: random(1)}
-              next[x][y] = {a: 0, b: 0 }
-            }
+        for (var y = 0; y < height; y++) {
+            grid[x][y] = {a: 1, b: 0}; // Start with uniform state
+            next[x][y] = {a: 1, b: 0};
+        }
+    }
+    
+    // Add some chemical B to start the reaction
+    for (var i = 180; i < 220; i++) {
+        for (var j = 180; j < 220; j++) {
+            grid[i][j].b = 1;
+        }
     }
 }
 
 function draw() {
-    background(5);
-
-    for (var x = 0; x < width; x++)
-        {
-            for (var y = 0; y < height; y++)
-            {
-                var a = grid[x][y].a ;
-                var b = grid[x][y].b ;
-                //a
-                next[x][y].a = a +
-                (dA +  laplaceA() * a  ) -
-                (a * b * b)  +
+    background(51);
+    
+    // Calculate next state
+    for (var x = 1; x < width-1; x++) {
+        for (var y = 1; y < height-1; y++) {
+            var a = grid[x][y].a;
+            var b = grid[x][y].b;
+            
+            // Calculate new A and B concentrations
+            next[x][y].a = a + 
+                (dA * laplaceA(x, y)) - 
+                (a * b * b) + 
                 (feed * (1 - a));
-                //b
-                next[x][y].a = b +
-                (dB +  laplaceB() ) +
-                (a * b * b)  +
+                
+            next[x][y].b = b + 
+                (dB * laplaceB(x, y)) + 
+                (a * b * b) - 
                 ((k + feed) * b);
-
-            //    next[x][y].a = grid[x][y].a*0.9;
-            //    next[x][y].b = grid[x][y].b*0.8;
-              
-            }
         }
-
+    }
+    
+    // Update and display
     loadPixels();
-    for (var x = 0; x < width; x++)
-    {
-        for (var y = 0; y < height; y++)
-        {
-            var pix = (x + y * width)* 4;
-            // pixels[pix + 0] =floor(grid[x][y].a*255);
-            pixels[pix + 0] =floor(next[x][y].a*255);
-            pixels[pix + 1] = 0;
-            // pixels[pix + 2] = floor(grid[x][y].b*255);
-            pixels[pix + 2] = floor(next[x][y].b*255);
+    for (var x = 0; x < width; x++) {
+        for (var y = 0; y < height; y++) {
+            var pix = (x + y * width) * 4;
+            var c = floor((next[x][y].a - next[x][y].b) * 255);
+            pixels[pix + 0] = c;
+            pixels[pix + 1] = c;
+            pixels[pix + 2] = c;
             pixels[pix + 3] = 255;
         }
     }
     updatePixels();
-
+    
     swap();
-
 }
 
-swap()
-{
-    var temp = grid; //really like this
+function laplaceA(x, y) {
+    var sumA = 0;
+    sumA += grid[x][y].a * -1;
+    sumA += grid[x-1][y].a * 0.2;
+    sumA += grid[x+1][y].a * 0.2;
+    sumA += grid[x][y+1].a * 0.2;
+    sumA += grid[x][y-1].a * 0.2;
+    sumA += grid[x-1][y-1].a * 0.05;
+    sumA += grid[x+1][y-1].a * 0.05;
+    sumA += grid[x+1][y+1].a * 0.05;
+    sumA += grid[x-1][y+1].a * 0.05;
+    return sumA;
+}
+
+function laplaceB() {  
+    var sumB = 0;
+    sumB += grid[x][y].b * -1;
+    sumB += grid[x-1][y].b * 0.2;
+    sumB += grid[x+1][y].b * 0.2;
+    sumB += grid[x][y+1].b * 0.2;
+    sumB += grid[x][y-1].b * 0.2;
+    sumB += grid[x-1][y-1].b * 0.05;
+    sumB += grid[x+1][y-1].b * 0.05;
+    sumB += grid[x+1][y+1].b * 0.05;
+    sumB += grid[x-1][y+1].b * 0.05;
+    return sumB;
+}
+
+function swap() {
+    var temp = grid;
     grid = next;
-    next = grid;
+    next = temp;
 }
