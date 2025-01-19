63aaa300167bad2736ebce3ac1dcd207c66b3ead
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-19 16:29:47 +0200

    Added 7.Reaction-Diffusion , created  another array to store the next generation

--- 7.Reaction-Diffusion/sketch.js
+++ 7.Reaction-Diffusion/sketch.js
@@ -3,16 +3,30 @@
 //   [ 1 , 1 , 1]
 //   [ 1 , 1 , 1]
 //   [ 1 , 1 , 1] ]
+var grid;
+var next;
 
 function setup() {
     createCanvas(600, 400);
     pixelDensity(1);
     grid = [];
+    next = [];
     for (var x = 0; x < width; x++)
     {
+        grid[x] = [];
+        next[x] = [];
         for (var y = 0; y < width; y++)
             {
-        
+              grid[x][y]
+              {
+                a: 0;
+                b: 0;
+              }
+              next[x][y]
+              {
+                a: 0;
+                b: 0;
+              }
             }
     }
 }
