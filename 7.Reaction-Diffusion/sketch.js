6db2d6a5a1c7f803819ac2f8410b91838b5ca89c
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-19 17:45:51 +0200

    set the pixel colors based on the amount of a and b

--- 7.Reaction-Diffusion/sketch.js
+++ 7.Reaction-Diffusion/sketch.js
@@ -15,22 +15,28 @@ function setup() {
     {
         grid[x] = [];
         next[x] = [];
-        for (var y = 0; y < width; y++)
+        for (var y = 0; y < height; y++)
             {
-              grid[x][y]
-              {
-                a: 0;
-                b: 0;
-              }
-              next[x][y]
-              {
-                a: 0;
-                b: 0;
-              }
+              grid[x][y] = {a: random(1), b: random(1)}
+              next[x][y] = {a: 0, b: 0 }
             }
     }
 }
 
 function draw() {
-    background(0);
+    background(5);
+
+    loadPixels();
+    for (var x = 0; x < width; x++)
+    {
+        for (var y = 0; y < height; y++)
+        {
+            var pix = (x + y * width)* 4;
+            pixels[pix + 0] =floor(grid[x][y].a*255);
+            pixels[pix + 1] = 0;
+            pixels[pix + 2] = floor(grid[x][y].b*255);
+            pixels[pix + 3] = 255;
+        }
+    }
+    updatePixels();
 }
