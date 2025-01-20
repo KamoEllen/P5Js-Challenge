4895cbe664da2f30c383da5a90e5ae245a18388e
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-20 12:12:31 +0200

    Update 9.Object-Oriented-Fractal-Trees , used point x and y to create branch , removed most code from 8.fractal-trees folder

--- 9.Object-Oriented-Fractal-Trees/branch.js
+++ 9.Object-Oriented-Fractal-Trees/branch.js
@@ -1,5 +1,12 @@
-function Brnach(begin,end)
+function Branch(begin,end) // x , y -> p5 vector obj
 {
     this.begin = begin;
     this.end = end;
+
+    this.show = function()
+    {
+        stroke(255);
+        line(this.begin.x , this.begin.y, this.end.x, this.end.y);
+
+    }
 }
--- 9.Object-Oriented-Fractal-Trees/sketch.js
+++ 9.Object-Oriented-Fractal-Trees/sketch.js
@@ -1,35 +1,31 @@
-var angle = 0;
-var slider;
-
+var root;
 function setup() {
     createCanvas(400, 400);
-    slider = createSlider(0,TWO_PI, PI/4,0.01);
+    var a = createVector(width/2, height);
+    var b = createVector(width/ 2 , height - 100);
+     root = new Branch(a , b );
 }
 
 function draw() {
     background(51);
-    angle = slider.value();
-    stroke(255);
-    translate(200,height);
-    branch(100);
+    root.show();
 }
 
-function branch(len)
-{
-   line(0,0,0,-len);
-   translate(0,-len);
+// {
+//    line(0,0,0,-len);
+//    translate(0,-len);
   
-    if (len > 4) //base case 
-    {
-        push(); //save current state
-        rotate(angle);
-        branch(len * 0.67); 
-        pop(); //restore 
+//     if (len > 4) //base case 
+//     {
+//         push(); //save current state
+//         rotate(angle);
+//         branch(len * 0.67); 
+//         pop(); //restore 
 
-        push(); //save again for next branch
-        rotate(-angle);
-        branch(len * 0.67);
-        pop(); //restore
-    }
+//         push(); //save again for next branch
+//         rotate(-angle);
+//         branch(len * 0.67);
+//         pop(); //restore
+//     }
 
-} 
+// } 
