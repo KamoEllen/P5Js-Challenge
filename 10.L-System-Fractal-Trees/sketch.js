ec890e100eb58f2b7b98a547779954174c14c84d
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-20 13:05:35 +0200

    Update 10.L-System-Fractal-Trees, generate function works

--- 10.L-System-Fractal-Trees/sketch.js
+++ 10.L-System-Fractal-Trees/sketch.js
@@ -1,7 +1,56 @@
-function setup() {
-    createCanvas(400, 400);
+//l-system
+//recursive way of generating sentences over and over again using string replacement
+//rule : v
+// 0 : A
+// 1 : AB
+// 2 : AB B
+// 3 : AB A AB
+var axiom = "A";
+var sentence = axiom;
+var rule1 =
+{
+    a : "A",
+    b : "AB"
+}
+var rule2 =
+{
+    a : "B",
+    b : "A"
 }
 
-function draw() {
-    background(51);
+function generate()
+{
+    var nextSentence = "";
+    for (var i = 0; i < sentence.length;i++)
+    {
+        var current = sentence.charAt(i);
+        if (current == rule1.a)
+        {
+            nextSentence += rule1.b;
+
+        }
+        else if (current == rule2.a)
+        {
+            nextSentence += rule2.b;
+        }
+        else
+        {
+            nextSentence += current;
+        }
+
+    }
+    sentence = nextSentence;
+    createP(sentence);
+
 }
+function setup() {
+    // createCanvas(400, 400);
+    noCanvas();
+    createP(axiom);
+    var button = createButton("generate");
+    button.mousePressed(generate);
+}
+
+// function draw() {
+//     background(51);
+// }
