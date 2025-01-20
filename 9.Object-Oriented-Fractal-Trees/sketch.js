e16539a28f16bdd2cca81e6fbc035a90c44b5036
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-20 12:14:41 +0200

    Update 9.Object-Oriented-Fractal-Trees , removed root , using tree array

--- 9.Object-Oriented-Fractal-Trees/sketch.js
+++ 9.Object-Oriented-Fractal-Trees/sketch.js
@@ -1,14 +1,21 @@
-var root;
+// var root;
+var tree = [];
 function setup() {
     createCanvas(400, 400);
     var a = createVector(width/2, height);
     var b = createVector(width/ 2 , height - 100);
      root = new Branch(a , b );
+     tree[0] = root;
 }
 
 function draw() {
     background(51);
     root.show();
+
+    for (var i = 0 ; i < tree.length; i++)
+    {
+        tree[i].show();
+    }
 }
 
 // {
