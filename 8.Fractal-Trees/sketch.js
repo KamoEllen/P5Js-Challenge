e8d3185024f1f89358549662de01afdf6b9b8374
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-20 11:56:05 +0200

    Update 8.Fractal-Trees , added push() and pop() , slider works

--- 8.Fractal-Trees/sketch.js
+++ 8.Fractal-Trees/sketch.js
@@ -1,6 +1,3 @@
-//continue until you reach (*1)
-//4! = 4 * 3 * 2 * 1
-//5!= 5 * 4 * 3 * 2 * 1
 var angle = 0;
 var slider;
 
@@ -12,46 +9,27 @@ function setup() {
 function draw() {
     background(51);
     angle = slider.value();
-
-    //trunk of tree
-    // var len = 100; //length
-    stroke(255); //white stoke
+    stroke(255);
     translate(200,height);
-    // line(200, height , 200, height - len);
     branch(100);
 }
-//doesnt work without 'function'
-// branch(len)
-// {
-//     line(200, height , 200, height - len);
-
-// }
 
 function branch(len)
 {
-    // line(200, height , 200, height - len);
-    line(0,0,0,-len);
-    //now wanna rotate n draw branch to another direction
-    translate(0,-len);
-    //without 'translate(0,-len);' it will start from origin
-    //want it to start at top of last branch
-    // rotate(PI/4);
-    
-    if (len > 4)
+   line(0,0,0,-len);
+   translate(0,-len);
+  
+    if (len > 4) //base case 
     {
-        push();
+        push(); //save current state
         rotate(angle);
-        branch(len * 0.67); //calling branch n rotatin then again
-        //want to come back to parent then move in diff direction
+        branch(len * 0.67); 
+        pop(); //restore 
+
+        push(); //save again for next branch
         rotate(-angle);
         branch(len * 0.67);
-        pop();
+        pop(); //restore
     }
-   
-    // branch(len * 0.67); //calling branch from within branch - infinite loop
 
-    // line(0,0,0,-len*0.67);
-
-} //wanna draw branches from origin , but origin is
-//top left corner , so imma switch that n make the origin
-//center bottom , gonna use translate
+} 
