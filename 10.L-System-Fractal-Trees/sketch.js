b86ac649e05ae790b011f446114e33c05075adc5
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-20 16:12:02 +0200

    Update 10.L-System-Fractal-Trees , translate to the bottom, middle of the screen

--- 10.L-System-Fractal-Trees/sketch.js
+++ 10.L-System-Fractal-Trees/sketch.js
@@ -5,20 +5,27 @@
 // 1 : AB
 // 2 : AB B
 // 3 : AB A AB
-var axiom = "A";
+var axiom = "F";
+// var axiom = "A";
 var sentence = axiom;
 var rules = [];
+var len = 100;
 
 rules[0] =
 {
-    a : "A",
-    b : "ABC"
+    a : "F",
+    b : "FF+[+F-F-F]-[-F+F+F]"
 }
-rules[1] =
-{
-    a : "B",
-    b : "A"
-}
+// rules[0] =
+// {
+//     a : "A",
+//     b : "ABC"
+// }
+// rules[1] =
+// {
+//     a : "B",
+//     b : "A"
+// }
 
 function generate()
 {
@@ -43,11 +50,49 @@ function generate()
     }
     sentence = nextSentence;
     createP(sentence);
+    turtle();
+}
+
+function turtle()
+{
+    background(51);
+    translate(width/2, height);
+    stroke(255);
+    //turtle graphics engine 
+    for (var i = 0; i < sentence.length; i++)
+    {
+        var current =sentence.charAt(i);
+
+        if (current == "F")
+        {
+            line(0,0,0,-len);
+            translate(0,-len);
+        }
+        else if (current == "+")
+        {
+            rotate(PI/6);
+        }
+        else if (current == "-")
+        {
+            rotate(-PI/6);
+        }
+        else if (current == "[")
+        {
+            push();
+        }
+        else if (current == "]")
+        {
+            pop();
+        }
+    }
+
 }
 function setup() {
-    // createCanvas(400, 400);
-    noCanvas();
+    createCanvas(400, 400);
+    // noCanvas();
+    background(15);
     createP(axiom);
+    turtle();
     var button = createButton("generate");
     button.mousePressed(generate);
 }
