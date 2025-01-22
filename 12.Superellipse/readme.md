62e9470e48f7719f2734e223286263c0a52321f2
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-22 02:48:16 +0200

    Update 12.Superellipse , added sgn function

---
+++ 12.Superellipse/readme.md
@@ -0,0 +1,7 @@
+https://en.wikipedia.org/wiki/Superellipse
+
+Calculation used:
+https://wikimedia.org/api/rest_v1/media/math/render/svg/ffbfd07939ee59abba3be13ca364526d4829b1a2
+
+Sgn Function
+https://en.wikipedia.org/wiki/Sign_function
--- 12.Superellipse/sketch.js
+++ 12.Superellipse/sketch.js
@@ -5,20 +5,45 @@
 function setup() {
     createCanvas(400, 400);
   }
+  function sgn(val)
+  {
+    if (val > 0)
+    {
+        return 1;
+    }
+    else if ( val < 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+  }
   
   function draw() {
     background(51);
     translate(width/2, height/2);
-    var r = 100;
+    // var r = 100;
+    var a = 100;
+    var b = 100;
+    var m = 2;
     stroke(255);
     noFill();
 
     beginShape();
 
-    for (var a = 0 ; a < TWO_PI; a += 0.1)
+    for (var angle = 0 ; angle < TWO_PI; angle += 0.1)
     {
-        var x = r *cos(a);
-        var y = r * sin(a);
+        // var x = r *cos(a);
+        // var y = r * sin(a);
+        var na = 2 / n ;
+        //sgn(num) <- is num is +positive it returns 1 , if num is
+        //negative it returns -1 , if num is 0 , it returns 0
+        var x = pow(abs(cos(angle)) , na) * a * sgn(cos(angle));
+        var yx = pow(abs(sin(angle)) , na) * b * sgn(sin(angle));
+
+
         // point(x,y);
         vertex(x,y);
     }
