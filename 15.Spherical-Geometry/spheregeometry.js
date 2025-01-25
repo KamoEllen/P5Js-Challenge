3341f1abe9433f0533b94bc3d1a3422fd3eda2b7
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-25 18:31:57 +0200

    Update 15.Spherical-Geometry, added code will clean up

---
+++ 15.Spherical-Geometry/spheregeometry.js
@@ -0,0 +1,51 @@
+     //100 -> detail level / points
+     // (radius,latitude,longitude) -> (x,y,z)
+     //Polar Coordinates
+     //x = rcos(theta)
+     //y = rsin(theta)
+     //https://en.wikipedia.org/wiki/Spherical_coordinate_system
+
+const globe = [];
+const r = 200;
+const total = 25;
+let angleX = 0;
+let angleY = 0;
+
+function setup() {
+  createCanvas(500, 500, WEBGL);
+  fill(12);
+  strokeWeight(2);
+  stroke(30);
+
+  for (let i = 0; i < total + 1; i++) {
+    globe[i] = [];
+    const lat = map(i, 0, total, 0, PI);
+    for (let j = 0; j < total + 1; j++) {
+      const lon = map(j, 0, total, 0, TWO_PI);
+      const x = r * sin(lat) * cos(lon);
+      const y = r * sin(lat) * sin(lon);
+      const z = r * cos(lat);
+      globe[i][j] = createVector(x, y, z);
+    }
+  }
+}
+
+function draw() {
+  background(25);
+  rotateX(angleX);
+  rotateY(angleY);
+
+  for (let i = 0; i < total; i++) {
+    beginShape(TRIANGLE_STRIP);
+    for (let j = 0; j < total + 1; j++) {
+      const v1 = globe[i][j];
+      vertex(v1.x, v1.y, v1.z);
+      const v2 = globe[i + 1][j];
+      vertex(v2.x, v2.y, v2.z);
+    }
+    endShape();
+  }
+
+  angleX += 0.005;
+  angleY += 0.006;
+}
