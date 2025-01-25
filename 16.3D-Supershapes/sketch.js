03215e900d270363bbb4af438801feb3787a6edf
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-25 18:51:56 +0200

    Added  cute dot

--- 16.3D-Supershapes/sketch.js
+++ 16.3D-Supershapes/sketch.js
@@ -1,9 +1,14 @@
+var firework;
 function setup()
 {
-    // createCanva(400,400);
     createCanvas(400, 400);
+    stroke(255);
+    strokeWeight(4);
+    firework = new Particle(200,150);
 }
 function draw()
 {
     background(51);
+    firework.update();
+    firework.show();
 }
