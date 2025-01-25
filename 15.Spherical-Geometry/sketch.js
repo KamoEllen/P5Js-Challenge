dc919a798318ecf1daba5a75c687a9f80e84ac45
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-25 18:12:43 +0200

    Update 15.Spherical-Geometry , added p5.js built in sphere

--- 15.Spherical-Geometry/sketch.js
+++ 15.Spherical-Geometry/sketch.js
@@ -1,8 +1,12 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(400, 400 , WEBGL);
   }
 
 function draw()
 {
     background(51);
+    fill(255);
+    lights();
+    translate(width/4 , height/4);
+    sphere(100);
 }
