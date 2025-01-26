28252b07c9d90aa99a8555253ac4c93c51d62262
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-26 17:44:59 +0200

    Update 16.Fireworks , particles travel up and explode

--- 16.Fireworks/firework.js
+++ 16.Fireworks/firework.js
@@ -1,6 +1,7 @@
 class Firework {
     constructor() {
         this.exploded = false;
+        this.particles = [];
         this.position = createVector(random(width), height);
         this.velocity = createVector(0, random(-12, -8));
         this.acceleration = createVector(0, 0);
@@ -21,12 +22,26 @@ class Firework {
         {
             // this.firework = null;
             this.exploded = true;
+            this.explode();
         }
     } 
 
+
+    explode = function()
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            var p = new Particle(this.fireworks.pos.x,this.fireworks.pos.y);
+            this.particles.push(p);
+        }
+    }
     show() {
         stroke(255);
         strokeWeight(4);
         point(this.position.x, this.position.y);
+        for (var i = 0; i < this.particles.length; i++)
+        {
+            this.particles[i].show();
+        }
     }
 }
