aac777901e36b3f8f4a65e8c8cb568e161158ef6
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-27 22:59:08 +0200

    Added 18.Smart-Rockets folder

---
+++ 18.Smart-Rockets/readme.md
@@ -0,0 +1,4 @@
+goal- rocket to reach destination
+1. rocket - physics built into it
+2. dna-control physics, needs mutation + cross over (generic algorithms)
+3. population obj- will manage rocket array
--- 18.Smart-Rockets/sketch.js
+++ 18.Smart-Rockets/sketch.js
@@ -1,6 +1,34 @@
 function setup() {
     createCanvas(400, 400);
-  }
+  } 
   function draw() {
-    background(51);
+    background(1);
+  }
+
+  function Rocket()
+  {
+    this.pos = createVector();
+    this.vel = createVector();
+    this.acc = createVector();
+
+
+    this.applyForce = fuction(force)
+    {
+        this.acc.add(force);
+    }
+    this.update = function()
+    {
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
+    }
+
+    this.show = function()
+    {
+        translate(this.pos.x, this.pos.y);
+        rotate(this.vel.heading());
+        rectMode(CENTER);
+        rect(this.pos.y, this.pos.y,10,50);
+
+    }
   }
