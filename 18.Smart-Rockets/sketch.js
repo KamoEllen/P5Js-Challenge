a2a615158b910c6cce898660213bc8504032d17d
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-27 23:08:28 +0200

    Update 18.Smart-Rockets , added rocket object

--- 18.Smart-Rockets/sketch.js
+++ 18.Smart-Rockets/sketch.js
@@ -1,34 +1,43 @@
+var rocket;
+
 function setup() {
-    createCanvas(400, 400);
-  } 
-  function draw() {
-    background(1);
-  }
+  createCanvas(400, 400);
+  rocket = new Rocket();
+} 
+function draw() {
+  background(1);
+  rocket.update();
+  rocket.show();
+}
 
-  function Rocket()
+function Rocket()
+{
+  this.pos = createVector(width/2, height);
+  this.vel = createVector(0,-1);
+  this.acc = createVector();
+
+
+  this.applyForce = function(force) 
   {
-    this.pos = createVector();
-    this.vel = createVector();
-    this.acc = createVector();
-
-
-    this.applyForce = fuction(force)
-    {
-        this.acc.add(force);
-    }
-    this.update = function()
-    {
-        this.vel.add(this.acc);
-        this.pos.add(this.vel);
-        this.acc.mult(0);
-    }
-
-    this.show = function()
-    {
-        translate(this.pos.x, this.pos.y);
-        rotate(this.vel.heading());
-        rectMode(CENTER);
-        rect(this.pos.y, this.pos.y,10,50);
-
-    }
+      this.acc.add(force);
   }
+
+  this.update = function()
+  {
+      this.vel.add(this.acc);
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+  }
+
+  this.show = function()
+  {
+    push();
+      translate(this.pos.x, this.pos.y);
+      rotate(this.vel.heading());
+      rectMode(CENTER);
+      // rect(this.pos.y, this.pos.y,10,50);
+      rect(0,0,50,50);
+    pop();
+
+  }
+}
