e9252abb90b166f9b1c4582054194d77d30bd04a
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-28 22:37:07 +0200

    Cleaned code and comments

--- 18.Smart-Rockets/sketch.js
+++ 18.Smart-Rockets/sketch.js
@@ -1,110 +1,95 @@
 var population;
-var lifespan = 400; //each rocket lives 200 frames, its dna will have array will have 200 vectors
+var lifespan = 400; // Lifespan of each rocket in frames
 var lifeP;
 var count = 0;
 var target;
-var rx = 100;
-var ry = 150;
-var rw = 200;
-var rh = 10;
-var maxforce = 0.2;
+//obstacle
+var rx = 100; //x position
+var ry = 150; //y position
+var rw = 200; //width
+var rh = 10;  //height
+var maxforce = 0.2; // max force applied to rockets
 
 function setup() {
   createCanvas(400, 400);
-  rocket = new Rocket();
   population = new Population();
   lifeP = createP();
-  target = createVector(width/2, 50);
-} 
+  target = createVector(width / 2, 50); // Target position
+}
 
 function draw() {
   background(1);
   population.run();
-  // pop.show();
   lifeP.html(count);
-  count++; // Fixed: the global count variable
-  if (count == lifespan) //200
-  {
-    population.evaluate();
-    population.selction();
-    // population = new Population(); //should restart 
-    count = 0; //reset
-  }
-  //obstacle 
-  fill(255);
-  rect(100,150,200,10); //x = 100, y = 150 , 150 pixels across, 10 pixels wide
- 
- 
+  count++;
 
-  ellipse(target.x, target.y , 16,16); //the target the cute little lines are trying to get to
+  // Reset population after lifespan is reached
+  if (count == lifespan) {
+    population.evaluate();
+    population.selection();
+    count = 0;
+  }
+
+  // obstacle
+  fill(255);
+  rect(rx, ry, rw, rh);
+
+  // target
+  ellipse(target.x, target.y, 16, 16);
 }
+
 
 function Population() {
   this.rockets = [];
   this.popsize = 50;
   this.matingpool = [];
 
+  
   for (var i = 0; i < this.popsize; i++) {
     this.rockets[i] = new Rocket();
   }
 
+  // fitness 
   this.evaluate = function() {
-    //next thing being added , reusing rockets that got the closest
-    //to the target n creating new lines from them ,babies
-
-    //calculate their fitness
-    //create mating pool to collect parents 
-
     var maxfit = 0;
 
+    // Calculate fitness for each rocket
     for (var i = 0; i < this.popsize; i++) {
-      this.rockets[i].calcFitness(); //calcFitness() in Rocket()
-
+      this.rockets[i].calcFitness();
       if (this.rockets[i].fitness > maxfit) {
         maxfit = this.rockets[i].fitness;
       }
     }
 
-    createP(maxfit); //max fitness is going up :)
-    // console.log(this.rockets);
-
+    // Normalize fitness values
     for (var i = 0; i < this.popsize; i++) {
       this.rockets[i].fitness /= maxfit;
     }
 
-    //
-    this.matingpool = []; // generating new array
-  for (var i = 0; i < this.popsize; i++) 
-    {
-      var n = this.rockets[i].fitness *100;
-      for (var j = 0 ; j < n; j++)
-      {
+    // Mating pool based on fitness
+    this.matingpool = [];
+    for (var i = 0; i < this.popsize; i++) {
+      var n = this.rockets[i].fitness * 100;
+      for (var j = 0; j < n; j++) {
         this.matingpool.push(this.rockets[i]);
       }
-      //rocket a is more likely to be picked if its in the 
-      //mating pool 70 times compared to rocket b that is only there 5 times
-  }
-  } // Fixed: Added missing closing bracket
-
-
-  this.selction =  function()
-  {
-    var newRockets = [];
-    for (var i = 0; i < this.rockets.length;i++)
-    {
-        var parentA = random(this.matingpool).dna; //using random function on array , dont have to use index
-    var parentB = random(this.matingpool).dna;
-    var child = parentA.crossover(parentB); //using ma n pa dna
-    child.mutation();
-    newRockets[i] = new Rocket(child);
     }
-  this.rockets = newRockets;
   }
 
+  // Select healthy parents to make kids
+  this.selection = function() {
+    var newRockets = [];
+    for (var i = 0; i < this.rockets.length; i++) {
+      var parentA = random(this.matingpool).dna;
+      var parentB = random(this.matingpool).dna;
+      var child = parentA.crossover(parentB);
+      child.mutation();
+      newRockets[i] = new Rocket(child);
+    }
+    this.rockets = newRockets;
+  }
 
-  
-
-
+  // Update and display every rockets
   this.run = function() {
     for (var i = 0; i < this.popsize; i++) {
       this.rockets[i].update();
@@ -113,50 +98,32 @@ function Population() {
   }
 }
 
-function DNA( genes) { 
-  //can recieve genes n make new genes , or create new random genes
-  if (genes)
-  {
+// manage genetic information
+function DNA(genes) {
+  if (genes) {
     this.genes = genes;
-  }
-  else
-  {
+  } else {
     this.genes = [];
-  for (var i = 0; i < lifespan; i++) {
-    this.genes[i] = p5.Vector.random2D();
-    this.genes[i].setMag(maxforce); //genes weaker movement
-
-  }
-  
-
+    for (var i = 0; i < lifespan; i++) {
+      this.genes[i] = p5.Vector.random2D();
+      this.genes[i].setMag(maxforce);
+    }
   }
 
-  this.crossover = function(partner)
-  {
-    var newgenes =  [];
+  // Crossover two DNA sequences
+  this.crossover = function(partner) {
+    var newgenes = [];
     var mid = floor(random(this.genes.length));
-    for ( var i = 0; i < this.genes.length;i++) //reminds me of binary search
-    {
-      if (i > mid)
-      {
-        newgenes[i] = this.genes[i];
-      }
-      else
-      {
-        newgenes[i] = partner.genes[i];
-      }
-     
-
+    for (var i = 0; i < this.genes.length; i++) {
+      newgenes[i] = (i > mid) ? this.genes[i] : partner.genes[i];
     }
     return new DNA(newgenes);
   }
 
-  this.mutation = function()
-  {
-    for (var i =0; i  < this.genes.length; i++)
-    {
-      if (random(1) < 0.01)
-      {
+  // dna mutation with small probability maxfoce=0.2
+  this.mutation = function() {
+    for (var i = 0; i < this.genes.length; i++) {
+      if (random(1) < 0.01) {
         this.genes[i] = p5.Vector.random2D();
         this.genes[i].setMag(maxforce);
       }
@@ -164,85 +131,58 @@ function DNA( genes) {
   }
 }
 
+//individual rockets
 function Rocket(dna) {
-  this.pos = createVector(width/2, height);
+  this.pos = createVector(width / 2, height);
   this.vel = createVector();
-  // this.vel = p5.Vector.random2D();
   this.acc = createVector();
   this.completed = false;
-  this.crashed = false; // checking if rockets hit canvas with crashed function
-  if (dna)
-  {
-    this.dna = dna;
-  }
-  else
-  {
-    this.dna = new DNA();
-  }
-  
-  // this.count = 0;
+  this.crashed = false;
+  this.dna = dna ? dna : new DNA();
   this.fitness = 0;
 
+  // Apply force to the rocket
   this.applyForce = function(force) {
     this.acc.add(force);
   }
 
+  // Calculate fitness based on distance to target
   this.calcFitness = function() {
-    var d = dist(this.pos.x, this.pos.y, target.x, target.y); //distance from rocket n the target
-    this.fitness = map(d,0,width,width,0);
+    var d = dist(this.pos.x, this.pos.y, target.x, target.y);
+    this.fitness = map(d, 0, width, width, 0);
 
-    if (this.completed)
-    {
-      this.fitness*=10;
-    }
-    if (this.crashed)
-    {
-      this.fitness = 10; // rockets should evolve to go around obstacle
-    }
+    if (this.completed) this.fitness *= 10;
+    if (this.crashed) this.fitness = 10;
   }
 
+  //rocket's position and state
   this.update = function() {
-
-    var d = dist(this.pos.x,this.pos.y , target.x,target.y);
-    if (d < 10)
-    {
-      //then it reached target
+    var d = dist(this.pos.x, this.pos.y, target.x, target.y);
+    if (d < 10) {
       this.completed = true;
       this.pos = target.copy();
     }
-   
 
     if (this.pos.x > rx && this.pos.x < rx + rw &&
-       this.pos.y > ry && this.pos.y < ry + rh )
-    {
-
+        this.pos.y > ry && this.pos.y < ry + rh) {
       this.crashed = true;
     }
 
-    if (this.pos.y > height || this.pos.y < 0)
-    {
-      this.crashed = true; //height
+    if (this.pos.y > height || this.pos.y < 0 || this.pos.x > width || this.pos.x < 0) {
+      this.crashed = true;
     }
-
-    if (this.pos.x > width || this.pos.x < 0)
-    {
-      this.crashed = true; //width
-    }
-
 
     this.applyForce(this.dna.genes[count]);
 
-    // this.count++;
-
-    if (!this.completed && !this.crashed) //only update physics engine if goal incomplete
-    {
+    if (!this.completed && !this.crashed) {
       this.vel.add(this.acc);
-    this.pos.add(this.vel);
-    this.acc.mult(0);
-    this.vel.limit(4);
+      this.pos.add(this.vel);
+      this.acc.mult(0);
+      this.vel.limit(4);
     }
   }
 
+  // Display 
   this.show = function() {
     push();
     translate(this.pos.x, this.pos.y);
