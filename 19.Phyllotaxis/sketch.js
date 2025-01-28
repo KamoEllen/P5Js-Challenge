e3c4c6c61db230eaf8d60b92d24c23c7a109e7ed
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-29 00:14:57 +0200

    Update 19.Phyllotaxis , learnt the phyllotaxis ,added a dot with a specific angle andd radius

--- 19.Phyllotaxis/sketch.js
+++ 19.Phyllotaxis/sketch.js
@@ -1,7 +1,34 @@
+//angle = n * 137.5
+//angle will be the direction clockwise
+//r = c * root n
+//r is the radius
+//have to convert angle n r in (x,y)
+//using 'polar to cartesian coordinate transformation'
+// x = r * cos()
+// y = r * sin()
+// cos() = x/r
+//how to get n , what is n
+//n i snumber of dots
+//c is scaling pattern
+
+var n = 0;
+var c = 2;
+
 function setup() {
-    createCanvas(400, 200);
+    createCanvas(400, 400);
+    background(0);
   }
   
   function draw() {
-    background(51);
+    var a = n * 137.5;
+    var r = c * sqrt(n);
+
+    var x = r * cos(a) + width/2;
+    var y = r * sin(a) + height/2;
+
+    fill(255);
+    ellipse(x,y,8,8);
+
+    
+    n++;
   }
