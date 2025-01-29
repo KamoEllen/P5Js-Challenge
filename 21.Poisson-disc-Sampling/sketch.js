650efd4f311c79b4857f8adfc15c38c4313c4dae
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-29 13:35:17 +0200

    Update 21.Poisson-disc-Sampling , added random active point

--- 21.Poisson-disc-Sampling/sketch.js
+++ 21.Poisson-disc-Sampling/sketch.js
@@ -1,7 +1,9 @@
 var r = 10; //radius
 var k = 30; //constant k , limit
 var grid = [];
-var w = r / sqrt(2);
+// var w = r / sqrt(2);
+var w = r / Math.sqrt(2);
+var active = [];
 
 function setup() {
     createCanvas(640, 480);
@@ -11,15 +13,40 @@ function setup() {
     //step 0
     var cols = floor(width/w);
     var rows = floor(height/w);
-    for (vari = 0; i < cols * rows ; i++)
+    for (var  i = 0; i < cols * rows ; i++)
     {
         grid[i] = -1;
     }
 
+    //step 1
+    var x = random(width);
+    var y = random(height);
+    var i = floor(x/ w);
+    var j = floor(y / w);
+    var pos = createVector(x,y);
+    grid[i+j * cols] = pos;
+    active.push(pos);
 
+    
 
   }
   
-//   function draw() {
-//     background(0);
-//   }
+  function draw() {
+    background(0);
+    for ( var i = 0; i < grid.length; i++)
+    {
+        if ( grid[i]!= -1)
+        {
+            stroke(255);
+            strokeWeight(4);
+            point(grid[i].x, grid[i].y);
+        }
+    }
+
+    for ( var i = 0; i < active.length;  i++)
+    {
+        stroke(255, 0, 255);
+        strokeWeight(4);
+        point(active[i].x , active[i].y);
+    }
+  }
