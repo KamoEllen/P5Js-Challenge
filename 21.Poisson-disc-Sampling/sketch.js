75b84c61770ba8b8cdd6c6397c686cd5ff7db776
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-29 12:41:48 +0200

    Update 21.Poisson-disc-Sampling , added step 0

--- 21.Poisson-disc-Sampling/sketch.js
+++ 21.Poisson-disc-Sampling/sketch.js
@@ -1,12 +1,23 @@
+var r = 10; //radius
+var k = 30; //constant k , limit
+var grid = [];
+var w = r / sqrt(2);
+
 function setup() {
     createCanvas(640, 480);
     background(0);
     strokeWeight(4);
-    for ( var i = 0; i < 1000; i++)
+
+    //step 0
+    var cols = floor(width/w);
+    var rows = floor(height/w);
+    for (vari = 0; i < cols * rows ; i++)
     {
-        stroke(255);
-        point(random(width), random(height));
+        grid[i] = -1;
     }
+
+
+
   }
   
 //   function draw() {
