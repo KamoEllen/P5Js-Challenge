ac48433e3f4a9aec4c923485d5623920f8729696
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-29 20:33:09 +0200

    Update 21.Poisson-disc-Sampling

--- 21.Poisson-disc-Sampling/sketch.js
+++ 21.Poisson-disc-Sampling/sketch.js
@@ -4,9 +4,10 @@ var grid = [];
 // var w = r / sqrt(2);
 var w = r / Math.sqrt(2);
 var active = [];
-
+//points are in cells on the grid , find the one closest to the active poit 
+//by checking the contents of the closest cells
 function setup() {
-    createCanvas(640, 480);
+    createCanvas(409, 400);
     background(0);
     strokeWeight(4);
 
@@ -32,6 +33,67 @@ function setup() {
   }
   
   function draw() {
+
+    if (active.length > 0)
+    {
+        var randIndex = floor(random(active.length));
+
+        var pos = active[randIndex]; 
+        var found = false; 
+
+        for ( var n = 0; n < k; n++)
+        {
+            // var a = random(TWO_PI);
+
+            // var offsetX = cos(a); //with angle we can find size,position
+            // var offsetY = sin(a);
+
+            var sample = p5.Vector.random2D();
+            var  m = random(r, 2 * r);
+            sample.setMag(m);//set magnitud
+            // sample.setMag(m);
+            
+
+            sample.add(pos);
+
+            var col = floor(sample.x / w);
+            var rorw =floor(sample.y / w);
+
+            var ok = true; //distnce
+
+            for (var i = -1; i <= 1; i++)
+            {
+                for (var j = -1; j <= 1; j++)
+                {
+                    var neighbor = grid[i + j] * cols;
+                    var d = p5.Vector.dist(sample,neighbor);
+
+                    if ( d < r)
+                    {//if theres nothing there 
+                        ok = false;
+                       
+                    }
+                }
+            }
+
+
+        }
+        if (ok)
+        {
+            found = true;
+            grid[col + row * cols] = sample;
+            active.push(sample); // might use the point to pick a point next to it
+            // break;
+        }
+    }
+
+    if (!found)
+    {
+        active.splice(randIndex,1);
+    }
+
+
+
     background(0);
     for ( var i = 0; i < grid.length; i++)
     {
