8259e1da3b7b358a2e20c5944daa1e8ca9bec42f
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-29 11:30:40 +0200

    Update 20.Flappy-bird , added pipe function

--- 20.Flappy-bird/pipe.js
+++ 20.Flappy-bird/pipe.js
@@ -1,28 +1,31 @@
-function Pipe()
-{
-    this.top = randon(height/2);
-    this.bottom = randon(height/2);
+function Pipe() {
+    // Gap between top and bottom pipes
+    this.gap = 100; 
+    let gapPosition = random(this.gap, height - this.gap);
+
+    // Top pipe height
+    this.top = gapPosition - this.gap / 2;
+    // Bottom pipe height
+    this.bottom = height - (gapPosition + this.gap / 2);
+
+
     this.x = width;
-    this.w = 20;
+    this.w = 20; 
     this.speed = 5;
 
-    this.show = function()
-    {
+  
+    this.show = function () {
         fill(255);
-        
-        //top n bottom in same line vertically
-        //top section
+
+        // Top pipe
         rect(this.x, 0, this.w, this.top);
-        //space
-        //bottom section
-        rect(this.x, height-this.bottom, this.w, this.bottom);
 
-    }
-
-    this.update = function()
-    {
-        this.x -= this.speed
-    }
+        // Bottom pipe
+        rect(this.x, height - this.bottom, this.w, this.bottom);
+    };
 
 
+    this.update = function () {
+        this.x -= this.speed;
+    };
 }
--- 20.Flappy-bird/sketch.js
+++ 20.Flappy-bird/sketch.js
@@ -1,18 +1,25 @@
 var bird;
+var pipes = [];
 
 function setup() {
     createCanvas(600, 400);
     bird = new Bird();
+    pipes.push(new Pipe());
 }
 
 function draw() {
     background(51);
     bird.update();
     bird.show();
+
+    for (var i = 0; i < pipes.length; i++) {
+        pipes[i].show();
+        pipes[i].update();
+    }
 }
 
 function keyPressed() {
     if (key == ' ') { // if space bar was pressed
         bird.up();
     }
-}
+}    
