7750ef8fda4fb43bb0db0af5799d86189dfa0652
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-29 12:25:39 +0200

    Update 21.21.Poisson-disc-Sampling , 1000 random poins

--- 21.Poisson-disc-Sampling/sketch.js
+++ 21.Poisson-disc-Sampling/sketch.js
@@ -1,7 +1,14 @@
 function setup() {
     createCanvas(640, 480);
+    background(0);
+    strokeWeight(4);
+    for ( var i = 0; i < 1000; i++)
+    {
+        stroke(255);
+        point(random(width), random(height));
+    }
   }
   
-  function draw() {
-    background(0);
-  }
+//   function draw() {
+//     background(0);
+//   }
