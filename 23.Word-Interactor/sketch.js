533215cd4f011985c117570175a97ba543115ee6
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-31 00:04:24 +0200

    Update  23.Word-Interactor, added rule

--- 23.Word-Interactor/sketch.js
+++ 23.Word-Interactor/sketch.js
@@ -1,7 +1,25 @@
+var textfield;
+var output;
+var submit;
 function setup() {
-    createCanvas(400, 400);
+    // createCanvas(400, 400);
+    noCanvas();
+    textfield =select('#input');
+    output = select('#output');
+    submit = select('#submit');
+    submit.mousePressed(newText);
   }
+
+  function newText()
+{
+    var s = textfield.value();
+
+    var words = s.split(/\W+/); //anything thats not from a-z or 0-9
+    console.log(words);
+    // console.log(s);
+    createP(s);
+}
   
-  function draw() {
-    background(51);
-  }
+//   function draw() {
+//     background(51);
+//   }
