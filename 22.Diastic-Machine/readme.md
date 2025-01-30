a65383e74674dd673e17f59549d5ea9483f64577
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-30 23:32:48 +0200

    Update 22.Diastic-Machine

---
+++ 22.Diastic-Machine/readme.md
@@ -0,0 +1,2 @@
+Change value in index.html , right now its 'anger' change it to whatever n play 
+<input id = "seed" value = "anger"></input>
--- 22.Diastic-Machine/sketch.js
+++ 22.Diastic-Machine/sketch.js
@@ -1,7 +1,42 @@
-function setup() {
-    createCanvas(400, 400);
+function diastic(seed, words) {
+
+  var phrase = "";
+  var currentWord = 0;
+
+  for (var i = 0; i < seed.length; i++) {
+    var c = seed.charAt(i);
+
+    for (var j = currentWord; j < words.length; j++) {
+      if (words[j].charAt(i) == c) {
+        phrase += words[j];
+        phrase += " ";
+        currentWord = j + 1;
+        break;
+      }
+    }
+  }
+  return phrase;
 }
-    function draw() {
-      // background(0);
-    
-    }  
+
+var srctxt;
+var words;
+
+function preload() {
+  srctxt = loadStrings('bee-spider-frnds.txt');
+}
+
+function setup() {
+  noCanvas();
+  srctxt = join(srctxt, ' ');
+  words = splitTokens(srctxt, ' ,!.?');
+
+  var seed = select("#seed");
+  var submit = select("#submit");
+  submit.mousePressed(function() {
+    var phrase = diastic(seed.value(), words);
+    createP(phrase);
+  });
+
+
+
+}
