7af8f116b31c66d5b82498a8a58d49c7be9e0196
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-31 15:14:03 +0200

    Update 24.Purple-rain, couldnt see drops , fixed lie 13 & 14 , I have d.fall and d.show instead of drops[i]

--- 24.Purple-rain/sketch.js
+++ 24.Purple-rain/sketch.js
@@ -1,20 +1,16 @@
-let drops = new Array(100).fill(null).map(() => new Drop());
+let drops = [];
 
 function setup() {
     createCanvas(640, 360);
-    for (var i = 0; i < drops.length;i++)
-    {
-      drops[i] = new Drop();  
+    for (var i = 0; i < 100; i++) {
+        drops.push(new Drop());
     }
-    
 }
 
 function draw() {
     background(230, 230, 250);
-    for (var i = 0;i < drops.length; i++)
-    {
-        d.fall();
-    drops[i].fall();
-    drops[i].show();
-}
+    for (var i = 0; i < drops.length; i++) {
+        drops[i].fall();
+        drops[i].show();
+    }
 }
