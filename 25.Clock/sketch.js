1ad20217b9adc0ab3b52f17498434a88b3f43a3d
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-31 18:19:37 +0200

    Update 25.clock , added hands

--- 25.Clock/sketch.js
+++ 25.Clock/sketch.js
@@ -25,19 +25,37 @@ function draw() {
 
     //when second gets to 60 , minute goes up by 1
     //seconds
-    stroke(200,230,150);
-    let end1 = map(sc,0,60,0,360);
-    arc(0,0,150,150,0,end1);//0-360 , complete circle
+    stroke(100,230,150);
+    let secondAngle = map(sc,0,60,0,360);
+    arc(0,0,150,150,0,secondAngle);//0-360 , complete circle
+
+    push();
+    rotate(secondAngle);
+    stroke(100,230,150);
+    line(0,0,70,0);
+    pop();
 
     //minutes
     stroke(230,150,180);
-    let end2 = map(mn,0,60,0,360);
-    arc(0,0,289,280,0,end2);//0-360 , complete circle
+    let minAngle = map(mn,0,60,0,360);
+    arc(0,0,289,280,0,minAngle);//0-360 , complete circle
+
+    push();
+    rotate(minAngle);
+    stroke(230,150,180);
+    line(0,0,140,0);
+    pop();
 
     //hour
     stroke(150,100,255);
-    let end3 = map(hr %12,0,12,0,360);
-    arc(0,0,200,200,0,end3);//0-360 , complete circle
+    let hourAngle = map(hr %12,0,12,0,360);
+    arc(0,0,200,200,0,hourAngle);//0-360 , complete circle
+
+    push();
+    rotate(hourAngle);
+    stroke(150,100,255);
+    line(0,0,100,0);
+    pop();
 
 
 
