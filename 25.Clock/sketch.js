10b53197946b4e19cac1666a1014058c7d5d70f7
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-31 17:32:11 +0200

    Update 25.clock , added digital clock

--- 25.Clock/sketch.js
+++ 25.Clock/sketch.js
@@ -3,5 +3,13 @@ function setup() {
 }
 
 function draw() {
-    background(110, 230, 255);
+    background(0);                                           
+
+    let hr = hour();
+    let mn = minute();
+    let sc = second();
+
+    fill(255);
+    noStroke();
+    text(hr + ":" + mn  + ":" + sc , 10,200);
 }
