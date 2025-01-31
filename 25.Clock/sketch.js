204f33706d7770a9abb6a0b229138cfbae47c96b
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-31 18:11:18 +0200

    Updat 25.clock, map end of arc to sec,min and hour.Translate to center of canvas and rotated by -90 degrees

--- 25.Clock/sketch.js
+++ 25.Clock/sketch.js
@@ -5,6 +5,8 @@ function setup() {
 
 function draw() {
     background(0);  
+    translate(200,200);
+    rotate(-90);
     
     //displays corect time
 
@@ -25,17 +27,17 @@ function draw() {
     //seconds
     stroke(200,230,150);
     let end1 = map(sc,0,60,0,360);
-    arc(200,200,150,150,0,end1);//0-360 , complete circle
+    arc(0,0,150,150,0,end1);//0-360 , complete circle
 
     //minutes
     stroke(230,150,180);
     let end2 = map(mn,0,60,0,360);
-    arc(200,200,289,280,0,end2);//0-360 , complete circle
+    arc(0,0,289,280,0,end2);//0-360 , complete circle
 
     //hour
     stroke(150,100,255);
-    let end3 = map(hr,0,24,0,360);
-    arc(200,200,200,200,0,end3);//0-360 , complete circle
+    let end3 = map(hr %12,0,12,0,360);
+    arc(0,0,200,200,0,end3);//0-360 , complete circle
 
 
 
