2ce71c152c075ae320f74df077c9abffa01f3dd2
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-31 16:45:37 +0200

    Update 24.Purple-rain, added z value for 3d illusion

--- 24.Purple-rain/drop.js
+++ 24.Purple-rain/drop.js
@@ -4,8 +4,10 @@ class Drop {
     constructor() {
         this.x = random(width);
         this.y = random(-500,500);
-        this.yspeed = random(4,10);
-        this.len = random(10,20);
+        // this.yspeed = random(4,10);
+        this.z = random(0,20);
+        this.len = random(this.z,0,20,10,20); //drop size : long = close , short = far
+        this.yspeed = map(this.z,0,20,4,10);
     }
 
     fall() {
@@ -15,11 +17,15 @@ class Drop {
    if (this.y > height )
    {
     this.y = random(-200,-100);
-    this.yspeed = random(4,10);
+    // this.yspeed = random(4,10);
+    //speed is reset using z
+    this.yspeed = map(this.z,0,20,4,10);
    }
     }
 
     show() {
+        var thick = map(this.z,0,20,1,3); //20-thicer faster longer , 0- thinner shorter and slower
+        strokeWeight(thick);
      stroke(138,43,226);
      line(this.x, this.y, this.x, this.y + this.len);
     }
