0c6c346c7699431d85b2fef40e1bd7ab3eac54ea
KamoEllen <kamoellenkganakga@gmail.com>
2025-01-31 17:36:41 +0200

    Update 25.clock , added arc

--- 25.Clock/sketch.js
+++ 25.Clock/sketch.js
@@ -1,15 +1,27 @@
 function setup() {
     createCanvas(400, 360);
+    angleMode(DEGREES);
 }
 
 function draw() {
-    background(0);                                           
+    background(0);  
+    
+    //displays corect time
 
     let hr = hour();
     let mn = minute();
     let sc = second();
 
-    fill(255);
-    noStroke();
-    text(hr + ":" + mn  + ":" + sc , 10,200);
+    strokeWeight(8);
+    stroke(255);
+    noFill();
+    ellipse(200,200,200,200);
+
+    strokeWeight(4);
+    stroke(255,100,150);
+    arc(200,200,200,200,0,360);//0-360 , complete circle
+
+    // fill(255);
+    // noStroke();
+    // text(hr + ":" + mn  + ":" + sc , 10,200);
 }
