8d815af2c4cdbbdf6f531b96943bd475da04ea72
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-02 18:34:49 +0200

    Update 28.Simple-Particle-System, changed background and stroke color

--- 28.Simple-Particle-System/sketch.js
+++ 28.Simple-Particle-System/sketch.js
@@ -1,64 +1,47 @@
 let particles = [];
 function setup() {
-    createCanvas(600, 600);
-//    p = new Particle();
-//    let p = new Particle();
-//     particles.push(p);
-  }
-  
-  function draw() {
-    background(255,51,180);
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(51, 51, 180);
+
+  for (let i = 0; i < 5; i++) {
     let p = new Particle();
     particles.push(p);
-    // p.show();
-   for (let i = 0; i < particles.length;i++)
-    {
-        particles[i].update();
-       particles[i].show(); 
-
-       if (particles[i].finish())
-       {
-        //remove particle
-        particles.splice(i,1);
-
-       }
-       
-    } 
-    
   }
 
-  class Particle
-  {
-    constructor()
-    {
-        this.x = 300;
-        this.y = 380;
-        this.vx = random(-1,1);
-        this.vy = random(-5,-1);
-        this.alpha = 255;
+  for (let i = particles.length - 1; i >= 0; i--) {
+    particles[i].update();
+    particles[i].show();
+
+    if (particles[i].finish()) {
+      particles.splice(i, 1);
     }
-
-    finish()
-    {
-        return this.alpha < 0; //true or false response
-    }
-
-    update()
-    {
-        this.x += this.vx;
-        this.y += this.vy;
-        this.alpha -= 5; //fade effect
-
-    }
-    show()
-    {
-        stroke(255);
-        // noStroke();
-       fill(255,this.alpha); 
-       //particle 
-        ellipse(this.x , this.y , 16);
-
-
-    }
-   
   }
+}
+
+class Particle {
+  constructor() {
+    this.x = 300;
+    this.y = 380;
+    this.vx = random(-1, 1);
+    this.vy = random(-5, -1);
+    this.alpha = 255;
+  }
+
+  finish() {
+    return this.alpha < 0;
+  }
+
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.alpha -= 5;
+  }
+  show() {
+    stroke(100, 255, 40);
+    fill(100, this.alpha);
+    ellipse(this.x, this.y, 16);
+  }
+}
