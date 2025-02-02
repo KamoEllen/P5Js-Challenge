5129929ea384de479f62999e5d17d2917b580b40
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-02 13:51:06 +0200

    Update 27.Recursion, makes sierpinski triangle

--- 27.Recursion/sketch.js
+++ 27.Recursion/sketch.js
@@ -1,6 +1,6 @@
 // self reference
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(600, 600);
    
   }
   
@@ -16,9 +16,14 @@ function setup() {
     ellipse(x,y,z);
     if (z > 2)
     {
+        //right
       drawCircle(x+z * 0.5,y,z * 0.5); 
       //moving circle according to diameter shrnk size 
        // diameter shrinks [z * 0.5]
+       //left
+       drawCircle(x - z * 0.5,y,z * 0.5); 
+       //makes a sierpinski triangle.A fractal, or infinite pattern, that's shaped like an equilateral triangle.
+       drawCircle(x ,y  + z * 0.5,z * 0.5); 
     }
     
   }
