062693b7f62fcdc19864f3cbb9b62eaada272474
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-02 17:23:20 +0200

    Update 28.Simple-Particle-System , added circle

--- 28.Simple-Particle-System/sketch.js
+++ 28.Simple-Particle-System/sketch.js
@@ -1,8 +1,27 @@
+let p;
 function setup() {
     createCanvas(600, 600);
-   
+   p = new Particle();
   }
   
   function draw() {
     background(255,51,180);
+    p.show();
+  }
+
+  class Particle
+  {
+    constructor()
+    {
+        this.x = 300;
+        this.y = 380;
+    }
+    show()
+    {
+        stroke(255);
+       fill(255,10); 
+        ellipse(this.x , this.y , 46);
+
+
+    }
   }
