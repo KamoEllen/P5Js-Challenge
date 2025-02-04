126b376495e5e2a7de718325d1e1b1719e3c7b90
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-04 23:38:23 +0200

    Update 29.3D-Knots, changed bg

--- 29.3D-Knots/sketch.js
+++ 29.3D-Knots/sketch.js
@@ -3,5 +3,21 @@ function setup() {
 }
 
 function draw() {
-  background(74, 100, 20);
+  background(90, 90, 229);
+
+  var beta = 0;
+
+  beginShape();
+  while (beta < PI)
+  {
+    var r = 0.8 + 1.6 * sin(6  * beta);
+    var theta = 2 * beta;
+    var phi = 0.6 * PI * sin(12 * beta);
+    var x = r * cos(phi) * cos(theta);
+    var y = r * cos(phi) * sin(theta);
+    var z = r * sin(phi);
+
+    beta +=0.1;
+  }
+  endShape();
 }
