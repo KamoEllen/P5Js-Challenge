92412d73881def42a7a3d2611ccdc1c5079b8b77
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-05 18:13:56 +0200

    Update 30.Langtons-Ant,added turnRight() and turnLeft() functions and moveForward()

--- 30.Langtons-Ant/sketch.js
+++ 30.Langtons-Ant/sketch.js
@@ -1,12 +1,56 @@
 let grid;
 let x;
 let y;
+let dir;
+
+const ANTUP = 0;
+const ANTRIGHT = 1;
+const ANTDOWN = 2;
+const ANTLEFT = 3;
 
 function setup() {
   createCanvas(400, 400);
   grid = make2DArray(width, height);
   x = width / 2;
   y = height / 2;
+  dir = ANTUP;
+}
+
+function turnRight() {
+  dir++;
+  if (dir > ANTLEFT) {
+    dir = ANTUP;
+  }
+}
+
+function turnLeft() {
+  dir--;
+  if (dir < ANTUP) {
+    dir = ANTLEFT;
+  }
+}
+
+function moveForward() {
+  if (dir == ANTUP) {
+    y--;
+  } else if (dir == ANTRIGHT) {
+    x++;
+  } else if (dir == ANTDOWN) {
+    y++;
+  } else if (dir == ANTLEFT) {
+    x--;
+  }
+  
+  if (x > width - 1) {
+    x = 0;
+  } else if (x < 0) {
+    x = width - 1;
+  }
+  if (y > height - 1) {
+    y = 0;
+  } else if (y < 0) {
+    y = height - 1;
+  }
 }
 
 function draw() {
@@ -14,6 +58,7 @@ function draw() {
   stroke(0);
   strokeWeight(1);
   point(x, y);
+  moveForward();
 }
 
 function make2DArray(cols, rows) {
