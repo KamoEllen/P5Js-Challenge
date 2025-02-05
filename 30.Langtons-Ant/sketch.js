9953f96fdf961c7d8ef966a7b311e8b711f779a1
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-05 18:07:53 +0200

    Update 30.Langtons-Ant,added 2D array for grid storage

--- 30.Langtons-Ant/sketch.js
+++ 30.Langtons-Ant/sketch.js
@@ -1,8 +1,28 @@
+let grid;
+let x;
+let y;
+
 function setup() {
-    createCanvas(600, 600);
-   
+  createCanvas(400, 400);
+  grid = make2DArray(width, height);
+  x = width / 2;
+  y = height / 2;
+}
+
+function draw() {
+  background(220);
+  stroke(0);
+  strokeWeight(1);
+  point(x, y);
+}
+
+function make2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = 0;
+    }
   }
-  
-  function draw() {
-    background(100,255,180);
-  }
+  return arr;
+}
