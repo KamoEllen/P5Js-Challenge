341c74f7f8cac0dd6ca206f9b5ec884ce127875c
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-07 23:32:14 +0200

    Update 34.Binary-Search-Tree, added root with left and right nodes

--- 34.Binary-Search-Tree/sketch.js
+++ 34.Binary-Search-Tree/sketch.js
@@ -1,8 +1,32 @@
+var tree;
 
 function setup() {
     createCanvas(200, 200);
-  }
-  
-  function draw() {
+    tree = new Tree();
+    var n = new Node(5);
+    tree.addNode(n);
+}
+
+class Tree {
+    constructor() {
+        this.root = null; //with reference to root, can find any node
+    }
+
+    addNode(n) {
+        if (this.root === null) {
+            this.root = n;
+        }
+    }
+}
+
+class Node {
+    constructor(val) {
+        this.value = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function draw() {
     background(0);
-  }
+}
