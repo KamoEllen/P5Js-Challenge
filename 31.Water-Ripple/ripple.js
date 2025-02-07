cc4ce066138590e45d85712c33d353fae2153c8d
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-07 12:46:41 +0200

    Update 31.Water-Ripple, added white ripples that expand and fade out that work with clicks or drag

--- 31.Water-Ripple/ripple.js
+++ 31.Water-Ripple/ripple.js
@@ -1,15 +1,24 @@
 class Ripple {
     constructor(x, y) {
-
+        this.x = x;
+        this.y = y;
+        this.radius = 0;
+        this.opacity = 1;
     }
 
     update() {
-
+        this.radius += 3;
+        this.opacity -= 0.03;
     }
 
     show() {
-
+        stroke(255, 255, 255, this.opacity * 255);
+        strokeWeight(2);
+        noFill();
+        circle(this.x, this.y, this.radius * 2);
     }
 
-
+    isFinished() {
+        return this.opacity <= 0;
+    }
 }
--- 31.Water-Ripple/sketch.js
+++ 31.Water-Ripple/sketch.js
@@ -1,9 +1,28 @@
+let ripples = [];
+
 function setup() {
-    createCanvas(600, 400, WEBGL);
+    createCanvas(400, 400);
+    background(0);
 }
 
 function draw() {
-    background(20, 30, 50);
-    ripples[i].update();
-    ripples[i].show();
+    background(0,20, 220); //blue
+    
+   
+    for (let i = ripples.length - 1; i >= 0; i--) {
+        ripples[i].update();
+        ripples[i].show();
+        
+        if (ripples[i].isFinished()) {
+            ripples.splice(i, 1);
+        }
+    }
+}
+
+function mousePressed() {
+    ripples.push(new Ripple(mouseX, mouseY));
+}
+
+function mouseDragged() {
+    ripples.push(new Ripple(mouseX, mouseY));
 }
