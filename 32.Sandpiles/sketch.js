9d54ee08d58e97d9d655940aed3295aed7879695
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-07 15:03:13 +0200

    Update 32.Sandpiles , 2D arrays for current and next sandpile states and added sandpile in center

--- 32.Sandpiles/sketch.js
+++ 32.Sandpiles/sketch.js
@@ -1,10 +1,19 @@
 let sandpiles;
+let nextpiles;
+
 function setup() {
-    createCanvas(400, 400);
-   
-    sandpiles = new Array(width).fill().map(() => new Array(height).fill(0));
+  createCanvas(600, 600);
+  pixelDensity(1);
+  
+  sandpiles = new Array(width).fill().map(i => new Array(height).fill(0));
+  nextpiles = new Array(width).fill().map(i => new Array(height).fill(0));
+  
+  
+  sandpiles[width/2][height/2] = 1000000000;
+  
+  background(255);
 }
 
 function draw() {
-    background(90,40, 120); //blue
+
 }
