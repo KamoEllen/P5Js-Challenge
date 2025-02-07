0394e31fac05310c0fa0aaac5128222856807ded
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-07 15:24:03 +0200

    Update 32.Sandpiles , added render function and color palette for different sand levels

--- 32.Sandpiles/sketch.js
+++ 32.Sandpiles/sketch.js
@@ -1,3 +1,11 @@
+let defaultColor = [255, 0, 0];
+let colors = [
+  [0, 0, 0],   // black for 0
+  [0, 185, 63],    // green for 1
+  [0, 104, 255],   // blue for 2
+  [122, 0, 229]    // purple for 3
+];
+
 let sandpiles;
 let nextpiles;
 
@@ -8,12 +16,31 @@ function setup() {
   sandpiles = new Array(width).fill().map(i => new Array(height).fill(0));
   nextpiles = new Array(width).fill().map(i => new Array(height).fill(0));
   
-  
   sandpiles[width/2][height/2] = 1000000000;
   
-  background(255);
+  background(defaultColor[0], defaultColor[1], defaultColor[2]);
+}
+
+function render() {
+  loadPixels();
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      let num = sandpiles[x][y];
+      let col = defaultColor;
+      if (num == 0) col = colors[0];
+      else if (num == 1) col = colors[1];
+      else if (num == 2) col = colors[2];
+      else if (num == 3) col = colors[3];
+
+      let pix = (x + y * width) * 4;
+      pixels[pix] = col[0];
+      pixels[pix + 1] = col[1];
+      pixels[pix + 2] = col[2];
+    }
+  }
+  updatePixels();
 }
 
 function draw() {
-
+  render();
 }
