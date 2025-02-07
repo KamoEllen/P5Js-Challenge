1cdac369ef0648e0f8c83a8c3bf33d98a5aa46f7
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-07 20:01:00 +0200

    Update 33.Animated-Sprites , loading images from the assets folder

--- 33.Animated-Sprites/sketch.js
+++ 33.Animated-Sprites/sketch.js
@@ -1,7 +1,11 @@
 let animation = [];
-let horses = [];
 
-function preload() {}
+function preload() {
+  for (let i = 0; i <= 6; i++) {
+    let img = loadImage(`./assets/ice-horse-run-0${i}.png`);
+    animation.push(img);
+  }
+}
 
 function setup() {
   createCanvas(640, 480);
@@ -9,4 +13,6 @@ function setup() {
 
 function draw() {
   background(0);
+  //this is to show first frame of the animation
+  image(animation[0], 0, 0);
 }
