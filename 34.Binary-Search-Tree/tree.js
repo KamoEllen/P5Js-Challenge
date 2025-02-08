4995b48c52ae2ad71717c839997a8bf8f26b60bf
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-08 09:33:06 +0200

    Update 34.Binary-Search-Tree, added tree class with methods for searching, traversing, and adding nodes

---
+++ 34.Binary-Search-Tree/tree.js
@@ -0,0 +1,25 @@
+function Tree() {
+    this.root = null;
+  }
+  
+  Tree.prototype.traverse = function() {
+    if (this.root != null) {
+      this.root.visit(this.root);
+    }
+  };
+  
+  Tree.prototype.search = function(val) {
+    if (this.root != null) {
+      return this.root.search(val);
+    }
+    return null;
+  };
+  
+  Tree.prototype.addValue = function(val) {
+    var n = new Node(val);
+    if (this.root == null) {
+      this.root = n;
+    } else {
+      this.root.addNode(n);
+    }
+  };
