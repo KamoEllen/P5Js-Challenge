842f84fa91203a36d348d9c27b51e534754b3ca6
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-08 14:17:18 +0200

    Update yoff

--- 35.Butterfly/sketch.js
+++ 35.Butterfly/sketch.js
@@ -1,43 +1,50 @@
 function setup() {
   createCanvas(400, 400);
-  background(100, 200, 30);
-  translate(width / 2, height / 2); // everythin relative to center
-
-  var r = 100;
-  stroke(255);
-  fill(200, 50);
-  strokeWeight(1);
-  var delta_angle = PI / 100;
-  var xoff = 0;
-  //right wing
-  beginShape();
-  for (var a = -PI / 2; a <= PI / 2; a += delta_angle) { //fix 
-    var n = noise(xoff);
-    // var r = map(n, 0, 1, 50, 100); //noise
-    var r = sin(2*a) * map(n, 0, 1, 50, 100);
-    var x = r * cos(a);
-    var y = r * sin(a);
-    xoff += 0.1;
-    vertex(x, y);
-  }
-  //want to make left and right wing combine , theres spae seperating them
-//   endShape();
-  //left wing
-//   var xoff = 0; //fix
-//   beginShape();
-  for (
-    var a = PI / 2;
-    a < (3 * PI) / 2;
-    a += delta_angle 
-  ) {
-    var n = noise(xoff);
-    // var r = map(n, 0, 1, 50, 100);
-    var r = sin(2*a) *  map(n, 0, 1, 50, 100);;
-    var x = r * cos(a);
-    var y = r * sin(a);
-    xoff -= 0.1;
-
-    vertex(x, y);
-  }
-  endShape();
 }
+
+  function draw()
+  {
+
+    background(100, 200, 30);
+    translate(width / 2, height / 2); // everythin relative to center
+      rotate(PI/2);
+  //   var r = 100;
+    stroke(255);
+    fill(200, 50);
+    strokeWeight(1);
+    var delta_angle = PI / 100;
+    var dx= 0.5; //noise move 
+    var xoff = 0;
+    //right wing
+    beginShape();
+    for (var a = -PI / 2; a <= PI / 2; a += delta_angle) { //fix 
+      var n = noise(xoff);
+      // var r = map(n, 0, 1, 50, 100); //noise
+      var r = sin(2*a) * map(n, 0, 1, 50, 100);
+      var x = r * cos(a);
+      var y = r * sin(a);
+      xoff += dx;
+      vertex(x, y);
+    }
+    //want to make left and right wing combine , theres spae seperating them
+  //   endShape();
+    //left wing
+  //   var xoff = 0; //fix
+  //   beginShape();
+    for (
+      var a = PI / 2;
+      a < (3 * PI) / 2;
+      a += delta_angle 
+    ) {
+      var n = noise(xoff);
+      // var r = map(n, 0, 1, 50, 100);
+      var r = sin(2*a) *  map(n, 0, 1, 50, 100);;
+      var x = r * cos(a);
+      var y = r * sin(a);
+      xoff -= dx;
+  
+      vertex(x, y);
+    }
+    endShape();
+    
+  }
