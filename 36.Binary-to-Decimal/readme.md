701361e0422a3a6e43a4c12b83082917b1abc593
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-08 23:10:45 +0200

    Update 36.Binary-to-Decimal, added binaryToDecimal function

---
+++ 36.Binary-to-Decimal/readme.md
@@ -0,0 +1,11 @@
+base-number of possibilities in a number system
+base 2 : 0 & 1
+base 10 : 0-9
+base 16: 0-9 and a-f
+
+Example of base 16
+#FF00FF   (red , green,blue  )
+ff is 255 in decimal
+00 is 0 in decimal
+
+100 <- each digit is a bit, when u have 8 its called a byte
--- 36.Binary-to-Decimal/sketch.js
+++ 36.Binary-to-Decimal/sketch.js
@@ -1,7 +1,19 @@
+let num = "11111111";
 function setup() {
     createCanvas(400, 400);
+    binaryToDecimal(num);
   }
   
   function draw() {
-    background(200, 200, 30); //yellow 
+    // background(200, 200, 30); //yellow 
+  }
+  function binaryToDecimal(val)
+  {
+    let sum = 0;
+    for (let i =0; i < val.length; i++)
+    {
+        let bit = val.charAt(val.length-i-1);
+        sum += parseInt(bit) * pow(2, i);
+    }
+    console.log(sum);
   }
