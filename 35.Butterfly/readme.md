baf2571faf3aaf0c9b02dd8b9658f06d9e769533
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-08 12:56:15 +0200

    Update 25.Butterfly, added delta angle

--- 35.Butterfly/readme.md
+++ 35.Butterfly/readme.md
@@ -1,4 +1,4 @@
 polar coordinates
 x = r*cos(-)
 y = r * sin(-)
-perline noise- algorithm piks random numbers based on previous random number
+Perlin noise- algorithm piks random numbers based on previous random number
--- 35.Butterfly/sketch.js
+++ 35.Butterfly/sketch.js
@@ -4,23 +4,47 @@ function setup() {
     translate(width/2, height/2);// everythin relative to center
 
     var r = 100;
-    beginShape();
+    stroke(255);
+            fill(200,50);
+            strokeWeight(1);
+    var delta_angle = PI/100;
     var xoff= 0; 
-    for (var a = -PI/2 ; a < PI/2 ; a+= 0.1 )
+    //right wing
+    beginShape();
+    for (var a = -PI/2 ; a < PI/2 ; a+= delta_angle ) //3
     {
         var n = noise(xoff);
        var r = map(n,0,1,50,125);
         var x = r * cos(a);
         var y = r * sin(a);
-        xoff +=0.2;
-        stroke(255);
-        fill(200,50);
-        strokeWeight(1);
+        xoff +=0.1; //1
+        // stroke(255);
+        // fill(200,50);
+        // strokeWeight(1);
         // point(x,y);
         vertex(x,y);
         // r--; //making wing
+        
     }
     endShape();
+        //left wing
+        beginShape();
+        for (var a = -PI/2 ; a > -3*PI/2; a -= delta_angle ) //3
+        {
+            var n = noise(xoff); //n,0,1,50,125
+           var r = map(n,0,1,50,125);
+            var x = r * cos(a);
+            var y = r * sin(a);
+            xoff +=0.1; //1
+            
+            // point(x,y);
+            vertex(x,y);
+            // r--; //making wing
+            
+        }
+        endShape();
+       
+   
 }
 // function draw()
 // {
