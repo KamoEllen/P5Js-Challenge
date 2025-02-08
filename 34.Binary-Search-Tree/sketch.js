0bde6d4c8933c8c446fef9828c643182f0907c56
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-08 09:24:28 +0200

    Update 34.Binary-Search-Tree,added node class with functions for searching, visiting, and adding nodes

--- 34.Binary-Search-Tree/sketch.js
+++ 34.Binary-Search-Tree/sketch.js
@@ -1,32 +1,4 @@
-var tree;
-
 function setup() {
-    createCanvas(200, 200);
-    tree = new Tree();
-    tree.addValue();
-    // tree.addNode(new Node(5));
-}
-
-class Tree {
-    constructor() {
-        this.root = null; //with reference to root, can find any node
-    }
-
-    addNode(n) {
-        if (this.root === null) {
-            this.root = n;
-        }
-    }
-}
-
-class Node {
-    constructor(val) {
-        this.value = val;
-        this.left = null;
-        this.right = null;
-    }
-}
-
-function draw() {
-    background(0);
-}
+    createCanvas(600, 400);
+    background(51);
+  }
