da230150bf64a6474c4fdc282a20afc92beb8f14
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-08 09:57:12 +0200

    Update 34.Binary-Search-Tree, updated visit methodto visualize tree nodes with text and circles , added lines to connect parent and child node

--- 34.Binary-Search-Tree/node.js
+++ 34.Binary-Search-Tree/node.js
@@ -1,44 +1,31 @@
-function Node(val, x, y) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
-    this.x = x;
-    this.y = y;
-  }
-  
-  Node.prototype.search = function(val) {
-    if (this.value == val) {
-      return this;
-    } else if (val < this.value && this.left != null) {
-      return this.left.search(val);
-    } else if (val > this.value && this.right != null) {
-      return this.right.search(val);
-    }
-    return null;
-  };
-  
-  Node.prototype.visit = function(parent) {
+Node.prototype.visit = function(parent) {
     if (this.left != null) {
       this.left.visit(this);
     }
     console.log(this.value);
+    fill(255);
+    noStroke();
+    textAlign(CENTER);
+    text(this.value, this.x, this.y);
+    stroke(255);
+    noFill();
+    ellipse(this.x, this.y, 20, 20);
+    if (parent != null) {
+      line(parent.x, parent.y, this.x, this.y);
+    }
     if (this.right != null) {
       this.right.visit(this);
     }
   };
   
-  Node.prototype.addNode = function(n) {
-    if (n.value < this.value) {
-      if (this.left == null) {
-        this.left = n;
-      } else {
-        this.left.addNode(n);
-      }
-    } else if (n.value > this.value) {
-      if (this.right == null) {
-        this.right = n;
-      } else {
-        this.right.addNode(n);
-      }
+  // Tree.js (updated addValue method)
+  Tree.prototype.addValue = function(val) {
+    var n = new Node(val);
+    if (this.root == null) {
+      this.root = n;
+      this.root.x = width / 2;
+      this.root.y = 16;
+    } else {
+      this.root.addNode(n);
     }
   };
