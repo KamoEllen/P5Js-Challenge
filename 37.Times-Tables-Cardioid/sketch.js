a520913e8df353d04347c1dbff9e68bc1f6c43c8
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-09 09:23:45 +0200

    Update 37.Times-Tables-Cardioid,  mapped poin index to an angle , used polar coordinates to get point location, draw the points,added comments

--- 37.Times-Tables-Cardioid/sketch.js
+++ 37.Times-Tables-Cardioid/sketch.js
@@ -1,6 +1,26 @@
+let total = 10;
+
 function setup() {
     createCanvas(200, 200);
-  }
-  function draw() {
+}
+
+function draw() {
     background(51);
-  }
+    var r = width / 2; // radius
+    translate(width / 2, height / 2); // origin is moved from 0,0 to the center of the canvas
+
+    // main circle with no stroke
+    noFill();
+    // stroke(255);
+    ellipse(0, 0, r * 2);
+
+    // Draw 10 points around the circumference of the main circle
+    for (var i = 0; i < total; i++) {
+        var angle = map(i, 0, total, 0, TWO_PI);
+        var x = r * cos(angle);
+        var y = r * sin(angle);
+        fill(255);
+        noStroke();
+        ellipse(x, y, 10); //small circle at each point
+    }
+}
