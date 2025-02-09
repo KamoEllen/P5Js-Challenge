1a1b36a5785dad5f13427a7acab6ece2fefe8ff2
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-09 14:49:13 +0200

    Update  39.Heart-Curve-GIF-Loop , added code from previous projet file 38.Heart-curve sketch.js

--- 39.Heart-Curve-GIF-Loop/sketch.js
+++ 39.Heart-Curve-GIF-Loop/sketch.js
@@ -1,7 +1,32 @@
+const heart = [];
+let a = 0;
+
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(50, 50, 20); //keeping bg color
+  translate(width / 2, height / 2); 
+
+  stroke(255);
+  strokeWeight(2);
+  fill(255, 30, 30); //red
+
+  beginShape();
+  for (let v of heart) {
+    vertex(v.x, v.y);
   }
-  
-  function draw() {
-    background(50, 50, 20);
+  endShape();
+
+  const r = height / 40;
+  const x = r * 16 * pow(sin(a), 3);
+  const y = -r * (13 * cos(a) - 5 * cos(2 * a) - 2 * cos(3 * a) - cos(4 * a));
+  heart.push(createVector(x, y));
+
+  if (a > TWO_PI) {
+    noLoop();
   }
+
+  a += 0.01;
+}
