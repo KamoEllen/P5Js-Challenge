2ecd0343c28973178aa0544826d30702e655a24c
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-09 14:36:18 +0200

    Update 38.Heart-curve, scaled heart , animated the heart

--- 38.Heart-curve/sketch.js
+++ 38.Heart-curve/sketch.js
@@ -1,22 +1,32 @@
-function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
-    background(20, 10, 130);
-    translate(width/2, height/2); //polar coordinates
+const heart = [];
+let a = 0;
 
-    noFill();
-    stroke(255);
-    beginShape();
-    for (var a = 0; a < TWO_PI; a+=0.01)
-    {
-        //equation for heart
-        var r = 10;
-        var x = r * 16 * pow(sin(a),3) ;
-        
-        var y = -r * (13 * cos(a)  - 5* cos(2 * a) - 2 * cos(3 * a) - cos(4* a));
-        vertex(x,y);
-    }
-    endShape();
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(20, 10, 130);
+  translate(width / 2, height / 2); //polar coordinates
+
+  stroke(255);
+  strokeWeight(2);
+  fill(255, 30, 30); //red
+
+  beginShape();
+  for (let v of heart) {
+    vertex(v.x, v.y);
   }
+  endShape();
+
+  const r = height / 40;
+  const x = r * 16 * pow(sin(a), 3);
+  const y = -r * (13 * cos(a) - 5 * cos(2 * a) - 2 * cos(3 * a) - cos(4 * a));
+  heart.push(createVector(x, y));
+
+  if (a > TWO_PI) {
+    noLoop();
+  }
+
+  a += 0.01;
+}
