a88f460d90a7c978dcf41e4fee10e008fc349667
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-09 09:33:39 +0200

    Update 37.Times-Tables-Cardioid, added getVector function

--- 37.Times-Tables-Cardioid/sketch.js
+++ 37.Times-Tables-Cardioid/sketch.js
@@ -1,12 +1,23 @@
 let total = 10;
+var r;
 
+//pvector -obj that has and stores x and y
+//can make x and y components from angle and scale by multiplying by radius
 function setup() {
     createCanvas(200, 200);
+    r = width / 2; // radius
 }
+
+function getVector(index) {
+    const angle = map(index, 0, total, 0, TWO_PI);
+    const v = p5.Vector.fromAngle(angle);
+    v.mult(r);
+    return v;
+  }
 
 function draw() {
     background(51);
-    var r = width / 2; // radius
+   
     translate(width / 2, height / 2); // origin is moved from 0,0 to the center of the canvas
 
     // main circle with no stroke
@@ -22,5 +33,12 @@ function draw() {
         fill(255);
         noStroke();
         ellipse(x, y, 10); //small circle at each point
+    }
+
+    for (var i = 0; i < total; i++)
+    {
+        //a point connects to b
+        const a = getVector(i, total);
+        const b = getVector(i * factor, total);
     }
 }
