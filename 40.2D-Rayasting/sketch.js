6e3594236db4ce16af660e3369cdecd3d63d57e7
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-09 17:43:46 +0200

    Update 40.2D-Rayasting , added ray and boundry class

--- 40.2D-Rayasting/sketch.js
+++ 40.2D-Rayasting/sketch.js
@@ -1,7 +1,14 @@
+let wall;
+let ray;
+
 function setup() {
     createCanvas(400, 400);
+    wall = new Boundary(300,100,300,200);
+    ray = new Ray(40,100);
   }
   
   function draw() {
-    background(30, 200, 180); 
+    background(0); 
+    wall.show();
+    ray.show();
   }
