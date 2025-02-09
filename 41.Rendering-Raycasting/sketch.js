f6db2cbfa9b9ef80e611d608ec05cd5a28c8b0ec
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-09 20:41:29 +0200

    Update 41.Rendering-Raycasting , added a 2D scene rendering based on ray distances.

--- 41.Rendering-Raycasting/sketch.js
+++ 41.Rendering-Raycasting/sketch.js
@@ -14,10 +14,14 @@ function setup() {
     let y2 = random(height);
     walls[i] = new Boundary(x1, y1, x2, y2);
   }
-  walls.push(new Boundary(-1, -1, width, -1));
-  walls.push(new Boundary(width, -1, width, height));
-  walls.push(new Boundary(width, height, -1, height));
-  walls.push(new Boundary(-1, height, -1, -1));
+  // walls.push(new Boundary(-1, -1, width, -1));
+  // walls.push(new Boundary(width, -1, width, height));
+  // walls.push(new Boundary(width, height, -1, height));
+  // walls.push(new Boundary(-1, height, -1, -1));
+  walls.push(new Boundary(0, 0, sceneW, 0));
+  walls.push(new Boundary(sceneW, 0, sceneW, sceneH));
+  walls.push(new Boundary(sceneW, sceneH, 0, sceneH));
+  walls.push(new Boundary(0, sceneH, 0, 0));
   particle = new Particle();
   sliderFOV = createSlider(0, 360, 45);
   sliderFOV.input(changeFOV);
@@ -37,6 +41,7 @@ function changeFOV() {
 
 function draw() {
   background(0);
+  //using keys to move
   if (keyIsDown(LEFT_ARROW)) {
     particle.rotate(-0.1);
   } else if (keyIsDown(RIGHT_ARROW)) {
@@ -57,4 +62,19 @@ function draw() {
 
   xoff += 0.01;
   yoff += 0.01;
+  const scene = particle.look(walls);
+  const w = width / scene.length;
+  push();
+  translate(width, 0);
+  for (let i = 0; i < scene.length; i++) {
+    noStroke();
+    const sq = scene[i] * scene[i];
+    const wSq = width * width;
+    const b = map(sq, 0, wSq, 255, 0);
+    const h = map(scene[i], 0, width, height, 0);
+    fill(b);
+    rectMode(CENTER);
+    rect(i * w + w / 2, height / 2, w + 1, h);
+  }
+  pop();
 }
