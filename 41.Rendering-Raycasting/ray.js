e2f6f696623c5f4177acf81836d8ede8e602ecb9
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-09 19:07:18 +0200

    Added 40.Rendering-Raycasting , added code from previous project 40.2D-Raycasting

--- 41.Rendering-Raycasting/ray.js
+++ 41.Rendering-Raycasting/ray.js
@@ -34,12 +34,11 @@ class Ray {
         return;
       }
   
-      //formula for t and u
+     
       const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
       const u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
 
-      //if lines are paralled , intersection  or no intersection
-      if (t > 0 && t < 1 && u > 0) {
+            if (t > 0 && t < 1 && u > 0) {
         const pt = createVector();
         pt.x = x1 + t * (x2 - x1);
         pt.y = y1 + t * (y2 - y1);
