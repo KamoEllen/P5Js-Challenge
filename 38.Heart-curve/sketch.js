629d81b910d35cc4e6392e60f503f4c9de2a3e95
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-09 14:08:05 +0200

    Update 38.Heart-curve, made cirle using polar coordinates

--- 38.Heart-curve/sketch.js
+++ 38.Heart-curve/sketch.js
@@ -4,4 +4,17 @@ function setup() {
   
   function draw() {
     background(20, 10, 130);
+    translate(width/2, height/2); //polar coordinates
+
+    noFill();
+    stroke(255);
+    beginShape();
+    for (var a = 0; a < TWO_PI; a+=0.01)
+    {
+        var r = 100;
+        var x = r * cos(a);
+        var y = r * sin(a);
+        vertex(x,y);
+    }
+    endShape();
   }
