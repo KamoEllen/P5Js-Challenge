adbf50ad0978dab0f198e9a27d31a12d302682b2
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-10 12:30:48 +0200

    Update 43.Seven-Segment , removed fill and changed width and height of each segment

--- 43.Seven-Segment/sketch.js
+++ 43.Seven-Segment/sketch.js
@@ -9,20 +9,16 @@ function draw()
 }
 function sevenSegment()
 { 
-    //draw segments relative to point
+    noFill();
     push();
-    fill(0);
+    // fill(0);
     stroke(255);
-    rect(40,30,100,20); //a
-    // stroke(255);
-    rect(140,50,20,100); //b
-    // stroke(255);
-    rect(140,160,20,100); //c
-    rect(40,260,100,20); //d
-    rect(20,160,20,100); //e
-    rect(20,50,20,100); //b
-    rect(40,145,100,20); //g
-   
-
+    rect(40,30,90,10); //a
+    rect(140,50,10,90); //b
+    rect(140,160,10,90); //c
+    rect(40,260,90,10); //d
+    rect(20,160,10,90); //e
+    rect(20,50,10,90); //b
+    rect(40,145,90,10); //g
     pop();
 }
