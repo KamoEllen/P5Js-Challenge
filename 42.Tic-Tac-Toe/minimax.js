f340f608275f2855719a57198658b7b51ce60326
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-10 10:39:35 +0200

    Update 42.Tic-Tac-Toe , added functions and few comments

--- 42.Tic-Tac-Toe/minimax.js
+++ 42.Tic-Tac-Toe/minimax.js
@@ -1,32 +1,63 @@
-function bestMove()
-{
-    // let available = [];
+function bestMove() {
     let bestScore = -Infinity;
-    let bestMove;
-    for (let i = 0; i< 3;i++)
-    {
-for (let j = 0; j < 3; j++)
-{
-    //is there a spot available
-    if (boar[i][j] == '')
-    {
-        board[i][j] = ai;
-       let score = minimax[board];
-       board[i][j] = '';
-       if (score > bestScore)
-       {
-        bestScore = score;
-        bestMove = {i,j};
-       }
+    let move;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] == '') {
+          board[i][j] = ai;
+          let score = minimax(board, 0, false);
+          board[i][j] = '';
+          if (score > bestScore) {
+            bestScore = score;
+            move = { i, j };
+          }
+        }
+      }
     }
-    // if (board[i][j] == '')
-    // {
-    //     available.push({i,j});
-    // }
-}
-    }
-
-    // let move = random(available);
-    board[bestMove.i][bestMove.j] = ai;
+    board[move.i][move.j] = ai;
     currentPlayer = human;
-}
+  }
+  
+  //3 types
+  let scores = {
+    X: 10,
+    O: -10,
+    tie: 0
+  };
+  
+  //terminal condition ? or check other possible moves
+  function minimax(board, depth, isMaximizing) {
+    let result = checkWinner();
+    if (result !== null) {
+      return scores[result];
+    }
+  
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (board[i][j] == '') {
+            board[i][j] = ai;
+            let score = minimax(board, depth + 1, false);
+            board[i][j] = '';
+            bestScore = max(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (board[i][j] == '') {
+            board[i][j] = human;
+            let score = minimax(board, depth + 1, true);
+            board[i][j] = '';
+            bestScore = min(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    }
+  }
+  
