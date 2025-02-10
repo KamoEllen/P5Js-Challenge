60c1296bac4bd2fa3ba99ff2a59e7cf890aae8d7
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-10 12:28:00 +0200

    Update 43.Seven-Segment , displayed number 8 using 7 segments

--- 43.Seven-Segment/sketch.js
+++ 43.Seven-Segment/sketch.js
@@ -5,4 +5,24 @@ function setup() {
 function draw()
 {
     background(51,88,94);
+    sevenSegment();
+}
+function sevenSegment()
+{ 
+    //draw segments relative to point
+    push();
+    fill(0);
+    stroke(255);
+    rect(40,30,100,20); //a
+    // stroke(255);
+    rect(140,50,20,100); //b
+    // stroke(255);
+    rect(140,160,20,100); //c
+    rect(40,260,100,20); //d
+    rect(20,160,20,100); //e
+    rect(20,50,20,100); //b
+    rect(40,145,100,20); //g
+   
+
+    pop();
 }
