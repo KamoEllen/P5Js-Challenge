a682af095a5f9e459429ad06f12dd9f468009769
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-11 12:35:15 +0200

    demo vid

--- 46.Markov-chain/sketch.js
+++ 46.Markov-chain/sketch.js
@@ -1,62 +1,42 @@
-var txt = "The unicorn is a legendary creature that has been described since antiquity as a beast with a large, pointed, spiraling horn projecting from its forehead. The unicorn was depicted in ancient seals of the Indus Valley Civilization and was mentioned by the ancient Greeks in accounts of natural history by various writers, including Ctesias, Strabo, Pliny the Younger, and Aelian.[1] The Bible also describes an animal, the re'em, which some translations have erroneously rendered with the word unicorn.[1] In European folklore, the unicorn is often depicted as a white horse-like or goat-like animal with a long horn and cloven hooves (sometimes a goat's beard). In the Middle Ages and Renaissance, it was commonly described as an extremely wild woodland creature, a symbol of purity and grace, which could only be captured by a virgin. In the encyclopedias its horn was said to have the power to render poisoned water potable and to heal sickness. In medieval and Renaissance times, the tusk of the narwhal was sometimes sold as unicorn horn.";
-var order = 9;//tri
-var ngrams =  {};
+var txt =
+  "The unicorn is a legendary creature that has been described since antiquity as a beast with a large, pointed, spiraling horn projecting from its forehead. The unicorn was depicted in ancient seals of the Indus Valley Civilization and was mentioned by the ancient Greeks in accounts of natural history by various writers, including Ctesias, Strabo, Pliny the Younger, and Aelian.[1] The Bible also describes an animal, the re'em, which some translations have erroneously rendered with the word unicorn.[1] In European folklore, the unicorn is often depicted as a white horse-like or goat-like animal with a long horn and cloven hooves (sometimes a goat's beard). In the Middle Ages and Renaissance, it was commonly described as an extremely wild woodland creature, a symbol of purity and grace, which could only be captured by a virgin. In the encyclopedias its horn was said to have the power to render poisoned water potable and to heal sickness. In medieval and Renaissance times, the tusk of the narwhal was sometimes sold as unicorn horn.";
+var order = 3;
+var ngrams = {};
 var button;
 
 function setup() {
-    button = createButton("generate");
-    button.mousePressed(markovIt);
-     noCanvas();
-    // createCanvas(400, 400, WEBGL);
-
-    //loop text
-    for (var i = 0;i <= txt.length - order ; i++)
-    {
-        var gram = txt.substring(i,i+ order); //substring :first index included , last excluded
-        if (!ngrams[gram])
-        {
-            ngrams[gram] = [];
-            // ngrams[gram].push(txt.charAt(i+3));
-            //if  unique , list it and the next three letters
-
-        }
-        else
-        {
-            // ngrams[gram]++
-            ngrams[gram].push(txt.charAt(i+order));
-            
-        }
-        // ngrams.push(gram);
+  button = createButton("generate");
+  button.mousePressed(markovIt);
+  noCanvas();
+  for (var i = 0; i <= txt.length - order; i++) {
+    var gram = txt.substring(i, i + order); //substring :first index included , last excluded
+    if (!ngrams[gram]) {
+      ngrams[gram] = [];
+    } else {
+      ngrams[gram].push(txt.charAt(i + order));
     }
   }
-  function draw() {
-    // background(145,145,145);
-    
-    console.log(ngrams);
-  }
+}
+function draw() {
+  console.log(ngrams);
+}
 
-  function markovIt()
-  {
+function markovIt() {
+  var currentGram = txt.substring(0, order);
 
-    var currentGram = txt.substring(0, order);
+  var result = currentGram;
 
-    var result= currentGram;
+  for (var i = 0; i < 30; i++) {
+    var possibilities = ngrams[currentGram];
 
-    for (var i =0; i < 30; i++)
-    {
-    //rndom next poss characters
-        var possibilities = ngrams[currentGram];
-        // if order is 3+
-        if(!possibilities)
-        {
-            break;
-        }
-        var next = random(possibilities);
-        result += next;
-        var len =result.length
-        currentGram = result.substring(len-order, len);
+    if (!possibilities) {
+      break;
     }
-    
-
-    createP(result);
+    var next = random(possibilities);
+    result += next;
+    var len = result.length;
+    currentGram = result.substring(len - order, len);
   }
+
+  createP(result);
+}
