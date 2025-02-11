9631b3771bcc5e887f4be188184f6bfda491a0a2
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-11 17:47:51 +0200

    Update 50.Chaos-Game-P2 , hexagon shape , in center , added comments

--- 50.Chaos-Game-P2/sketch.js
+++ 50.Chaos-Game-P2/sketch.js
@@ -1,4 +1,4 @@
-let points = [];
+let points ;
 var current;
 
 let x, y;
@@ -7,37 +7,53 @@ function setup() {
   createCanvas(400, 400);
   background(0); 
 
+  //constant
+  points = [];//empty array
+  const n = 8;
+ 
+  //3 random points
+  for (let i = 0; i < n; i++) {
+    let angle = i * TWO_PI/ n;
+    let v = p5.Vector.fromAngle(angle);
+    v.mult(width/2);
+    v.add(width/2, height/2);
+    points.push(v);
+  }
+
+
   reset(); // Refactoring :)
+//   translate(width/2, height/2);
 }
 
 function reset()
 {
-     
-  stroke(255);
-  strokeWeight(8); // thickness
- 
-  //3 random points
-  for (let i = 0; i < 3; i++) {
-    let v = createVector(random(width), random(height)); // Fixed typo: ceateVector -> createVector
-    points.push(v);
-  }
-
+    
+  
   //current point is set to a random position
   current = createVector(random(width), random(height)); // Fixed typo: ceateVector -> createVector
 
+  background(0);
+  stroke(255);
+  strokeWeight(8); // thickness
   //initial 3 points
   for (let p of points) {
     point(p.x, p.y);
   }
+  
 }
 function draw() {
+    // translate(width/2, height/2);
+
+    if (frameCount % 100 == 0)
+    {
+        reset(); 
+    }
   for (let i = 0; i < 100; i++) {
     strokeWeight(2);
     stroke(255,0,255);
     point(x, y);
 
-    // Randomly choose one of the 3 points and interpolate towards it
-    // let r = floor(random(3));
+ 
     let next = random(points);
     current.x = lerp(current.x, next.x,0.5);
     current.y = lerp(current.y, next.y,0.5);
