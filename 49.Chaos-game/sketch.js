920ed69745dc61cc9f12c371cdce6da9595da3d6
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-11 16:55:54 +0200

    Update 49.Chaos-game , added3 random seed points

--- 49.Chaos-game/sketch.js
+++ 49.Chaos-game/sketch.js
@@ -1,8 +1,27 @@
-function setup()
-{
-    createCanvas(400,400);
+let ax, ay;
+let bx, by;
+let cx, cy;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    stroke(255);
+    strokeWeight(8); // thickness
+    background(0); // Clear the canvas every frame
+
+    // Generate new random positions
+    ax = random(width);
+    ay = random(height);
+    bx = random(width);
+    by = random(height);
+    cx = random(width);
+    cy = random(height);
+
+    // Draw the points
+    point(ax, ay);
+    point(bx, by);
+    point(cx, cy);
 }
-function draw()
-{
-    background(99,0,200);
+
+function draw() {
+
 }
