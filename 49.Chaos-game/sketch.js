cb75189ce29910c9610f0b671d82632ab3a5147a
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-11 17:07:54 +0200

    Update 49.Chaos-game , seed points at corners, added different colors

--- 49.Chaos-game/sketch.js
+++ 49.Chaos-game/sketch.js
@@ -11,12 +11,19 @@ function setup() {
     background(0); // Clear the canvas every frame
 
     // Generate new random positions
-    ax = random(width);
-    ay = random(height);
-    bx = random(width);
-    by = random(height);
-    cx = random(width);
-    cy = random(height);
+    // ax = random(width);
+    ax = width/2;
+    // ay = random(height);
+    // bx = random(width);
+    ay = 0;
+    bx = 0;
+    by = height;
+    cx = width;
+    cy = height;
+
+    // by = random(height);
+    // cx = random(width);
+    // cy = random(height);
 
     x = random(width);
     y = random(height);
@@ -28,25 +35,34 @@ function setup() {
 }
 
 function draw() {
-    stroke(255,0,255);
+    for (let i = 0;i < 100; i++)
+    {
+
+    
+    
+    strokeWeight(2);
     point(x,y);
 
     //picking random point n movinghalfway there
     let r = floor(random(3));
     if (r == 0)
     {
+        stroke(255,0,255);
         x = lerp(x,ax,0.5);
         y = lerp(y,ay,0.5);
     }
     else if ( r==1)
         {
+            stroke(0,255,255);
             x = lerp(x,bx,0.5);
             y = lerp(y,by,0.5);
         }
     else if ( r==2)
     {
+        stroke(255,255,0);
         x = lerp(x,cx,0.5);
         y = lerp(y,cy,0.5);
     }
+}
 
 }
