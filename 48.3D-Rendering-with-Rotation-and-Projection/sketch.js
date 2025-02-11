86ea6744df0ceaa61a654bf027950d4dc4e3532d
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-11 16:32:57 +0200

    Update 48.3D-Rendering-with-Rotation-and-Projection , added rotation matrix,cube has 8 points, conected edges with line

--- 48.3D-Rendering-with-Rotation-and-Projection/sketch.js
+++ 48.3D-Rendering-with-Rotation-and-Projection/sketch.js
@@ -1,36 +1,79 @@
 let angle = 0;
 let points = [];
-let projection = []
-
-projection = 
-[
-    [1,0,0],
-    [0,1,0]
-]
 
 function setup() {
-    createCanvas(400,400);
-    //4 points of a square
-    points[0] = createVector(-50,-50 , 0);
-    points[1] = createVector(50,-50 , 0);
-    points[2] = createVector(50,50, 0);
-    points[3] = createVector(-50,50, 0);
-    
+  createCanvas(600, 400);
+
+  points[0] = createVector(-0.5, -0.5, -0.5);
+  points[1] = createVector(0.5, -0.5, -0.5);
+  points[2] = createVector(0.5, 0.5, -0.5);
+  points[3] = createVector(-0.5, 0.5, -0.5);
+  points[4] = createVector(-0.5, -0.5, 0.5);
+  points[5] = createVector(0.5, -0.5, 0.5);
+  points[6] = createVector(0.5, 0.5, 0.5);
+  points[7] = createVector(-0.5, 0.5, 0.5);
 }
-function draw()
-{
-    background(200,70,40);
-    translate(width/2, height/2);
+
+function draw() {
+  background(200,70,40);
+  translate(width / 2, height / 2);
+
+  const rotationZ = [
+    [cos(angle), -sin(angle), 0],
+    [sin(angle), cos(angle), 0],
+    [0, 0, 1],
+  ];
+
+  const rotationX = [
+    [1, 0, 0],
+    [0, cos(angle), -sin(angle)],
+    [0, sin(angle), cos(angle)],
+  ];
+
+  const rotationY = [
+    [cos(angle), 0, sin(angle)],
+    [0, 1, 0],
+    [-sin(angle), 0, cos(angle)],
+  ];
+
+  let projected = [];
+
+  for (let i = 0; i < points.length; i++) {
+    let rotated = matmul(rotationY, points[i]);
+    rotated = matmul(rotationX, rotated);
+    rotated = matmul(rotationZ, rotated);
+    let distance = 2;
+    let z = 1 / (distance - rotated.z);
+    const projection = [
+      [z, 0, 0],
+      [0, z, 0],
+    ];    
+    let projected2d = matmul(projection, rotated);
+
+    projected2d.mult(200);
+    projected[i] = projected2d;
+  }
+
+  for (let i = 0; i < projected.length; i++) {
     stroke(255);
-    strokeWeight(14);
+    strokeWeight(16);
     noFill();
+    const v = projected[i];
+    point(v.x, v.y);
+  }
+  for (let i = 0; i < 4; i++) {
+    connect(i, (i + 1) % 4, projected);
+    connect(i + 4, ((i + 1) % 4) + 4, projected);
+    connect(i, i + 4, projected);
+  }
 
-    //drawing points
-    for (let v = 0; v < points.length; v++) 
-    {
-        // point(v.x, v.y);
-        point(points[v].x, points[v].y);// Access the vector using points[v]
-    }
+  angle += 0.03;
+}
 
-    angle +=0.01;
+function connect(i, j, points) {
+  const a = points[i];
+  const b = points[j];
+  strokeWeight(1);
+  stroke(255);
+  line(a.x, a.y, b.x, b.y);
 }
