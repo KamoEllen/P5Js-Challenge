7ecfad678209a735a7815d73a81469b8c9964f7c
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-11 17:14:42 +0200

    Update 50.Chaos-Game-P2 , added code from previous project 49.Chaos-game

---
+++ 50.Chaos-Game-P2/sketch.js
@@ -0,0 +1,68 @@
+let ax, ay;
+let bx, by;
+let cx, cy;
+
+let x,y;
+
+function setup() {
+    createCanvas(400, 400);
+    stroke(255);
+    strokeWeight(8); // thickness
+    background(20); // Clear the canvas every frame
+
+    // Generate new random positions
+    // ax = random(width);
+    ax = width/2;
+    // ay = random(height);
+    // bx = random(width);
+    ay = 0;
+    bx = 0;
+    by = height;
+    cx = width;
+    cy = height;
+
+    // by = random(height);
+    // cx = random(width);
+    // cy = random(height);
+
+    x = random(width);
+    y = random(height);
+
+    // Draw the points
+    point(ax, ay);
+    point(bx, by);
+    point(cx, cy);
+}
+
+function draw() {
+    for (let i = 0;i < 100; i++)
+    {
+
+    
+    
+    strokeWeight(2);
+    point(x,y);
+
+    //picking random point n movinghalfway there
+    let r = floor(random(3));
+    if (r == 0)
+    {
+        stroke(255,0,255);
+        x = lerp(x,ax,0.5);
+        y = lerp(y,ay,0.5);
+    }
+    else if ( r==1)
+        {
+            stroke(0,255,255);
+            x = lerp(x,bx,0.5);
+            y = lerp(y,by,0.5);
+        }
+    else if ( r==2)
+    {
+        stroke(255,255,0);
+        x = lerp(x,cx,0.5);
+        y = lerp(y,cy,0.5);
+    }
+}
+
+}
