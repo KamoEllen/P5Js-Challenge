35629f32c58a1a61a38af6dd5794b043779e6b1d
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-11 22:13:36 +0200

    Update 51.Ulam-Spiral , counter

--- 51.Ulam-Spiral/sketch.js
+++ 51.Ulam-Spiral/sketch.js
@@ -1,7 +1,44 @@
+let x, y;
+
+let step = 1;
+let stepSize = 100;
+let numberSteps = 1;
+let state;
+
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(500, 500);
+    x = width / 2;
+    y = height / 2;
+    background(200, 30, 50);
+    
 }
-function draw()
-{
-    background(200,30,50);
+
+function draw() {
+    background(200, 30, 50);
+    textSize(64);
+    textAlign(CENTER, CENTER); 
+    fill(255);
+    text(step, x, y);
+
+    switch (state) {
+        case 0:
+            x += stepSize; // Move right
+            break;
+        case 1:
+            y -= stepSize; // Move up
+            break;
+        case 2:
+            x -= stepSize; // Move left
+            break;
+        case 3:
+            y += stepSize; // Move down
+            break;
+    }
+
+    step++;
+
+    // Change direction every 2 steps
+    if (step % 2 == 0) {
+        state = state + 1;
+    }
 }
