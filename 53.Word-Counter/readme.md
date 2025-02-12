5e3c19817f41f2960aacbc2d77de102e75f70980
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-12 16:29:16 +0200

    Update 53.Word-Counter , fixed the typo from noCanavs() to noCanvas() and removed extra space

---
+++ 53.Word-Counter/readme.md
@@ -0,0 +1,4 @@
+text in words.txt is from 
+https://en.wikipedia.org/wiki/Software_engineering
+
+is it a new word ? yesset count to one, no ? incremenet its existing count
--- 53.Word-Counter/sketch.js
+++ 53.Word-Counter/sketch.js
@@ -1,8 +1,34 @@
-function setup()
-{
-    createCanvas(400,400);
+var txt;
+var counts = {};
+
+function preload() {
+    txt = loadStrings('words.txt'); // del extra space
 }
-function draw()
-{
-    background(200,60,77);
+
+function setup() {
+    var allwords = txt.join("\n");
+    // /\W+/ == any non-alphanumeric character
+    var tokens = allwords.split(/\W+/);
+    
+
+
+    console.log(tokens);
+
+    for (var i = 0; i < tokens.length; i++) {
+        var word = tokens[i]; // Check each word
+        // Word will be key and count will be priority
+
+        if (counts[word] === undefined) {
+            counts[word] = 1;
+        } else {
+            counts[word] = counts[word] + 1;
+        }
+    }
+
+    noCanvas(); // Fixed typo here
+    
 }
+
+// function draw() {
+//     background(200, 60, 77);
+// }
