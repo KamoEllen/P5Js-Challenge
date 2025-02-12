7a7b0dd13b15ffb694f27beb6c59fde2e77e39b6
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-12 18:59:51 +0200

    Update  54.Context-free-grammar , sentence creation [The cat barks] and [The dog meows]

--- 54.Context-free-grammar/sketch.js
+++ 54.Context-free-grammar/sketch.js
@@ -1,9 +1,46 @@
+var rules = {
+  "S": [
+    ["The", "N", "V"]
+  ],
+  "N": [
+    ["cat"],
+    ["dog"]
+  ],
+  "V": [
+    ["meows"],
+    ["barks"]
+  ]
+};
+
+function expand(start, expansion) {
+    if (rules[start]) {
+      var pick = random(rules[start]);
+      console.log(pick);
+      for (var i = 0; i < pick.length; i++) {
+        expand(pick[i], expansion);
+      }
+    } else {
+      expansion.push(start);
+    }
+    return expansion.join(" ");
+  }
 
 function setup()
 {
-    createCanvas(400,400);
+    // createCanvas(400,400);
+    noCanvas();
+  
+   for (var i = 0; i < 10; i++)
+   {
+    var start = "S";
+    var expansion = [];
+   
+    var  result =  expand(start, expansion);
+    console.log(result);
+    createP(result);
+   }
 }
-function draw()
-{
- background(20,40,66);
-}
+// function draw()
+// {
+//  background(20,40,66);
+// }
