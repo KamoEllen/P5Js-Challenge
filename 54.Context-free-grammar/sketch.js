345bb8969c4dfffed23a9394844e7f4a987546d4
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-12 19:07:21 +0200

    Update  54.Context-free-grammar , expand function recursively builds sentences based on the rules.cfg function generates 10 random sentences and displays them on the page.removed draw function.

--- 54.Context-free-grammar/sketch.js
+++ 54.Context-free-grammar/sketch.js
@@ -1,16 +1,69 @@
 var rules = {
-  "S": [
-    ["The", "N", "V"]
-  ],
-  "N": [
-    ["cat"],
-    ["dog"]
-  ],
-  "V": [
-    ["meows"],
-    ["barks"]
-  ]
-};
+    "S": [
+      ["NP", "VP"],
+      ["Interj", "NP", "VP"]
+    ],
+    "NP": [
+      ["Det", "N"],
+      ["Det", "N", "that", "VP"],
+      ["Det", "Adj", "N"]
+    ],
+    "VP": [
+      ["Vtrans", "NP"],
+      ["Vintr"]
+    ],
+    "Interj": [
+      ["oh"],
+      ["my"],
+      ["wow"],
+      ["darn"]
+    ],
+    "Det": [
+      ["this"],
+      ["that"],
+      ["the"]
+    ],
+    "N": [
+        ["software engineer"],
+        ["developer"],
+        ["singer"],
+        ["dancer"],
+        ["code"],
+        ["algorithm"],
+        ["music"],
+        ["performance"]
+    ],
+    "Adj": [
+        ["creative"],
+        ["efficient"],
+        ["talented"],
+        ["passionate"],
+        ["innovative"],
+        ["dynamic"]
+    ],
+    "Vtrans": [
+        ["writes"],
+        ["debugs"],
+        ["designs"],
+        ["sings"],
+        ["dances"],
+        ["composes"]
+    ],
+    "Vintr": [
+        ["solves problems"],
+        ["performs"],
+        ["creates"],
+        ["collaborates"]
+    
+    ],
+    "Ving": [
+        ["coding"],
+        ["singing"],
+        ["dancing"],
+        ["debugging"],
+        ["performing"]
+    ]
+  };
 
 function expand(start, expansion) {
     if (rules[start]) {
@@ -25,22 +78,28 @@ function expand(start, expansion) {
     return expansion.join(" ");
   }
 
+  var button;
+
 function setup()
 {
     // createCanvas(400,400);
     noCanvas();
-  
-   for (var i = 0; i < 10; i++)
-   {
-    var start = "S";
-    var expansion = [];
-   
-    var  result =  expand(start, expansion);
-    console.log(result);
-    createP(result);
-   }
+    button = createButton('generate')
+    button.mousePressed(cfg);
+
 }
-// function draw()
-// {
-//  background(20,40,66);
-// }
+
+function cfg()
+{
+      
+   for (var i = 0; i < 10; i++)
+    {
+     var start = "S";
+     var expansion = [];
+    
+     var  result =  expand(start, expansion);
+     console.log(result);
+     createP(result);
+    }
+
+}
