8de8c49d8ee965c2e3ef7d7667a0bdc9bb889265
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-14 22:59:32 +0200

    Update 56.Paper-Marbling, tap canvas for dots to show up

--- 56.Paper-Marbling/sketch.js
+++ 56.Paper-Marbling/sketch.js
@@ -1,7 +1,17 @@
+let drops = [];
+
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 400);
+}
+
+function mousePressed() {
+  let drop = new Drop(mouseX, mouseY, 50);
+  drops.push(drop);
+}
+
+function draw() {
+  background(200, 22, 88);
+  for (let drop of drops) {
+    drop.show();
   }
-  
-  function draw() {
-    background(200,22,88);
-  }  
+}
