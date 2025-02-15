b963a4cda2cc3c1a34ae2558d0d5dd3cf8b1710c
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-15 15:56:27 +0200

    Update 56.Paper-Marbling

--- 56.Paper-Marbling/ink.js
+++ 56.Paper-Marbling/ink.js
@@ -15,32 +15,39 @@ class Drop {
     this.col = random(0, 200);
   }
 
-    marble(other) {
-        for (let v of this.vertices) {
-          let c = other.center;
-          let r = other.r;
-          let p = v.copy();
-          p.sub(c);
-          //p-c , mag of vector
-          let m = p.mag();
-          let root = sqrt(1 + (r * r) / (m * m));
-          p.mult(root);
-          p.add(c);
-          v.set(p);
-        }
-    }
-
-    
-  
-    show() {
-      fill(0);
-    //   circle(this.x, this.y, this.r * 2);
-
-    //decagon
-      beginShape();
-      for (let v of this.vertices) {
-        vertex(v.x, v.y);
-      }
-      endShape(CLOSE);
+  tine(m, x, y, z, c) {
+    let u = 1 / pow(2, 1 / c);
+    let b = createVector(x, y);
+    for (let v of this.vertices) {
+      let pb = p5.Vector.sub(v, b);
+      let n = m.copy().rotate(HALF_PI);
+      let d = abs(pb.dot(n));
+      let mag = z * pow(u, d);
+      v.add(m.copy().mult(mag));
     }
   }
+
+  marble(other) {
+    for (let v of this.vertices) {
+      let c = other.center;
+      let r = other.r;
+      let p = v.copy();
+      p.sub(c);
+      let m = p.mag();
+      let root = sqrt(1 + (r * r) / (m * m));
+      p.mult(root);
+      p.add(c);
+      v.set(p);
+    }
+  }
+
+  show() {
+    fill(this.col);
+    noStroke();
+    beginShape();
+    for (let v of this.vertices) {
+      vertex(v.x, v.y);
+    }
+    endShape(CLOSE);
+  }
+}
--- 56.Paper-Marbling/sketch.js
+++ 56.Paper-Marbling/sketch.js
@@ -1,22 +1,48 @@
 let drops = [];
 
 function setup() {
-  createCanvas(400, 400);
+    createCanvas(600, 400);
+    for (let i = 0; i < 20; i++) {
+        addInk(300, height / 2, 50);
+    }
+    for (let i = 0; i < 50; i++) {
+        let x = random(width);
+        let y = random(height);
+        let r = random(10, 50);
+        addInk(x, y, r);
+    }
 }
 
-function mousePressed() {
-  let drop = new Drop(mouseX, mouseY, 50);
+let start;
 
-  for (let other of drops)
-  {
-    other.marble(drop);
-  }
-  drops.push(drop);
+function mousePressed() {
+    start = createVector(mouseX, mouseY);
+}
+
+function mouseDragged() {
+    let end = createVector(mouseX, mouseY);
+    end.sub(start);
+    end.normalize();
+    tineLine(end, mouseX, mouseY, 2, 16);
+}
+
+function tineLine(v, x, y, z, c) {
+    for (let drop of drops) {
+        drop.tine(v, x, y, z, c);
+    }
+}
+
+function addInk(x, y, r) {
+    let drop = new Drop(x, y, r);
+    for (let other of drops) {
+        other.marble(drop);
+    }
+    drops.push(drop);
 }
 
 function draw() {
-  background(200, 22, 88);
-  for (let drop of drops) {
-    drop.show();
-  }
+    background(255);
+    for (let drop of drops) {
+        drop.show();
+    }
 }
