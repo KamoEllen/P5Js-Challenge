3cc8d450931f81d1ec184ebd81e37966125fd445
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-15 20:48:43 +0200

    Update 59.Pi-Leibniz, fix:Replaced iterations with iteration, corrected denominator cal

--- 59.Pi-Leibniz/sketch.js
+++ 59.Pi-Leibniz/sketch.js
@@ -1,35 +1,36 @@
 // alternating series where the terms are reciprocals of odd integers, with signs alternating between positive and negative.
-
-let pi = 1;
+let pi = 4;
 let iteration = 0;
+let history = [];
+let div;
 
 function setup() {
     createCanvas(600, 600);
-   
-   
-        createDiv(pi);
-    }
+    div = createDiv(pi).style('font-size', '64pt');
+}
 
-
-
-function draw()
-{
-    background(22,88,66);
-    for (let i = 0;i < 10; i++)
-        {
-            //alt seq odd nums
-            let den = iteration * 2 + 3;// denominato
-            if (iteration % 2 == 0)
-            {
-                pi -= (1/den);
-    
-            }
-            else{
-                pi +=  (1/den);
-            }
-    
-            pi *= 4;
-            div.html(pi);
-            iteration++;
+function draw() {
+    background(22, 88, 66);
+    for (let i = 0; i < 10; i++) {
+        let den = iteration * 2 + 3;
+        if (iteration % 2 == 0) {
+            pi -= (4 / den);
+        } else {
+            pi += (4 / den);
         }
+        history.push(pi);
+        beginShape();
+        noFill();
+        stroke(255);
+        strokeWeight(2);
+        let spacing = width / history.length;
+        for (let i = 0; i < history.length; i++) {
+            let x = i * spacing;
+            let y = map(history[i], 2, 4, height);
+            vertex(x, y);
+        }
+        endShape();
+        div.html(pi);
+        iteration++;
+    }
 }
