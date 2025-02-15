51103c79f60c214dfb7b28ca7197ea21cf1be023
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-15 20:38:07 +0200

    Update 59.Pi-Leibniz, keeping track of numbers added to series,updating appr of pi and displaying it

--- 59.Pi-Leibniz/sketch.js
+++ 59.Pi-Leibniz/sketch.js
@@ -1,7 +1,35 @@
+// alternating series where the terms are reciprocals of odd integers, with signs alternating between positive and negative.
+
+let pi = 1;
+let iteration = 0;
+
 function setup() {
     createCanvas(600, 600);
-}
+   
+   
+        createDiv(pi);
+    }
+
+
+
 function draw()
 {
     background(22,88,66);
+    for (let i = 0;i < 10; i++)
+        {
+            //alt seq odd nums
+            let den = iteration * 2 + 3;// denominato
+            if (iteration % 2 == 0)
+            {
+                pi -= (1/den);
+    
+            }
+            else{
+                pi +=  (1/den);
+            }
+    
+            pi *= 4;
+            div.html(pi);
+            iteration++;
+        }
 }
