03e5b6215039f712eaf51d475bd04cd83e6c8d1b
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-15 16:25:39 +0200

    Update  57.Falling-Sand, added gride

--- 57.Falling-Sand/sketch.js
+++ 57.Falling-Sand/sketch.js
@@ -1,7 +1,45 @@
+//from game of life folder
+function make2DArray(cols,rows)
+{
+    let arr = new Array(cols);
+    for (let i = 0; i< arr.length; i++)
+    {
+        arr[i] = new Array(rows);
+    }
+    return arr;
+}
+
+let grid;
+let w = 10;
+let cols, rows;
+
 function setup() {
     createCanvas(400, 400);
+    cols = width/w;
+    rows = height/w;
+    grid = make2DArray(cols,rows);
+
+    for (let i = 0; i < cols; i++)
+    {
+        for (let j = 0; j < rows; j++)
+        {
+            grid[i][j]=0;
+        }
+    }
 }
 function draw()
 {
     background(22,99,33);
+
+    for (let i = 0; i < cols; i++)
+    {
+        for (let j= 0; j < rows; j++)
+        {
+            stroke(255);
+            fill(grid[i][j]*255);
+            let x = i * w;
+            let y = j * w;
+            square(x,y,w);
+        }
+    }
 }
