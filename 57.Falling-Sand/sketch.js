60c0f2c30bd0f4b9b1bc69d1e5d6f14f0ad5e65d
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-15 18:05:39 +0200

    Update  57.Falling-Sand, animated falling block

--- 57.Falling-Sand/sketch.js
+++ 57.Falling-Sand/sketch.js
@@ -4,6 +4,10 @@ function make2DArray(cols,rows)
     for (let i = 0; i< arr.length; i++)
     {
         arr[i] = new Array(rows);
+        for (let j = 0; i< arr[i].length; j++)
+            {
+               arr[i][j] = 0;
+            }
     }
     return arr;
 }
@@ -25,6 +29,7 @@ function setup() {
             grid[i][j]=0;
         }
     }
+    grid[20][10] = 1;
 }
 function draw()
 {
@@ -41,4 +46,24 @@ function draw()
             square(x,y,w);
         }
     }
+
+    let nextGrid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows ; j++) {
+      let state = grid[i][j];
+      if (state == 1)
+      {
+        let below = grid[i][j+1];
+        if (below == 0)
+        {
+            nextGrid[i][j] = 0;
+            nextGrid[i][j+1] =1;
+        }
+        }
+      }
+    }
+    grid = nextGrid;
+      
+
+
 }
