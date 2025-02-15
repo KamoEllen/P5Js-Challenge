e33b80056e69b4186d9bc98e46b057014d81e6c0
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-15 18:08:25 +0200

    Update  57.Falling-Sand, Fixed: Changed i to j"

--- 57.Falling-Sand/sketch.js
+++ 57.Falling-Sand/sketch.js
@@ -1,13 +1,10 @@
-function make2DArray(cols,rows)
-{
+function make2DArray(cols, rows) {
     let arr = new Array(cols);
-    for (let i = 0; i< arr.length; i++)
-    {
+    for (let i = 0; i < arr.length; i++) {
         arr[i] = new Array(rows);
-        for (let j = 0; i< arr[i].length; j++)
-            {
-               arr[i][j] = 0;
-            }
+        for (let j = 0; j < arr[i].length; j++) { 
+            arr[i][j] = 0;
+        }
     }
     return arr;
 }
@@ -18,52 +15,45 @@ let cols, rows;
 
 function setup() {
     createCanvas(400, 400);
-    cols = width/w;
-    rows = height/w;
-    grid = make2DArray(cols,rows);
+    cols = width / w;
+    rows = height / w;
+    grid = make2DArray(cols, rows);
 
-    for (let i = 0; i < cols; i++)
-    {
-        for (let j = 0; j < rows; j++)
-        {
-            grid[i][j]=0;
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = 0;
         }
     }
     grid[20][10] = 1;
 }
-function draw()
-{
-    background(22,99,33);
 
-    for (let i = 0; i < cols; i++)
-    {
-        for (let j= 0; j < rows; j++)
-        {
+function draw() {
+    background(22, 99, 33);
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
             stroke(255);
-            fill(grid[i][j]*255);
+            fill(grid[i][j] * 255);
             let x = i * w;
             let y = j * w;
-            square(x,y,w);
+            square(x, y, w);
         }
     }
 
     let nextGrid = make2DArray(cols, rows);
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows ; j++) {
-      let state = grid[i][j];
-      if (state == 1)
-      {
-        let below = grid[i][j+1];
-        if (below == 0)
-        {
-            nextGrid[i][j] = 0;
-            nextGrid[i][j+1] =1;
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let state = grid[i][j];
+            if (state == 1) {
+                let below = grid[i][j + 1];
+                if (below == 0) {
+                    nextGrid[i][j] = 0;
+                    nextGrid[i][j + 1] = 1;
+                } else {
+                    nextGrid[i][j] = 1; 
+                }
+            }
         }
-        }
-      }
     }
     grid = nextGrid;
-      
-
-
 }
