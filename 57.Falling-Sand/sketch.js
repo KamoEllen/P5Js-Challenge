8645ea6900c2868d660ecaf02ce3770adc1e145d
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-15 18:43:21 +0200

    Update  57.Falling-Sand,added   withinCols and withinRows functions , removed comments

--- 57.Falling-Sand/sketch.js
+++ 57.Falling-Sand/sketch.js
@@ -1,41 +1,39 @@
-let hueValue = 200;
-
 function make2DArray(cols, rows) {
     let arr = new Array(cols);
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = new Array(rows);
-        for (let j = 0; j < arr[i].length; j++) { 
-            arr[i][j] = 0;
-        }
+      arr[i] = new Array(rows);
+      for (let j = 0; j < arr[i].length; j++) {
+        arr[i][j] = 0;
+      }
     }
     return arr;
-}
-
-let grid;
-let w = 10;
-let cols, rows;
-
-function setup() {
-    createCanvas(400, 400);
+  }
+  
+  let grid;
+  let w = 5;
+  let cols, rows;
+  let hueValue = 200;
+  
+  function withinCols(i) {
+    return i >= 0 && i <= cols - 1;
+  }
+  
+  function withinRows(j) {
+    return j >= 0 && j <= rows - 1;
+  }
+  
+  function setup() {
+    createCanvas(500, 300);
     colorMode(HSB, 360, 255, 255);
     cols = width / w;
     rows = height / w;
     grid = make2DArray(cols, rows);
-
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            grid[i][j] = 0;
-        }
-    }
-    // grid[20][10] = 1;
-}
-
-function mouseDragged() {
+  }
+  
+  function mouseDragged() {
     let mouseCol = floor(mouseX / w);
     let mouseRow = floor(mouseY / w);
     
-   
-    //can drop blocks in 5 radius
     let matrix = 5;
     let extent = floor(matrix / 2);
     for (let i = -extent; i <= extent; i++) {
@@ -43,62 +41,67 @@ function mouseDragged() {
         if (random(1) < 0.75) {
           let col = mouseCol + i;
           let row = mouseRow + j;
-          if (cols >= 0 && col <= cols -1 && row >= 0 )
-          {
+          if (withinCols(col) && withinRows(row)) {
             grid[col][row] = hueValue;
           }
         }
       }
     }
-    //hue color changes over time
     hueValue += 1;
-  if (hueValue > 360) {
-    hueValue = 1;
-  }
-
-}
-
-function draw() {
-    background(22, 99, 33);
-    noStroke();
-
-    for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            noStroke();
-            // fill(grid[i][j] * 255);
-            fill(grid[i][j], 255, 255);
-            let x = i * w;
-            let y = j * w;
-            square(x, y, w);
-        }
+    if (hueValue > 360) {
+      hueValue = 1;
     }
-
-    let nextGrid = make2DArray(cols, rows);
-
+  }
+  
+  function draw() {
+    background(0);
+    
     for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-            let state = grid[i][j];
-            if (state == 1) {
-                let below = grid[i][j + 1];
-                let belowA = -1;
-                let belowB = -1;
-                if (withinCols(i + dir)) {
-                    belowA = grid[i + dir][j + 1];
-                }
-                if (withinCols(i - dir)) {
-                    belowB = grid[i - dir][j + 1];
-                }
-                if (below === 0) {
-                    nextGrid[i][j + 1] = state;
-                } else if (belowA === 0) {
-                    nextGrid[i + dir][j + 1] = state;
-                } else if (belowB === 0) {
-                    nextGrid[i - dir][j + 1] = state;
-                } else {
-                    nextGrid[i][j] = state;
-                }
-            }
+      for (let j = 0; j < rows; j++) {
+        noStroke();
+        if (grid[i][j] > 0) {
+          fill(grid[i][j], 255, 255);
+          let x = i * w;
+          let y = j * w;
+          square(x, y, w);
         }
+      }
+    }
+    
+    let nextGrid = make2DArray(cols, rows);
+    
+    for (let i = 0; i < cols; i++) {
+      for (let j = 0; j < rows ; j++) {
+        let state = grid[i][j];
+        
+        if (state > 0) {
+          let below = grid[i][j + 1];
+          
+          let dir = 1;
+          if (random(1) < 0.5) {
+            dir *= -1;
+          }
+          
+          let belowA = -1;
+          let belowB = -1;
+          if (withinCols(i + dir)) {
+            belowA = grid[i + dir][j + 1];
+          }
+          if (withinCols(i - dir)) {
+            belowB = grid[i - dir][j + 1];
+          }
+          
+          if (below === 0) {
+            nextGrid[i][j + 1] = state;
+          } else if (belowA === 0) {
+            nextGrid[i + dir][j + 1] = state;
+          } else if (belowB === 0) {
+            nextGrid[i - dir][j + 1] = state;
+          } else {
+            nextGrid[i][j] = state;
+          }
+        }
+      }
     }
     grid = nextGrid;
-}
+  }
