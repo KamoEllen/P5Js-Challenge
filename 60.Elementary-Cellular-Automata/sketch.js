d5641077984dffe62cf1025e90fc830e96946cfc
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-16 14:18:59 +0200

    Update 60.Elementary-Cellular-Automata , calculating next gen of cells

--- 60.Elementary-Cellular-Automata/sketch.js
+++ 60.Elementary-Cellular-Automata/sketch.js
@@ -1,19 +1,54 @@
-let cells = [1,0,0,1,0,1,0,1,1,0,1,0,1,1,0];
+let cells = []; //1,0,0,1,0,1,0,1,1,0,1,0,1,1,0
 let w = 40;
+let y = 0;
 function setup() {
     createCanvas(600, 600);
+
+    let  total = width/2;
+    for (let i =0; i < total; i++)
+    {
+        cells[i] = floor(random(2));
+    }
+    background(222,234,99);
+  
 }
 function draw()
 {
-    background(222,234,99);
-
-    for (let i = 0; i <cells.length; i++)
+    
+    for (let i = 0; i < cells.length;i++)
     {
-        let x = i *w;
+        let x = i * w;
         stroke(0);
-        //1 cell is -purple
-        //0 cell is - pink
-        fill(255 -cells[i] * 88,11,220,90);
-        square(x,0,w);
+        fill(255 - cells[i] * 255);
+        square(x,y,w);
+    }
+    y += w;
+   
+
+
+    let nextCells = [];
+    nextCells[0] = cells[0];
+    nextCells[cells.length-1] =cells[cells.length-1];
+
+    for (let i=1; i < cells.length-1;i++)
+    {
+        let left = cells[i-1];
+        let right =  cells[i+1];
+        let state = cells[i];
+        let newState = celculateState(left,state,right);
+        nextCells[i] = newState;
+    }
+    cells = nextCells;
+
+    function celculateState(a,b,c)
+    {
+        if (a == 1 && b ==1 && c == 1) return 1;
+        if (a == 1 && b ==1 && c == 0) return 0;
+        if (a == 1 && b ==0 && c == 1) return 1;
+        if (a == 1 && b ==0 && c == 0) return 1;
+        if (a == 0 && b ==1 && c == 1) return 0;
+        if (a == 0 && b ==1 && c == 0) return 0;
+        if (a == 0 && b ==0 && c == 1) return 1;
+        if (a == 0 && b ==0 && c == 0) return 0;
     }
 }
