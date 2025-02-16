ccf32bc00e2beebe8a2d7c1281f061d490eb5f46
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-16 14:39:56 +0200

    Update 60.Elementary-Cellular-Automata , made cells wrap around edges& bigger cells

--- 60.Elementary-Cellular-Automata/sketch.js
+++ 60.Elementary-Cellular-Automata/sketch.js
@@ -1,62 +1,39 @@
-let cells = []; //1,0,0,1,0,1,0,1,1,0,1,0,1,1,0
-let ruleset = [1,0,0,1,0,1,0,1,1,0,1,0,1,1,0];
-let w = 10;
+let cells = [];
+let ruleValue = 90;
+let ruleSet;
+let w = 16;
 let y = 0;
 function setup() {
-    createCanvas(600,600);
-
-    let  total = width/w;
-    for (let i =0; i < total; i++)
-    {
-        // cells[i] = floor(random(2));
-        cells[i]=0;
+    createCanvas(600, 600);
+    ruleSet = ruleValue.toString(2).padStart(8, "0");
+    let total = width / w;
+    for (let i = 0; i < total; i++) {
+        cells[i] = 0;
     }
-    cells[floor(total/2)]   = 1;
-    background(222,234,99);
-  
+    cells[floor(total / 2)] = 1;
+    background(255);
 }
-function draw()
-{
-    
-    for (let i = 0; i < cells.length;i++)
-    {
+function draw() {
+    for (let i = 0; i < cells.length; i++) {
         let x = i * w;
-        // stroke(0);
         noStroke();
         fill(255 - cells[i] * 255);
-        square(x,y,w);
+        square(x, y, w);
     }
     y += w;
-   
-
-
     let nextCells = [];
-    nextCells[0] = cells[0];
-    nextCells[cells.length-1] =cells[cells.length-1];
-
-    for (let i=1; i < cells.length-1;i++)
-    {
-        let left = cells[i-1];
-        let right =  cells[i+1];
+    let len = cells.length;
+    for (let i = 0; i < len; i++) {
+        let left = cells[(i - 1 + len) % len];
+        let right = cells[(i + 1) % len];
         let state = cells[i];
-        let newState = calculateState(left,state,right);
+        let newState = calculateState(left, state, right);
         nextCells[i] = newState;
     }
     cells = nextCells;
-
-    function calculateState(a,b,c)
-    {
-        
-        let neighborhood = '' + a + b + c;
-        let value = 7-parseInt(neighborhood,2);
-        return ruleset[value];
-        // if (a == 1 && b ==1 && c == 1) return 1;
-        // if (a == 1 && b ==1 && c == 0) return 0;
-        // if (a == 1 && b ==0 && c == 1) return 1;
-        // if (a == 1 && b ==0 && c == 0) return 1;
-        // if (a == 0 && b ==1 && c == 1) return 0;
-        // if (a == 0 && b ==1 && c == 0) return 1;
-        // if (a == 0 && b ==0 && c == 1) return 1;
-        // if (a == 0 && b ==0 && c == 0) return 0;
-    }
+}
+function calculateState(a, b, c) {
+    let neighborhood = "" + a + b + c;
+    let value = 7 - parseInt(neighborhood, 2);
+    return parseInt(ruleSet[value]);
 }
