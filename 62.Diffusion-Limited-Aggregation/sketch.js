e98b1475522e98b489213fc9a8438fc556d1c888
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-16 19:04:21 +0200

    Update 62.Diffusion-Limited-Aggregation, added global variables, walkers jump around canvas

--- 62.Diffusion-Limited-Aggregation/sketch.js
+++ 62.Diffusion-Limited-Aggregation/sketch.js
@@ -1,7 +1,38 @@
+var tree = [];
+var walker;
+var r = 16;
+
 function setup() {
     createCanvas(600, 600);
-   
+    tree[0] = createVector(width / 2, height / 2);
 }
+
 function draw() {
-    background(255);
+    background(0);
+    var stuck = false;
+    walker = createVector(random(width), random(height));
+
+    while (!stuck) {
+        for (var i = 0; i < tree.length; i++) {
+            var d = p5.Vector.dist(walker, tree[i]);
+            if (d < r * 2) {
+                stuck = true;
+                break;
+            }
+        }
+        walker.x += random(-2, 2);
+        walker.y += random(-2, 2);
+        
+        // Corrected this part
+        walker.x = constrain(walker.x, 0, width);
+        walker.y = constrain(walker.y, 0, height);
+    }
+    
+    tree.push(walker);
+
+    for (var i = 0; i < tree.length; i++) {
+        strokeWeight(r);
+        stroke(255);
+        point(tree[i].x, tree[i].y);
+    }
 }
