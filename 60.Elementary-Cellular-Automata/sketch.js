f931ad7d0f360b7ec6914a778a56d89725a650f5
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-16 14:04:42 +0200

    Update 60.Elementary-Cellular-Automata , added cells

--- 60.Elementary-Cellular-Automata/sketch.js
+++ 60.Elementary-Cellular-Automata/sketch.js
@@ -1,7 +1,19 @@
+let cells = [1,0,0,1,0,1,0,1,1,0,1,0,1,1,0];
+let w = 40;
 function setup() {
     createCanvas(600, 600);
 }
 function draw()
 {
     background(222,234,99);
+
+    for (let i = 0; i <cells.length; i++)
+    {
+        let x = i *w;
+        stroke(0);
+        //1 cell is -purple
+        //0 cell is - pink
+        fill(255 -cells[i] * 88,11,220,90);
+        square(x,0,w);
+    }
 }
