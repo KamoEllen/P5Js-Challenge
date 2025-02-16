619c3529bd2ebc293a857962d5af87e742cd8bee
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-16 17:32:56 +0200

    Update  61.Climate-Spiral, animate spiral

--- 61.Climate-Spiral/sketch.js
+++ 61.Climate-Spiral/sketch.js
@@ -1,98 +1,135 @@
 let data;
+
 let months;
+
 let zeroRadius = 125;
 let oneRadius = 200;
 
-function preload() {
-    data = loadTable("giss-data-apr-11-2023.csv", "csv", "header");
-  }
-  
-function setup() {
-    createCanvas(600, 600);
-    // months = data.columns.slice(1);
-    // console.log(months);
-    months = [
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-        "Jan",
-        "Feb",
-      ];
+let currentRow = 1;
+let currentMonth = 0;
 
+let previousAnomaly = 0;
+
+function preload() {
+  data = loadTable("giss-data-apr-11-2023.csv", "csv", "header");
+}
+
+function setup() {
+  createCanvas(600, 600);
+  months = [
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+    "Jan",
+    "Feb",
+  ];
+
+  let row = data.getRow(0);
 }
 
 function draw() {
-    background(88,22,34);;
-    translate(width/2, height/2);
+  background(0);
+  translate(width / 2, height / 2);
+  textAlign(CENTER, CENTER);
+  textSize(16);
+  stroke(255);
+  strokeWeight(2);
+  noFill();
+  circle(0, 0, zeroRadius * 2);
+  fill(255);
+  noStroke();
+  text("0°", zeroRadius + 10, 0);
 
-    //0 degree
-    noFill();
-    stroke(255);
-    strokeWeight(4);
-    circle(0, 0, zeroRadius * 2);
-    fill(255);
+  stroke(255);
+  strokeWeight(2);
+  noFill();
+  circle(0, 0, oneRadius * 2);
+  fill(255);
+  noStroke();
+  text("1°", oneRadius + 10, 0);
+
+  stroke(255);
+  strokeWeight(2);
+  noFill();
+  circle(0, 0, 500);
+
+  for (let i = 0; i < months.length; i++) {
     noStroke();
-    text("0°", zeroRadius+10,0);
-
-    //1 degree
-    noFill();
-    stroke(255);
-    strokeWeight(4);
-    circle(0, 0, oneRadius * 2);
     fill(255);
-    noStroke();
-    text("1°",oneRadius+10,0);
+    textSize(24);
+    let angle = map(i, 0, months.length, 0, TWO_PI);
+    push();
+    let x = 264 * cos(angle);
+    let y = 264 * sin(angle);
+    translate(x, y);
+    rotate(angle + PI / 2);
+    text(months[i], 0, 0);
+    pop();
+  }
 
-    //month
-    noFill();
-    stroke(255);
-    strokeWeight(4);
-    circle(0,0,500);
-    fill(255);
-    noStroke();
-    // text("Months", 255,0);
+  let year = data.getRow(currentRow).get("Year");
+  textSize(32);
+  text(year, 0, 0);
 
-    for (let i = 0; i < months.length; i++) {
-        noStroke();
-        fill(255);
-        textSize(24);
-        let angle = map(i, 0, months.length, 0, TWO_PI);
-        push();
-        let x = 264 * cos(angle);
-        let y = 264 * sin(angle);
-        translate(x, y);
-        rotate(angle + PI / 2);
-        text(months[i], 0, 0);
-        pop();
-      }
-
-     
-    //   beginShape();
-    //   noFill();
-    beginShape();
-noFill();
-stroke(255); 
-strokeWeight(2);  
-for (let j = 0; j < data.getRowCount(); j++) {  
+  noFill();
+  stroke(255);
+  let firstValue = true;
+  for (let j = 0; j < currentRow; j++) {
     let row = data.getRow(j);
-    beginShape();  // inside the loop, creating a separate shape per year
-    for (let i = 0; i < months.length; i++) {  
-        let anomaly = row.getNum(months[i]);
-        if (!isNaN(anomaly)) {  
-            let angle = map(i, 0, months.length, 0, TWO_PI) - HALF_PI;  
-            let r = map(anomaly, 0, 1, zeroRadius, oneRadius);
-            let x = r * cos(angle);
-            let y = r * sin(angle);
-            vertex(x, y);
-        }
+
+    let totalMonths = months.length;
+    if (j == currentRow - 1) {
+      totalMonths = currentMonth;
     }
-    endShape();  
-}
+
+    for (let i = 0; i < totalMonths; i++) {
+      let anomaly = row.get(months[i]);
+
+      if (anomaly !== "***") {
+        anomaly = parseFloat(anomaly);
+        let angle = map(i, 0, months.length, 0, TWO_PI) - PI / 3;
+        let pr = map(previousAnomaly, 0, 1, zeroRadius, oneRadius);
+        let r = map(anomaly, 0, 1, zeroRadius, oneRadius);
+
+        let x1 = r * cos(angle);
+        let y1 = r * sin(angle);
+        let x2 = pr * cos(angle - PI / 6);
+        let y2 = pr * sin(angle - PI / 6);
+
+        if (!firstValue) {
+          let avg = (anomaly + previousAnomaly) * 0.5;
+          let cold = color(0, 0, 255);
+          let warm = color(255, 0, 0);
+          let zero = color(255);
+          let lineColor = zero;
+          if (avg < 0) {
+            lineColor = lerpColor(zero, cold, abs(avg));
+          } else {
+            lineColor = lerpColor(zero, warm, abs(avg));
+          }
+
+          stroke(lineColor);
+          line(x1, y1, x2, y2);
+        }
+        firstValue = false;
+        previousAnomaly = anomaly;
+      }
+    }
+  }
+
+  currentMonth = currentMonth + 1;
+  if (currentMonth == months.length) {
+    currentMonth = 0;
+    currentRow = currentRow + 1;
+    if (currentRow == data.getRowCount()) {
+      noLoop();
+    }
+  }
 }
