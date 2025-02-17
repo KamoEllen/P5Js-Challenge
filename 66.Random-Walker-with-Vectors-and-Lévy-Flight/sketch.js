0bd34c9ac4f356923af9e02818185c155de56c0d
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-17 19:50:36 +0200

    Update 66.Random-Walker-with-Vectors-and-Lévy-Flight folder, adding in levy flight

--- 66.Random-Walker-with-Vectors-and-Lévy-Flight/sketch.js
+++ 66.Random-Walker-with-Vectors-and-Lévy-Flight/sketch.js
@@ -1,17 +1,28 @@
 let pos;
+let prev;
 
 function setup() {
   createCanvas(400, 400);
-  background(0);
-  pos = createVector(200,200);
+  pos = createVector(width/2, height/2);
+  prev = pos.copy();
+  background(51);
 }
 
 function draw() {
   stroke(255);
   strokeWeight(2);
-point(pos.x,pos.y);
-  var step = p5.Vector.random2D();
-  pos =pos +step;
+  line(pos.x, pos.y, prev.x, prev.y);
+  prev.set(pos);
 
+  var step = p5.Vector.random2D();
+
+  var r = random(100);
+  if (r < 1) {
+    step.mult(random(25, 100));
+  } else {
+    step.setMag(2);
+  }
+
+  pos.add(step);
 
 }
