6ea6bb61e1a8ef52cb563bd46bf3d502805df039
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-17 19:32:13 +0200

    Update 66.Random-Walker-with-Vectors-and-Lévy-Flight folder, used p5.vector.random2d func to choose a random vector to travel in

--- 66.Random-Walker-with-Vectors-and-Lévy-Flight/sketch.js
+++ 66.Random-Walker-with-Vectors-and-Lévy-Flight/sketch.js
@@ -1,7 +1,17 @@
-function setup() 
-{
-    createCanvas(400, 400);
-  }
-  function draw() {
-    background(50,255,200);
-  }
+let pos;
+
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+  pos = createVector(200,200);
+}
+
+function draw() {
+  stroke(255);
+  strokeWeight(2);
+point(pos.x,pos.y);
+  var step = p5.Vector.random2D();
+  pos =pos +step;
+
+
+}
