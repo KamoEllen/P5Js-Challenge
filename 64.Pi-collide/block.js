f355c0a0a51286cdea51f096085c2553aaad34e5
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-17 14:54:12 +0200

    Update  64.Pi-collide,added bonce/reverse function ,both blocks bounce back when they hit the wall

--- 64.Pi-collide/block.js
+++ 64.Pi-collide/block.js
@@ -1,35 +1,38 @@
-class Block
-{
-    constructor(x,w,v)
-    {
-        this.x =x;
-        this.y = height -w;
+class Block {
+    constructor(x, w, v, m) {
+        this.x = x;
+        this.y = height - w;
         this.w = w;
         this.v = v;
-        this.m= 1; //mass
+        this.m = m; // mass
     }
 
-    bounce(other) 
-    {
+    hitWall() {
+        // collision on left or right wall
+        return (this.x <= 0 || this.x + this.w >= width);
+    }
+
+    reverse() {
+        //reverse velocity
+        this.v *= -1;
+    }
+
+    bounce(other) {
         let sumM = this.m + other.m;
         let newV = (this.m - other.m) / sumM * this.v;
         newV += (2 * other.m / sumM) * other.v;
         return newV;
     }
 
-    collide(other)
-    {
-        return !(this.x + this.w < other.x ||this.x > other.x + other.w)
-       
-
+    collide(other) {
+        return !(this.x + this.w < other.x || this.x > other.x + other.w);
     }
-    update()
-    {
+
+    update() {
         this.x += this.v;
     }
 
-    show()
-    {
-        image(blockImg, this.x, this.y,this.w,this.w);
+    show() {
+        image(blockImg, this.x, this.y, this.w, this.w);
     }
 }
--- 64.Pi-collide/sketch.js
+++ 64.Pi-collide/sketch.js
@@ -1,35 +1,38 @@
 let blockImg;
 let block1;
 let block2;
-// let clack;
 
-function preload()
-{
-    blockImg = loadImage('block1.png')
-    // clack = loadSound('clack.wav')
+function preload() {
+    blockImg = loadImage('block1.png');
 }
+
 function setup() {
     createCanvas(600, 600);
-    block1 = new Block(100,20,0);
-    block2 = new Block(200,150,-5);
+    block1 = new Block(100, 90, 2, 0);
+    block2 = new Block(200, 90, 1, -5);
 }
-function draw()
-{
-    background(77,99,44);
-    if (block1.collide(block2))
-    {
-        // block1.bounce(block2);
+
+function draw() {
+    background(77, 99, 44);
+
+    if (block1.collide(block2)) {
         const v1 = block1.bounce(block2);
-      const v2 = block2.bounce(block1);
-      block1.v = v1;
-      block2.v = v2;
+        const v2 = block2.bounce(block1);
+        block1.v = v1;
+        block2.v = v2;
     }
 
+    //both walls reverse/bonce once they hit the wall
+    if (block2.hitWall()) {
+        block2.reverse();
+    }
+    if (block1.hitWall()) {
+        block1.reverse();
+    }
 
     block1.show();
-  block2.show();
+    block2.show();
 
-  block1.update();
+    block1.update();
     block2.update();
-
 }
