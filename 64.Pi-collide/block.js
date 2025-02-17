bedbc7daccced04b02ccef0721818484bab04f9d
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-17 14:38:02 +0200

    Update  64.Pi-collide,added vounce feature and increased velocity

--- 64.Pi-collide/block.js
+++ 64.Pi-collide/block.js
@@ -6,6 +6,22 @@ class Block
         this.y = height -w;
         this.w = w;
         this.v = v;
+        this.m= 1; //mass
+    }
+
+    bounce(other) 
+    {
+        let sumM = this.m + other.m;
+        let newV = (this.m - other.m) / sumM * this.v;
+        newV += (2 * other.m / sumM) * other.v;
+        return newV;
+    }
+
+    collide(other)
+    {
+        return !(this.x + this.w < other.x ||this.x > other.x + other.w)
+       
+
     }
     update()
     {
--- 64.Pi-collide/sketch.js
+++ 64.Pi-collide/sketch.js
@@ -11,11 +11,21 @@ function preload()
 function setup() {
     createCanvas(600, 600);
     block1 = new Block(100,20,0);
-    block2 = new Block(200,150,-1);
+    block2 = new Block(200,150,-5);
 }
 function draw()
 {
     background(77,99,44);
+    if (block1.collide(block2))
+    {
+        // block1.bounce(block2);
+        const v1 = block1.bounce(block2);
+      const v2 = block2.bounce(block1);
+      block1.v = v1;
+      block2.v = v2;
+    }
+
+
     block1.show();
   block2.show();
 
