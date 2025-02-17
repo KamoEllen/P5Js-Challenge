cb3fbb149ae5bae29a1516d4dd86f69d97abc89d
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-17 15:16:14 +0200

    Update  64.Pi-collide,added timesteps and constraints

--- 64.Pi-collide/block.js
+++ 64.Pi-collide/block.js
@@ -1,38 +1,40 @@
 class Block {
-    constructor(x, w, v, m) {
-        this.x = x;
-        this.y = height - w;
-        this.w = w;
-        this.v = v;
-        this.m = m; // mass
+    constructor(x, w, m, v, xc) {
+      this.x = x;
+      this.y = height - w;
+      this.w = w;
+      this.v = v;
+      this.m = m;
+      this.xConstraint = xc;
     }
-
+  
     hitWall() {
-        // collision on left or right wall
-        return (this.x <= 0 || this.x + this.w >= width);
+      return (this.x <= 0)
     }
-
+  
     reverse() {
-        //reverse velocity
-        this.v *= -1;
+      this.v *= -1;
     }
-
-    bounce(other) {
-        let sumM = this.m + other.m;
-        let newV = (this.m - other.m) / sumM * this.v;
-        newV += (2 * other.m / sumM) * other.v;
-        return newV;
-    }
-
+  
     collide(other) {
-        return !(this.x + this.w < other.x || this.x > other.x + other.w);
+      return !(this.x + this.w < other.x ||
+        this.x > other.x + other.w);
     }
-
+  
+    bounce(other) {
+      let sumM = this.m + other.m;
+      let newV = (this.m - other.m) / sumM * this.v;
+      newV += (2 * other.m / sumM) * other.v;
+      return newV;
+    }
+  
     update() {
-        this.x += this.v;
+      this.x += this.v;
     }
-
+  
     show() {
-        image(blockImg, this.x, this.y, this.w, this.w);
+  
+      const x = constrain(this.x, this.xConstraint, width);
+      image(blockImg, x, this.y, this.w, this.w);
     }
-}
+  }
--- 64.Pi-collide/sketch.js
+++ 64.Pi-collide/sketch.js
@@ -1,52 +1,49 @@
 let blockImg;
-
 let block1;
 let block2;
 
-var count = 0;
-let digits = 1;
-
-var countDiv;
+let count = 0;
+let digits = 7;
+let countDiv;
+const timeSteps = 10 ** (digits - 1);
 
 function preload() {
-    blockImg = loadImage('block1.png');
+  blockImg = loadImage('block1.png');
 }
 
 function setup() {
-    createCanvas(600, 600);
-    block1 = new Block(100, 20, 2, 0);
-    const m2 = pow(100, digits);
-    block2 = new Block(300, 100, m2, -5);
-    countDiv = createDiv(count);
-    countDiv.style('font-size', '72pt');
+  createCanvas(400, 400);
+  block1 = new Block(100, 20, 1, 0, 0);
+  const m2 = pow(100, digits - 1);
+  block2 = new Block(300, 100, m2, -1 / timeSteps, 20);
+  countDiv = createDiv(count);
+  countDiv.style('font-size', '72pt');
+
 }
 
 function draw() {
-    background(77, 99, 44);
+  background(200);
 
+
+  for (let i = 0; i < timeSteps; i++) {
     if (block1.collide(block2)) {
-        const v1 = block1.bounce(block2);
-        const v2 = block2.bounce(block1);
-        block1.v = v1;
-        block2.v = v2;
-        count++;
+      const v1 = block1.bounce(block2);
+      const v2 = block2.bounce(block1);
+      block1.v = v1;
+      block2.v = v2;
+      count++;
     }
 
-    //both walls reverse/bonce once they hit the wall
-    if (block2.hitWall()) {
-        block2.reverse();
-        count++;
-    }
     if (block1.hitWall()) {
-        block1.reverse();
-        count++;
+      block1.reverse();
+      count++;
     }
-
-    block1.show();
-    block2.show();
 
     block1.update();
     block2.update();
-    
-    countDiv.html(nf(count));
+  }
+  block1.show();
+  block2.show();
+
+  countDiv.html(nf(count, digits));
 }
