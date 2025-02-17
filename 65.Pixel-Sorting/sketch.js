dc697c7f9becf3864ea1c6d1847aa9c0d9a59491
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-17 15:45:35 +0200

    Update 65.Pixel-Sorting, left half of the canvas shows original image (img), scaled to fit, right is black

--- 65.Pixel-Sorting/sketch.js
+++ 65.Pixel-Sorting/sketch.js
@@ -1,7 +1,18 @@
-function setup() {
-    createCanvas(400, 400);
+let img;
+
+function preload() {
+    img = loadImage("img.jpg");
 }
-function draw()
-{
+
+function setup() {
+    createCanvas(800, 400);
+}
+
+function draw() {
     background(0);
+
+
+    let imgWidth = width / 2; 
+    let imgHeight = (img.height / img.width) * imgWidth; // Maintain aspect ratio
+    image(img, 0, 0, imgWidth, imgHeight);
 }
