18f9f02a5de60b2ecf79590a263fa1b0ed08f68b
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-18 02:28:41 +0200

    Update 68.XOR, added random bright pixel/blocks on canvas

--- 68.XOR/sketch.js
+++ 68.XOR/sketch.js
@@ -25,4 +25,23 @@ function setup() {
   
   function draw() {
     background(99,66,44); //brown
+
+for (var i = 0; i < 1000; i++)
+{
+     let data = random(training_data);
+    nn.train(data.inputs,data.outputs); //this input should produce that output
+
+}
+   
+let resolution = 10;
+  let cols = width / resolution;
+  let rows = height / resolution;
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) 
+    {
+        fill(random(255));
+        rect(i * resolution , j * resolution, resolution);
+    }
+}
+
   }
