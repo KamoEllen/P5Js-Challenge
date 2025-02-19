9974cca3e847e9e74e621b44c97a7c4c12eed085
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-19 17:32:01 +0200

    Update 73.Tsp-Genetic-algorithm , added foundation  basic city generation and ga.js file

--- 73.Tsp-Genetic-algorithm/sketch.js
+++ 73.Tsp-Genetic-algorithm/sketch.js
@@ -1,7 +1,33 @@
+// sketch.js
+const cities = [];
+const totalCities = 15;
+const popSize = 500;
+const fitness = [];
+
+let population = [];
+let recordDistance = Infinity;
+let bestEver;
+let currentBest;
+
+let statusP;
+
 function setup() {
-    createCanvas(400, 300);
+  createCanvas(600, 600);
+  const order = [];
+  for (let i = 0; i < totalCities; i++) {
+    const v = createVector(random(width), random(height / 2));  // Random city placement
+    cities[i] = v;
+    order[i] = i;
   }
+
+  for (let i = 0; i < popSize; i++) {
+    population[i] = shuffle(order);  // Randomly shuffle the city order for the population
+  }
+  statusP = createP('').style('font-size', '32pt');
+}
+
+function draw() {
+  background(0,44,7);  // Clear the screen each frame
+
   
-  function draw() {
-    background(0,44,7);
-  }
+}
