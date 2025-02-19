262c80100a486d3173fb112f35c05e618ae3dfd8
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-19 16:27:30 +0200

    Update 71.Traveling-Salesperson ,added beginShape() and endShape() to connect cities

--- 71.Traveling-Salesperson/sketch.js
+++ 71.Traveling-Salesperson/sketch.js
@@ -1,23 +1,25 @@
-var recordDistance;
-var bestEver;
-
-function setup() {
-  createCanvas(400, 300);
-  for (var i = 0; i < totalCities; i++) {
-    var v = createVector(random(width), random(height));
-    cities[i] = v;
+function draw() {
+    background(188, 199, 234); // soft blue
+    fill(255);
+    for (var i = 0; i < cities.length; i++) {
+      ellipse(cities[i].x, cities[i].y, 8, 8);
+    }
+  
+    stroke(255);
+    strokeWeight(1);
+    noFill();
+    beginShape();
+    for (var i = 0; i < cities.length; i++) {
+      vertex(cities[i].x, cities[i].y);
+    }
+    endShape();
+  
+    stroke(255, 0, 255);
+    strokeWeight(4);
+    noFill();
+    beginShape();
+    for (var i = 0; i < cities.length; i++) {
+      vertex(bestEver[i].x, bestEver[i].y);
+    }
+    endShape();
   }
-
-  var d = calcDistance(cities);
-  recordDistance = d;
-  bestEver = cities.slice();
-}
-
-function calcDistance(points) {
-  var sum = 0;
-  for (var i = 0; i < points.length - 1; i++) {
-    var d = dist(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
-    sum += d;
-  }
-  return sum;
-}
