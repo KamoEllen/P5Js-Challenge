c156bb8afde2baaac8b6c08ab86eb284008ba038
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-19 16:17:31 +0200

    Update 71.Traveling-Salesperson ,Added cities array and totalCities to store city locations and randomly placed cities on the canvas using createVector

--- 71.Traveling-Salesperson/sketch.js
+++ 71.Traveling-Salesperson/sketch.js
@@ -1,9 +1,18 @@
+var cities = [];
+var totalCities = 5;
 
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 300);
+  for (var i = 0; i < totalCities; i++) {
+    var v = createVector(random(width), random(height));
+    cities[i] = v;
+  }
+}
 
+function draw() {
+  background(188, 199, 234); // soft blue
+  fill(255);
+  for (var i = 0; i < cities.length; i++) {
+    ellipse(cities[i].x, cities[i].y, 8, 8);
   }
-  
-  function draw() {
-    background(0);
-  }
+}
