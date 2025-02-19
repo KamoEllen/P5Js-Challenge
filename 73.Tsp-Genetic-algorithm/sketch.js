a342e8864021ec875ccd21359b7b513a56710f04
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-19 17:38:20 +0200

    Update 73.Tsp-Genetic-algorithm , cities are drawn as circles and rendering best path

--- 73.Tsp-Genetic-algorithm/sketch.js
+++ 73.Tsp-Genetic-algorithm/sketch.js
@@ -1,4 +1,3 @@
-// sketch.js
 const cities = [];
 const totalCities = 15;
 const popSize = 500;
@@ -15,19 +14,43 @@ function setup() {
   createCanvas(600, 600);
   const order = [];
   for (let i = 0; i < totalCities; i++) {
-    const v = createVector(random(width), random(height / 2));  // Random city placement
-    cities[i] = v;
+    const v = createVector(random(width), random(height / 2));  
+        cities[i] = v;
     order[i] = i;
   }
 
   for (let i = 0; i < popSize; i++) {
-    population[i] = shuffle(order);  // Randomly shuffle the city order for the population
+    population[i] = shuffle(order);  
   }
   statusP = createP('').style('font-size', '32pt');
 }
 
 function draw() {
-  background(0,44,7);  // Clear the screen each frame
-
+    background(0,44,7);
   
-}
+
+    stroke(255);
+    strokeWeight(4);
+    noFill();
+    beginShape();
+    for (let i = 0; i < bestEver.length; i++) {
+      const n = bestEver[i];
+      vertex(cities[n].x, cities[n].y);
+      ellipse(cities[n].x, cities[n].y, 16, 16);  
+        }
+    endShape();
+  
+
+    translate(0, height / 2);  
+        stroke(255);
+    strokeWeight(4);
+    noFill();
+    beginShape();
+    for (let i = 0; i < currentBest.length; i++) {
+      const n = currentBest[i];
+      vertex(cities[n].x, cities[n].y);
+      ellipse(cities[n].x, cities[n].y, 16, 16);
+    }
+    endShape();
+  }
+  
