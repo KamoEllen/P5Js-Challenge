e3fffa575dd85f66a76446dfc726ce47c9617486
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-19 17:00:39 +0200

    Update 72.Lexicographic-Order, added array to 0-9 and draw() to log the array to the console

--- 72.Lexicographic-Order/sketch.js
+++ 72.Lexicographic-Order/sketch.js
@@ -1,7 +1,18 @@
+var vals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 function setup() {
-    createCanvas(400, 300);
+  createCanvas(400, 300);
+}
+
+function draw() {
+  console.log(vals);
+
+  background(0);
+  textSize(64);
+  var s = '';
+  for (var i = 0; i < vals.length; i++) {
+    s += vals[i];
   }
-  
-  function draw() {
-    background(200, 234,200); 
-  }
+  fill(255);
+  text(s, 20, height / 2);
+}
