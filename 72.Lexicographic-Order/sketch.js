9733e3dd5e71952861fd5e7b0d8ba54a8e980ebb
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-19 17:09:35 +0200

    Update 72.Lexicographic-Order, added 4 steps ,finding the largest index,swapping and reversing the suffix of the array

--- 72.Lexicographic-Order/sketch.js
+++ 72.Lexicographic-Order/sketch.js
@@ -5,14 +5,49 @@ function setup() {
 }
 
 function draw() {
+    background(200, 234,200); 
   console.log(vals);
 
-  background(0);
+  // STEP 1
+  var largestI = -1;
+  for (var i = 0; i < vals.length - 1; i++) {
+    if (vals[i] < vals[i + 1]) {
+      largestI = i;
+    }
+  }
+  if (largestI == -1) {
+    noLoop();
+    console.log('finished');
+  }
+
+  // STEP 2
+  var largestJ = -1;
+  for (var j = 0; j < vals.length; j++) {
+    if (vals[largestI] < vals[j]) {
+      largestJ = j;
+    }
+  }
+
+  // STEP 3
+  swap(vals, largestI, largestJ);
+
+  // STEP 4
+  var endArray = vals.splice(largestI + 1);
+  endArray.reverse();
+  vals = vals.concat(endArray);
+
+//   background(0);
   textSize(64);
   var s = '';
   for (var i = 0; i < vals.length; i++) {
     s += vals[i];
   }
-  fill(255);
+  fill(0);
   text(s, 20, height / 2);
+}
+
+function swap(a, i, j) {
+  var temp = a[i];
+  a[i] = a[j];
+  a[j] = temp;
 }
