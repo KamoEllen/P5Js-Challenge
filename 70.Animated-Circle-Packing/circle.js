23cb83f57eb4366085b86e26a7b77ffef129eebd
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-19 13:57:01 +0200

    Update 70.Animated-Circle-Packing

--- 70.Animated-Circle-Packing/circle.js
+++ 70.Animated-Circle-Packing/circle.js
@@ -1,12 +1,30 @@
+
 class Circle {
-    constructor(x, y) {
-      this.x = x;
-      this.y = y;
-      this.r = 50;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.r = 1;
+    this.growing = true;
+  }
+
+  grow() {
+    if (this.growing) {
+      this.r += 1;
     }
-    show() {
-        fill(255);
-        noStroke();
-        ellipse(this.x, this.y, this.r * 2, this.r * 2);
-      }
-    }
+  }
+
+  show() {
+    fill(255);
+    noStroke();
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  }
+
+  edges() {
+    return (
+      this.x + this.r >= width ||
+      this.x - this.r <= 0 ||
+      this.y + this.r >= height ||
+      this.y - this.r <= 0
+    )
+  }
+}
