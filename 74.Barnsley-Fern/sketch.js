c66f193c626ebf90a59e3c18b22c89b64654c189
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-19 21:25:32 +0200

    Update 74.Barnsley-Fern, added nextPoint function to calculate the next point using the Barnsley Fern algorithm

--- 74.Barnsley-Fern/sketch.js
+++ 74.Barnsley-Fern/sketch.js
@@ -20,3 +20,29 @@ function drawPoint() {
   let py = map(y, 0, 9.9983, height, 0);
   point(px, py);
 }
+
+function nextPoint() {
+  let nextX;
+  let nextY;
+  let r = random(1);
+
+  if (r < 0.01) {
+    // Stem
+    nextY = 0.16 * y;
+    nextX = 0;
+  } else if (r < 0.86) {
+    // Small leaflet 
+    nextX = 0.85 * x + 0.04 * y;
+    nextY = -0.04 * x + 0.85 * y + 1.6;
+  } else if (r < 0.93) {
+    // Large left 
+    nextX = 0.2 * x + -0.26 * y;
+    nextY = 0.23 * x + 0.22 * y + 1.6;
+  } else {
+    // Large right 
+    nextX = -0.15 * x + 0.28 * y;
+    nextY = 0.26 * x + 0.24 * y + 0.44;
+  }
+  x = nextX;
+  y = nextY;
+}
