e06b3b7030ea0708d5f4c146f01bcaee7eabf52d
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-19 21:03:02 +0200

    Update 74.Barnsley-Fern, added x y variables to track point positions , map() to scale points to the canvas dimension and loop to draw 300 points per frame

--- 74.Barnsley-Fern/sketch.js
+++ 74.Barnsley-Fern/sketch.js
@@ -1,7 +1,22 @@
+let x = 0;
+let y = 0;
+
 function setup() {
-    createCanvas(600, 600);
-   }
-  
-  function draw() {
-    background(44,0,7); //red
+  createCanvas(640, 360);
+  background(44, 0, 27); // dark purple
+}
+
+function draw() {
+  for (let i = 0; i < 300; i++) {
+    drawPoint();
+    nextPoint();
   }
+}
+
+function drawPoint() {
+  stroke(399, 196, 22, 55); // yellowish
+  strokeWeight(2);
+  let px = map(x, -2.182, 2.6558, 0, width);
+  let py = map(y, 0, 9.9983, height, 0);
+  point(px, py);
+}
