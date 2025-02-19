fb7a772bb9ed693764176ba88af119502418731a
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-20 01:59:13 +0200

    Update 75.Animated-Circle-Packing , pdate on circle class to accept color parameter and newCircle can pass the color from the image to the circle

--- 75.Animated-Circle-Packing/circle.js
+++ 75.Animated-Circle-Packing/circle.js
@@ -1,29 +1,15 @@
 class Circle {
-    constructor(x, y) {
+    constructor(x, y, color) {
       this.x = x;
       this.y = y;
-      this.r = 1;
+      this.r = 2;
+      this.color = color;
       this.growing = true;
     }
   
-    grow() {
-      if (this.growing) {
-        this.r += 1;
-      }
-    }
-  
     show() {
-      fill(255);
+      fill(this.color);
       noStroke();
       ellipse(this.x, this.y, this.r * 2, this.r * 2);
-    }
-  
-    edges() {
-      return (
-        this.x + this.r >= width ||
-        this.x - this.r <= 0 ||
-        this.y + this.r >= height ||
-        this.y - this.r <= 0
-      );
     }
   }
--- 75.Animated-Circle-Packing/sketch.js
+++ 75.Animated-Circle-Packing/sketch.js
@@ -1,28 +1,3 @@
-let circles;
-let spots;
-let img;
-
-function preload() {
-  img = loadImage("dolphin.webp");
-}
-
-function setup() {
-  createCanvas(900, 400);
-  img.loadPixels();
-  spots = [];
-  circles = [];
-  for (let x = 0; x < img.width; x++) {
-    for (let y = 0; y < img.height; y++) {
-      let index = x + y * img.width;
-      let c = img.pixels[index * 4];
-      let b = brightness([c]);
-      if (b > 1) {
-        spots.push(createVector(x, y));
-      }
-    }
-  }
-}
-
 function newCircle() {
     let x = random(0, img.width);
     let y = random(0, img.height);
@@ -31,80 +6,19 @@ function newCircle() {
     for (let i = 0; i < circles.length; i++) {
       let circle = circles[i];
       let d = dist(x, y, circle.x, circle.y);
-      if (d < circle.r) {
+      if (d - 2 < circle.r) {
         valid = false;
         break;
       }
     }
     if (valid) {
-      return new Circle(x, y);
+      let index = (int(x) + int(y) * img.width) * 4;
+      let r = img.pixels[index];
+      let g = img.pixels[index + 1];
+      let b = img.pixels[index + 2];
+      let c = color(r, g, b);
+      return new Circle(x, y, color(c));
     } else {
       return null;
     }
   }
-  
-function draw() {
-  background(0);
-  let total = 5;
-  let count = 0;
-  let attempts = 0;
-
-  while (count < total) {
-    let newC = newCircle();
-    if (newC !== null) {
-      circles.push(newC);
-      count++;
-    }
-    attempts++;
-    if (attempts > 1000) {
-      noLoop();
-      console.log("finished");
-      break;
-    }
-  }
-
-  for (let i = 0; i < circles.length; i++) {
-    let circl = circles[i];
-    if (circl.growing) {
-      if (circl.edges()) {
-        circl.growing = false;
-      } else {
-        for (let j = 0; j < circles.length; j++) {
-          let other = circles[j];
-          if (circl !== other) {
-            var d = dist(circl.x, circl.y, other.x, other.y);
-            var distance = circl.r + other.r;
-            if (d - 4 < distance) {
-              circl.growing = false;
-              break;
-            }
-          }
-        }
-      }
-    }
-    circl.show();
-    circl.grow();
-  }
-}
-
-function newCircle() {
-  var r = int(random(0, spots.length));
-  var spot = spots[r];
-  var x = spot.x;
-  var y = spot.y;
-
-  var valid = true;
-  for (var i = 0; i < circles.length; i++) {
-    var circle = circles[i];
-    var d = dist(x, y, circle.x, circle.y);
-    if (d < circle.r) {
-      valid = false;
-      break;
-    }
-  }
-  if (valid) {
-    return new Circle(x, y);
-  } else {
-    return null;
-  }
-}
