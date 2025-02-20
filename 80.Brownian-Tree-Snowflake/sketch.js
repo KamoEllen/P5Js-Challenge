1683a4d2ca5c447d832c04a663d1be8c03a43491
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-20 21:52:52 +0200

    Update 80.Brownian-Tree-Snowflake

--- 80.Brownian-Tree-Snowflake/sketch.js
+++ 80.Brownian-Tree-Snowflake/sketch.js
@@ -1,28 +1,41 @@
+
 let current;
 let snowflake = [];
 
 function setup() {
-    createCanvas(600, 400);
-    current = new Particle(width/2,0);
-  }
- 
-  
-  function draw() {
-    background(200, 120, 180); //soft purple
-    translate(width/2, height/2);
+  createCanvas(windowWidth, windowHeight);
+  current = new Particle(height/2, 0);
+}
 
+function draw() {
+  translate(width/2, height/2);
+  rotate(PI/6);
+  background(200, 120, 180); //soft purple
+
+  let count = 0;
+  while (!current.finished() && !current.intersects(snowflake)) {
+    current.update();
+    count++;
+  }
+
+  
 
   snowflake.push(current);
   current = new Particle(height/2, 0);
 
-
-    for (let i = 0; i < 88; i++) {
+  for (let i = 0; i < 6; i++) {
     rotate(PI/3);
     current.show();
     for (let p of snowflake) {
       p.show();
     }
-    
-    current.update();
+
+    push();
+    scale(1, -1);
+    current.show();
+    for (let p of snowflake) {
+      p.show();
+    }
+    pop();
   }
 }
