7f6453e79ecddf2e84f8bbcb04105b5315703d75
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 00:33:26 +0200

    Update 82.Bubble-Sort , added lines that will be animated

--- 82.Bubble-Sort/sketch.js
+++ 82.Bubble-Sort/sketch.js
@@ -1,10 +1,21 @@
+let values = [];
+
 function setup() {
-    createCanvas(400, 400);
-
+  createCanvas(windowWidth, windowHeight);
+  values = new Array(width);
+  for (let i = 0; i < values.length; i++) {
+    values[i] = random(height);
+ 
   }
-  
-  function draw() {
+}
 
+function draw() {
     background(180,200, 120); //powder green
 
+
+  for (let i = 0; i < values.length; i++) {
+    stroke(255);
+    line(i, height, i, height - values[i]);
   }
+}
+
