1768dcfc2bd3f7662d3ab57893197eb3931f36ff
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-20 18:15:50 +0200

    Added 77.Inverse-Kinematics , added parent and child properties and follow() to make segments follow their child or a target

--- 77.Inverse-Kinematics/segment.js
+++ 77.Inverse-Kinematics/segment.js
@@ -1,10 +1,32 @@
-class Segment {
-    constructor(point, len, i) {
-      this.a = point;
+
+class Segment {    constructor(point, len, i) {
+      if (point.hasOwnProperty("angle")) {
+        this.par = point;
+        this.a = this.par.b.copy();
+      } else {
+        this.par = false;
+        this.a = point;
+      }
+  
       this.b = new p5.Vector();
       this.angle = 0;
       this.len = len;
       this.calculateB();
+    }
+  
+    follow(tx, ty) {
+      if (typeof ty == "undefined") {
+        let targetX = this.child.a.x;
+        let targetY = this.child.a.y;
+        this.follow(targetX, targetY);
+      } else {
+        let target = new p5.Vector(tx, ty);
+        let dir = p5.Vector.sub(target, this.a);
+        this.angle = dir.heading();
+        dir.setMag(this.len);
+        dir.mult(-1);
+        this.a = p5.Vector.add(target, dir);
+      }
     }
   
     calculateB() {
@@ -24,4 +46,3 @@ class Segment {
     }
   }
   
- 
--- 77.Inverse-Kinematics/sketch.js
+++ 77.Inverse-Kinematics/sketch.js
@@ -1,8 +1,30 @@
-function setup() {
-    createCanvas(600, 600);
-
+  let tentacle;
+  
+  function setup() {
+    createCanvas(600, 400);
+  
+    let point = new p5.Vector(300, 200);
+    let current = new Segment(point, 10, 0);
+    for (let i = 0; i < 20; i++) {
+      let next = new Segment(current, 10, i);
+      current.child = next;
+      current = next;
+    }
+    tentacle = current;
   }
   
   function draw() {
-    background(141,100,22); //brown
+    background(141, 100, 22);
+  
+    tentacle.follow();
+    tentacle.update();
+    tentacle.show();
+  
+    let next = tentacle.par;
+    while (next) {
+      next.follow();
+      next.update();
+      next.show();
+      next = next.par;
+    }
   }
