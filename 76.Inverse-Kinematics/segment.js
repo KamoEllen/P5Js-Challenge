d60707e04a8693a1cb7b68f8deb966f1757ad52d
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-20 17:04:43 +0200

    Update 76.Inverse-Kinematics, added segment class

--- 76.Inverse-Kinematics/segment.js
+++ 76.Inverse-Kinematics/segment.js
@@ -1,3 +1,14 @@
 class Segment {
+    constructor(point, len, angle) {
+        //point on 2 parts of segment, lenght/height of segment, angle of  rotation relative to x axis
+        this.len = len;
+    this.angle = angle;
+    this.point = point;
+    }
+    show() {
+        stroke(255);
+        strokeWeight(4);
+        line(this.len,this.point,this.angle);
+      }
 
 }
--- 76.Inverse-Kinematics/sketch.js
+++ 76.Inverse-Kinematics/sketch.js
@@ -1,8 +1,11 @@
+let tentacle;
 function setup() {
     createCanvas(900, 400);
+    tentacle = new Segment(10, 10, 10);
   }
   
   function draw() {
     background(14,35,16); //dark green
+    tentacle.show();
 
   }
