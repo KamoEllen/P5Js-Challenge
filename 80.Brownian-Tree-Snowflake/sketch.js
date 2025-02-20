a45dfaf69589333dba5d0512c5e41b823e7873b7
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-20 20:28:35 +0200

    Update 80.Brownian-Tree-Snowflake , added array of snowflakes

--- 80.Brownian-Tree-Snowflake/sketch.js
+++ 80.Brownian-Tree-Snowflake/sketch.js
@@ -1,4 +1,6 @@
 let current;
+let snowflake = [];
+
 function setup() {
     createCanvas(600, 400);
     current = new Particle(width/2,0);
@@ -8,6 +10,19 @@ function setup() {
   function draw() {
     background(200, 120, 180); //soft purple
     translate(width/2, height/2);
+
+
+  snowflake.push(current);
+  current = new Particle(height/2, 0);
+
+
+    for (let i = 0; i < 88; i++) {
+    rotate(PI/3);
     current.show();
+    for (let p of snowflake) {
+      p.show();
+    }
+    
     current.update();
   }
+}
