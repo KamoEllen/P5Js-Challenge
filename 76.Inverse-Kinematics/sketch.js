56ddee5a9baaa10a3e44a35dc594d48ad542002c
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-20 17:48:41 +0200

    Update 76.Inverse-Kinematics, added few more functions

--- 76.Inverse-Kinematics/sketch.js
+++ 76.Inverse-Kinematics/sketch.js
@@ -1,11 +1,30 @@
 let tentacle;
-function setup() {
-    createCanvas(900, 400);
-    tentacle = new Segment(10, 10, 10);
-  }
-  
-  function draw() {
-    background(14,35,16); //dark green
-    tentacle.show();
 
+function setup() {
+  createCanvas(600, 400);
+
+  let x = width / 2;
+  let y = height;
+  let pos = new p5.Vector(x, y);
+
+  tentacle = new Segment(pos, 10, 0);
+
+  let current = tentacle;
+  for (let i = 0; i < 20; i++) {
+    let next = new Segment(current, 10, 0);
+    current.child = next;
+    current = next;
   }
+}
+
+function draw() {
+  background(14,35,16);
+
+  let next = tentacle;
+  while (next) {
+    next.wiggle();
+    next.update();
+    next.show();
+    next = next.child;
+  }
+}
