8ca992645de316c9a51156efeed2567ffcae6185
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-20 02:06:11 +0200

    Update 75.Animated-Circle-Packing , removed comments and pdated cicle constructor

--- 75.Animated-Circle-Packing/circle.js
+++ 75.Animated-Circle-Packing/circle.js
@@ -7,9 +7,24 @@ class Circle {
       this.growing = true;
     }
   
+    grow() {
+      if (this.growing) {
+        this.r += 1;
+      }
+    }
+  
     show() {
       fill(this.color);
       noStroke();
       ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    }
+  
+    edges() {
+      return (
+        this.x + this.r >= width ||
+        this.x - this.r <= 0 ||
+        this.y + this.r >= height ||
+        this.y - this.r <= 0
+      );
     }
   }
