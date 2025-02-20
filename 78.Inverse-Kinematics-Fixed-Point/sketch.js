0a45d636b4b410fbe52548b50aa590b8a003f9d0
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-20 19:47:50 +0200

    Update 78.Inverse-Kinematics-Fixed-Point , update

--- 78.Inverse-Kinematics-Fixed-Point/sketch.js
+++ 78.Inverse-Kinematics-Fixed-Point/sketch.js
@@ -3,13 +3,13 @@ const segLen = 15;
 const numSegs = 40;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(400, 400);
 
     robotarm = new RobotArm(width / 2, height, numSegs, segLen, 0);
 }
 
 function draw() {
-    background(51);
+    background(141,100,22);
 
     robotarm.update();
     robotarm.show();
