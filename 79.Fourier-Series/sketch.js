7be53a120dafe4d985e24c69aa99214544b5cabf
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-20 20:14:42 +0200

    Update  79.Fourier-Series, added waves and a slider

--- 79.Fourier-Series/sketch.js
+++ 79.Fourier-Series/sketch.js
@@ -1,33 +1,57 @@
+
 let time = 0;
+let wave = [];
+let path = [];
+
+let slider;
+
 function setup() {
-    createCanvas(600, 400);
+  createCanvas(600, 400);
+  slider = createSlider(1, 50, 5);
+}
 
-  }
-  
-  function draw() { //draw loops each frame
-    background(141, 22,100 ); //purple
-    translate(200,200);
+function draw() {
+    
+    background(30,90,100,99 ); 
+  translate(150, 200);
 
-    //cirlce
-    let radius =90;
+  let x = 0;
+  let y = 0;
+
+  for (let i = 0; i < slider.value(); i++) {
+    let prevx = x;
+    let prevy = y;
+
+    let n = i * 2 + 1;
+    let radius = 75 * (4 / (n * PI));
+    x += radius * cos(n * time);
+    y += radius * sin(n * time);
+
+    stroke(255, 100);
     noFill();
-    stroke(0,100,66);
-    ellipse(0,0,radius*2);
+    ellipse(prevx, prevy, radius * 2);
 
-    //polar coordinates
-    //x = r * cos(time);
-    //y =  r * sin(time);
-
-    let x = radius * cos(time);
-    let y  = radius * sin(time);
     fill(30,90,100);
-    //line from origin to small circle
-    line(0,0,x,y);
-    ellipse(x,y,8);
-
-
-
-
-    time -= 0.07;
-  
+    stroke(255);
+    line(prevx, prevy, x, y);
+    ellipse(x, y, 8);
   }
+  wave.unshift(y);
+
+
+  translate(200, 0);
+  line(x - 200, y, 0, wave[0]);
+  beginShape();
+  noFill();
+  for (let i = 0; i < wave.length; i++) {
+    vertex(i, wave[i]);
+  }
+  endShape();
+
+  time += 0.05;
+
+
+  if (wave.length > 250) {
+    wave.pop();
+  }
+}
