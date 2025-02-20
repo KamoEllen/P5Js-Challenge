966deb5cd307488913f95a76f3c079d35534fe14
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-20 19:29:30 +0200

    Update 78.Inverse-Kinematics-Fixed-Point , added code from prev project in folder called 77.Inverse-Kinematics

--- 78.Inverse-Kinematics-Fixed-Point/sketch.js
+++ 78.Inverse-Kinematics-Fixed-Point/sketch.js
@@ -1,7 +1,30 @@
-function setup() {
-    createCanvas(600, 400);
-  }
+let tentacle;
   
-  function draw() {
-    background(22, 100, 141);
+function setup() {
+  createCanvas(600, 400);
+
+  let point = new p5.Vector(300, 200);
+  let current = new Segment(point, 10, 0);
+  for (let i = 0; i < 20; i++) {
+    let next = new Segment(current, 10, i);
+    current.child = next;
+    current = next;
   }
+  tentacle = current;
+}
+
+function draw() {
+  background(141, 100, 22);
+
+  tentacle.follow(mouseX, mouseY); // Follow mouse
+  tentacle.update();
+  tentacle.show();
+
+  let next = tentacle.par;
+  while (next) {
+    next.follow();
+    next.update();
+    next.show();
+    next = next.par;
+  }
+}
