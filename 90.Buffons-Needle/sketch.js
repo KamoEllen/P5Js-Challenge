c1d42f36a0e8c401a7290401f79a1e18c5272fa5
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 17:32:29 +0200

    Update 90.Buffons-Needle, drew vertical grid lines spaced by t = 20

--- 90.Buffons-Needle/sketch.js
+++ 90.Buffons-Needle/sketch.js
@@ -1,7 +1,18 @@
+let t = 20;
+let len = 10;
+
 function setup() {
-    createCanvas(600, 400);
-  }
+  createCanvas(600, 600);
+  background(155, 180, 100);
+
   
-  function draw() {
-    background(155,  180, 100);
+  for (let col = 0; col < width; col += t) {
+    stroke(255);
+    strokeWeight(1);
+    line(col, 0, col, height);
   }
+}
+
+function draw() {
+  
+}
