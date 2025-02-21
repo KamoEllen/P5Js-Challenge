db33f33ccf1e1fe192f431dcee388eec21a17125
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 14:56:08 +0200

    Update  84.GIF-Loop, added moving ellipse

--- 84.GIF-Loop/sketch.js
+++ 84.GIF-Loop/sketch.js
@@ -1,8 +1,20 @@
+const totalFrames = 120;
+let counter = 0;
+
 function setup() {
-    createCanvas(800, 800);
-  }
-  
-  
-  function draw() {
-    background(100,155,180)
-  }
+  createCanvas(400, 400);
+}
+
+function draw() {
+  let percent = 0;
+    percent = float(counter) / totalFrames;
+  render(percent);
+  counter++;
+}
+
+function render(percent) {
+  let angle = map(percent, 0, 1, 0, TWO_PI);
+  background(100,155,180)
+  ellipse(percent * width , height/2, 20,20);
+
+}
