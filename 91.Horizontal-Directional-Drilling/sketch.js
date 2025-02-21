f386c134c12dc8f3d8d864288ed6de8bbcb85e4b
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 22:02:16 +0200

    Update  91.Horizontal-Directional-Drilling , added scene

--- 91.Horizontal-Directional-Drilling/sketch.js
+++ 91.Horizontal-Directional-Drilling/sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-    createCanvas(600, 400);
-  }
-  
-  function draw() {
-   background(100, 255,155);
-  }
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(100, 255,155);
+  noStroke();
+  rectMode(CORNER);
+  fill(155, 100, 255);
+  rect(0, 100, width, height - 100);
+  fill(255, 100, 155);
+  arc(width / 2, 100, 400, 200, 0, PI);
+
+}
+
+   
