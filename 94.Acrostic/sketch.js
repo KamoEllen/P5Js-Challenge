0d8c0cfbbb45be31bd80c4a3a8e82f6aa588b8bb
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-22 00:08:54 +0200

    Update 94.Acrostic, added logic in functions

--- 94.Acrostic/sketch.js
+++ 94.Acrostic/sketch.js
@@ -1,25 +1,36 @@
 let url1 = 'https://api.datamuse.com/words?';
 
 function setup() {
-   noCanvas();
-   let button = select('#submitbutton');
-   let input = select('#wordinput');
-}
-// function draw()
-// {
-//     background(200,100,230);
-// }
+  noCanvas();
+  let button = select('#submitbutton');
+  let input = select('#wordinput');
+  let resultsDiv = select('#results');
 
-function pickWord(letter, word) {
+  button.mousePressed(makeAcrostic);
+
+  function pickWord(div, letter, word) {
     let url = `${url1}sp=${letter}*&ml=${word}&max=5`;
     loadJSON(url, gotData);
+
+    function gotData(data) {
+      if (data.length > 0) {
+        options = data;
+        let selection = random(options);
+        div.html(selection.word);
+      } else {
+        div.html('No matching word!');
+      }
+    }
   }
 
-function makeAcrostic()
- {
-    resultsDiv.html(''); 
+  function makeAcrostic() {
+    resultsDiv.html(''); // Clear previous results
     let word = input.value().toUpperCase();
-    console.log(word);
 
-    
+    for (let i = 0; i < word.length; i++) {
+      let letter = word.charAt(i);
+      let div = createDiv('').parent(resultsDiv);
+      pickWord(div, letter, word);
+    }
+  }
 }
