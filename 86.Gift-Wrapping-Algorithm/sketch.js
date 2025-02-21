cf3c19718414b32284e7e2dac0e6d7cef97e7d62
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 15:45:14 +0200

    Update  86.Gift-Wrapping-Algorithm, addeed random points and leftmost point

--- 86.Gift-Wrapping-Algorithm/sketch.js
+++ 86.Gift-Wrapping-Algorithm/sketch.js
@@ -1,15 +1,21 @@
-
 const points = [];
+const hull = [];
+
+let leftMost;
+
 function setup() {
   createCanvas(500, 500);
+  let buffer = 20;
   for (let i = 0; i < 50; i++) {
-    points.push(createVector(random( width ), random(height)));
+    points.push(createVector(random(width), random(height)));
   }
-
+  points.sort((a, b) => a.x - b.x);
+  leftMost = points[0];
+  hull.push(leftMost);
 }
 
 function draw() {
-    background(100,155,180)
+  background(100, 155, 180);
 
   stroke(255);
   strokeWeight(8);
@@ -17,9 +23,7 @@ function draw() {
     point(p.x, p.y);
   }
 
+  stroke(0, 255, 0);
+  strokeWeight(32);
+  point(leftMost.x, leftMost.y);
 }
-
-
-
-   
-  
