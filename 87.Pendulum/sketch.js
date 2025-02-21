4ddbe5cc36e177deeca71978e46da0c731d7718a
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 16:49:49 +0200

    Update 87.Pendulum ,using polar coordination

--- 87.Pendulum/sketch.js
+++ 87.Pendulum/sketch.js
@@ -1,12 +1,25 @@
+let angle;
+let bob;
+let len;
+let origin;
+
 function setup() {
     createCanvas(400, 400);
+    angle = PI/4;
+    origin = createVector(300, 0);
+    bob = createVector();
+    len = 100;
   }
   
   function draw() {
       background(180,100,155)
 
+      bob.x = len * sin(angle) + origin.x;
+  bob.y = len * cos(angle) + origin.y;
+
       stroke(255);
+      fill(200,300,100)
       strokeWeight(8);
-      line(300,0,300,200);
-      circle(300,200,64);
+      line(origin.x, origin.y, bob.x, bob.y);
+      circle(bob.x, bob.y, 64);
   }
