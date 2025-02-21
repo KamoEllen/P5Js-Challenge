a3403026dca7fe1798425dfb48fb3e41836077c8
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 14:59:42 +0200

    Update  84.GIF-Loop, added moving square

--- 84.GIF-Loop/sketch.js
+++ 84.GIF-Loop/sketch.js
@@ -7,14 +7,27 @@ function setup() {
 
 function draw() {
   let percent = 0;
+
     percent = float(counter) / totalFrames;
+
   render(percent);
+
+    save("output/gif-" + nf(counter, 3) + ".png");
+    if (counter == totalFrames - 1) {
+      noLoop();
+    }
+
   counter++;
 }
 
 function render(percent) {
   let angle = map(percent, 0, 1, 0, TWO_PI);
   background(100,155,180)
-  ellipse(percent * width , height/2, 20,20);
 
+  ellipse(percent * width , height/2, 20,20);
+  rotate(angle);
+  stroke(255);
+  noFill();
+  rectMode(CENTER);
+  square(0, 0, 100);
 }
