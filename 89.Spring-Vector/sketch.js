1958c1989c57f98cad1d158c77f14782ae0fced3
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 17:21:22 +0200

    Update 89.Spring-Vector, attached line to bouncing circle

--- 89.Spring-Vector/sketch.js
+++ 89.Spring-Vector/sketch.js
@@ -1,29 +1,41 @@
 
-let y = 250;
-let velocity = 0;
+let bob;
+let anchor;
+let velocity;
 let restLength = 200;
 let k = 0.01;
+let gravity;
 
 function setup() {
   createCanvas(600, 400);
+  bob = createVector(350, 0);
+  anchor = createVector(300, 0);
+  velocity = createVector(0, 0);
+  gravity = createVector(0, 0.1);
 }
 
 function draw() {
- background(155, 100, 180);
-  noStroke();
+  background(155, 100, 180);
+  strokeWeight(4);
+  stroke(255);
+  line(anchor.x, anchor.y, bob.x, bob.y);
   fill(45, 197, 244);
-  circle(300, y, 64);
+  circle(anchor.x, anchor.y, 32);
+  circle(bob.x, bob.y, 64);
 
-  let x = y - restLength;
-  let force = -k * x;
+  if (mouseIsPressed) {
+    bob.x = mouseX;
+    bob.y = mouseY;
+    velocity.set(0, 0);
+  }
 
+  let force = p5.Vector.sub(bob, anchor);
+  let x = force.mag() - restLength;
+  force.normalize();
+  force.mult(-1 * k * x);
 
-  velocity += force;
-  y += velocity;
-
-  velocity *= 0.99;
+  velocity.add(force);
+  velocity.add(gravity);
+  bob.add(velocity);
+  velocity.mult(0.99);
 }
-
-
-     
-  
