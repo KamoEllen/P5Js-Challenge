84a1f8c2d28c32bcc4be8113db3f6198ddf5fb6e
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 16:54:36 +0200

    Update 87.Pendulum ,animated shape

--- 87.Pendulum/sketch.js
+++ 87.Pendulum/sketch.js
@@ -1,25 +1,38 @@
+
 let angle;
+
+let angleV = 0;
+let angleA = 0;
+
 let bob;
 let len;
 let origin;
 
-function setup() {
-    createCanvas(400, 400);
-    angle = PI/4;
-    origin = createVector(300, 0);
-    bob = createVector();
-    len = 100;
-  }
-  
-  function draw() {
-      background(180,100,155)
+let gravity = 1;
 
-      bob.x = len * sin(angle) + origin.x;
+function setup() {
+  createCanvas(600, 600);
+  origin = createVector(300, 0);
+  angle = PI / 4;
+  bob = createVector();
+  len = 200;
+}
+
+function draw() {
+    background(180,100,155)
+
+  let force = gravity * sin(angle);
+  angleA = (-1 * force) / len;
+  angleV += angleA;
+  angle += angleV;
+
+
+  bob.x = len * sin(angle) + origin.x;
   bob.y = len * cos(angle) + origin.y;
 
-      stroke(255);
-      fill(200,300,100)
-      strokeWeight(8);
-      line(origin.x, origin.y, bob.x, bob.y);
-      circle(bob.x, bob.y, 64);
-  }
+  stroke(255);
+  strokeWeight(8);
+  fill(127);
+  line(origin.x, origin.y, bob.x, bob.y);
+  circle(bob.x, bob.y, 64);
+}
