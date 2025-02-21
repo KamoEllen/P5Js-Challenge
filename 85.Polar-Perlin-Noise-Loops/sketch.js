36861a6be4cad39fb31b4c3d62b3a753d40a86fe
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 15:26:11 +0200

    Update 85.Polar-Perlin-Noise-Loops, added slider

--- 85.Polar-Perlin-Noise-Loops/sketch.js
+++ 85.Polar-Perlin-Noise-Loops/sketch.js
@@ -1,5 +1,11 @@
+
+let phase = 0;
+let zoff = 0;
+let slider;
+
 function setup() {
   createCanvas(400, 400);
+  slider = createSlider(0, 10, 3, 0.1);
 }
 
 function draw() {
@@ -9,12 +15,18 @@ function draw() {
   strokeWeight(2);
   noFill();
   beginShape();
-
-  for (let a = 0; a < TWO_PI; a += 0.01) {
-   let r = random(50,100);
+  let noiseMax = slider.value();
+  for (let a = 0; a < TWO_PI; a += radians(2)) {
+    let xoff = map(cos(a + phase), -1, 1, 0, noiseMax);
+    let yoff = map(sin(a + phase), -1, 1, 0, noiseMax);
+    let r = map(noise(xoff, yoff, zoff), 0, 1, 100, height / 2);
+    // let r = random(50,100);
     let x = r * cos(a);
     let y = r * sin(a);
     vertex(x, y);
   }
   endShape(CLOSE);
+//   noLoop();
+  phase += 0.003;
+  zoff += 0.01;
 }
