3288ea1f3b4ffff4e51c43dc8a4669f95fbae23d
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 23:59:43 +0200

    Update 94.Acrostic, added pickWord function and api

--- 94.Acrostic/sketch.js
+++ 94.Acrostic/sketch.js
@@ -1,12 +1,20 @@
+let url1 = 'https://api.datamuse.com/words?';
+
 function setup() {
    noCanvas();
    let button = select('#submitbutton');
    let input = select('#wordinput');
 }
-function draw()
-{
-    background(200,100,230);
-}
+// function draw()
+// {
+//     background(200,100,230);
+// }
+
+function pickWord(letter, word) {
+    let url = `${url1}sp=${letter}*&ml=${word}&max=5`;
+    loadJSON(url, gotData);
+  }
+
 function makeAcrostic()
  {
     resultsDiv.html(''); 
