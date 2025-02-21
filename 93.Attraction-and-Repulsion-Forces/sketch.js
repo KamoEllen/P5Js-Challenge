c996a6a645344375b228ef64b34a6892ee9b794b
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 23:25:11 +0200

    Update  93.Attraction-and-Repulsion-Forces,  100 particles and attractors are shown as green dots.

--- 93.Attraction-and-Repulsion-Forces/sketch.js
+++ 93.Attraction-and-Repulsion-Forces/sketch.js
@@ -11,4 +11,25 @@ function mousePressed() {
 
 function draw() {
   background(200, 200, 155);
+
+  particles.push(new Particle(random(width), random(height)));
+
+  if (particles.length > 100) {
+    particles.splice(0, 1);
+  }
+
+  for (let i = 0; i < attractors.length; i++) {
+    stroke(0, 255, 0);
+    strokeWeight(4);
+    point(attractors[i].x, attractors[i].y);
+  }
+
+  for (let i = 0; i < particles.length; i++) {
+    let particle = particles[i];
+    for (let j = 0; j < attractors.length; j++) {
+      particle.attracted(attractors[j]);
+    }
+    particle.update();
+    particle.show();
+  }
 }
