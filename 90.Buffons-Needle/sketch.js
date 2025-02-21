dd16f0a73d14576eb5341242ed6fbe3a8dfd81b8
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 17:37:06 +0200

    Update 90.Buffons-Needle,added random lines with random angles and positions and tracking number of lines drawn

--- 90.Buffons-Needle/sketch.js
+++ 90.Buffons-Needle/sketch.js
@@ -1,18 +1,18 @@
-let t = 20;
-let len = 10;
-
-function setup() {
-  createCanvas(600, 600);
-  background(155, 180, 100);
-
-  
-  for (let col = 0; col < width; col += t) {
-    stroke(255);
-    strokeWeight(1);
-    line(col, 0, col, height);
-  }
-}
+let total = 0;
+let intersecting = 0;
 
 function draw() {
-  
+  for (let i = 0; i < 100; i++) {
+    let angle = random(0, PI);
+    let x = random(width);
+    let y = random(height);
+    push();
+    translate(x, y);
+    rotate(angle);
+    strokeWeight(1);
+    line(0, -len / 2, 0, len / 2);
+    pop();
+
+    total++;
+  }
 }
