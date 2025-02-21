cc74a29f383da9a8288d8f3e2e375b8505b9f203
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 15:21:34 +0200

    Update 85.Polar-Perlin-Noise-Loops, added animated circle

--- 85.Polar-Perlin-Noise-Loops/sketch.js
+++ 85.Polar-Perlin-Noise-Loops/sketch.js
@@ -1,6 +1,20 @@
 function setup() {
-    createCanvas(400, 400);
-  }
-  function draw() {
+  createCanvas(400, 400);
+}
+
+function draw() {
     background(180,155,100)
+  translate(width / 2, height / 2);
+  stroke(255);
+  strokeWeight(2);
+  noFill();
+  beginShape();
+
+  for (let a = 0; a < TWO_PI; a += 0.01) {
+   let r = random(50,100);
+    let x = r * cos(a);
+    let y = r * sin(a);
+    vertex(x, y);
   }
+  endShape(CLOSE);
+}
