1377e10d87211f659a3f048357656be442fdcbb8
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-21 22:29:53 +0200

    Update 92.Mad-Libs-Generator , added text data

--- 92.Mad-Libs-Generator/sketch.js
+++ 92.Mad-Libs-Generator/sketch.js
@@ -1,4 +1,14 @@
-function setup() {
+let words = {
+    Exclamation: ["Wow", "Oh no", "Yay", "Oops", "Hooray"],
+    Adverb: ["quickly", "slowly", "happily", "angrily", "quietly"],
+    Noun: ["car", "plane", "bike", "rocket", "boat"],
+    Adjective: ["shiny", "old", "colorful", "rusty", "sparkling"],
+    PluralNoun: ["shoes", "socks", "hats", "gloves", "sunglasses"]
+  };
+  
+  let txt = "_____________! they said ________ as they jumped into their _____________ and flew off with their __________ __________.";
+  
+  function setup() {
     createCanvas(600, 400);
 }
 function draw() {
