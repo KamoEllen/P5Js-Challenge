04923d59ccc8d0efcdc087184fe3bd292e9ebaf2
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-23 00:34:39 +0200

    Update 98.Countdown, displaying secondsand minutes

--- 98.Countdown/sketch.js
+++ 98.Countdown/sketch.js
@@ -1,18 +1,26 @@
-var counter=400;
+var counter=0;
+var timeLeft =60;
 var timer;
 function setup() {
   // createCanvas(400, 400);
   noCanvas();
   var timer= select('#timer')
-  timer.html(counter)
+  timer.html(convertSeconds(timeLeft - counter));
 
   function timeIt()
   {
-    counter--;
-    timer.html(counter);
+    counter++;
+    timer.html(convertSeconds(timeLeft - counter));
   }
 
   setInterval(timeIt,1000); 
+}
+
+function convertSeconds(s)
+{
+  var min = floor(s /60);  //no decimals
+  var sec = s % 60; //remainder 
+  return min + ':' + sec;
 }
 
 function draw() {
