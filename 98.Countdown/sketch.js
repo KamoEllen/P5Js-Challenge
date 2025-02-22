1393cea0612d37852888acaee6d5f721a7ee07f7
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-23 00:23:40 +0200

    Update 98.Countdown, timer goesup

--- 98.Countdown/sketch.js
+++ 98.Countdown/sketch.js
@@ -1,7 +1,20 @@
+var counter=0;
+var timer;
 function setup() {
-    createCanvas(400, 400);
+  // createCanvas(400, 400);
+  noCanvas();
+  var timer= select('#timer')
+  timer.html(counter)
+
+  function timeIt()
+  {
+    counter++;
+    timer.html(counter);
   }
-  
-  function draw() {
-    background(200,  100,29);
-  }
+
+  setInterval(timeIt,1000); 
+}
+
+function draw() {
+  background(200,  100,29);
+}
