8e39f4cfcfcc21b6f4f88ca59de12b2d2c036c78
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-22 20:08:09 +0200

    Update 96.Snowflakes-ml5js , shape generated but not a flower

--- 96.Snowflakes-ml5js/sketch.js
+++ 96.Snowflakes-ml5js/sketch.js
@@ -2,10 +2,16 @@ let model; //name of brain that can generate
 //down -pen down
 //up - pen up
 //end - drawing finished
+let x, y;
+let strokePath = null;
 
 function setup() {
   createCanvas(600, 600);
-  model = ml5.sketchRNN("snowflake", modelReady);
+  background(29,  200,100); //drawing background once
+  translate(width / 2, height / 2); 
+  x =0
+  y = 0;
+  model = ml5.sketchRNN("flower", modelReady);
 
 }
 
@@ -17,7 +23,18 @@ function modelReady()
 }
 
 function draw() {
-    background(29,  200.100);
+   
+    if (strokePath != null) {
+      stroke(255);
+      strokeWeight(4);
+      let newX = x + strokePath.dx ;
+      let newY = y + strokePath.dy ;
+      line(x, y, newX, newY);
+      strokePath = null;
+      x =newX;
+      y = newY;
+      model.generate(gotSketch);
+}
 }
 
 function gotSketch(error, s) { //all ml5 functions need error handling
