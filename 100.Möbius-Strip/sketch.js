5656780a6f8e0bedb70630da060a1a1a7b33bd17
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-23 11:49:23 +0200

    Update  100.Möbius-Strip ,added condition and animation

--- 100.Möbius-Strip/sketch.js
+++ 100.Möbius-Strip/sketch.js
@@ -8,15 +8,18 @@ function setup() {
   function draw() {
       background(70, 129,100);
       orbitControl();
-      noFill();
-  beginShape()
-  for (let theta = 0; theta < 360; theta ++)
-  {
+      rotateX(90)
+      strokeWeight(1);
+  stroke(0);
+  noFill();
+  beginShape(POINTS);
+  for (let theta = 0; theta < 360; theta+=10) {
     for (let phi = 0; phi < 360; phi += 10) {
-    let x = (r1+ r2 *cos(phi)) * cos(theta);
-    let y = (r1+ r2 *cos(phi)) * sin(theta)
-    vertex(x,y)
+      let x = (r1 + r2*cos(phi))*cos(theta);
+      let y = (r1 + r2*cos(phi))*sin(theta);
+      let z = r2*sin(phi)
+      vertex(x, y, z);  
     }
   }
-  endShape()
+  endShape();
   }
