72e7a429c0980da27a07722b6f4f25ab1d9e8665
KamoEllen <kamoellenkganakga@gmail.com>
2025-02-23 11:45:26 +0200

    Update  100.Möbius-Strip ,added formula and animation

--- 100.Möbius-Strip/sketch.js
+++ 100.Möbius-Strip/sketch.js
@@ -1,7 +1,22 @@
+let r1 = 150;
+let r2 = 20;
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(400, 400, WEBGL);
+    angleMode(DEGREES);
   }
   
   function draw() {
       background(70, 129,100);
+      orbitControl();
+      noFill();
+  beginShape()
+  for (let theta = 0; theta < 360; theta ++)
+  {
+    for (let phi = 0; phi < 360; phi += 10) {
+    let x = (r1+ r2 *cos(phi)) * cos(theta);
+    let y = (r1+ r2 *cos(phi)) * sin(theta)
+    vertex(x,y)
+    }
+  }
+  endShape()
   }
